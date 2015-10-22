63b42ba6ba2f981d3b7a708c8ad242cbe29ff34b
truell20 <truell20@gmail.com>
2015-10-22 17:30:53 -0400

    Began work on hlt
    Former-commit-id: fa0e37cd2ab575f563690366436ff8b2bc6bc2e9 [formerly fdec24ee54ffa5b2bc6c3b113e86be213410cb49]
    Former-commit-id: 2cff00b90314007e04b09bea2db71a60852a1949
    Former-commit-id: c710e9a32a6b580327c75ee66fd76f8933d6d6b8

--- AIResources/Python/hlt.py
+++ AIResources/Python/hlt.py
@@ -1,1 +1,47 @@
-print("Hello world")
+from collections import namedtuple
+import random
+import math
+
+Location = namedtuple("Location", "x y")
+Site = namedtuple("Site", "owner strength")
+Move = namedtuple("Move", "loc dir")
+
+class Map:
+	def __init__(width = 0, height = 0, numberOfPlayers = 0):
+		self.map_width = width
+		self.map_height = height
+		self.contents = []
+
+		for y in range(0, map_height):
+			row = []
+			for x in range(0, map_width):
+				row.append(Site(0, 0))
+
+		## Place players onto map
+		takenSpots = []
+		minDistance = math.sqrt(map_height*map_width) / 2
+		for a in range(1, numberOfPlayers + 1):
+			bad = True
+			counter = 0
+			location = None
+			while bad == True:
+				bad == False
+				location = Location(int(random.random()*map_width), int(random.random()*map_height));
+				for spot in takenSpots:
+					if getDistance(location, spot) <= minDistance:
+						bad = True
+						break
+				counter += 1
+				if counter > 150:
+					counter = 0
+					minDistance *= 0.85
+			contents[location.x][location.y] = Site(a, 255)
+			takenSpots.append(location)
+	def getDistance(l1, l2):
+		dx = math.abs(l1.x - l2.x)
+		dy = math.abs(l1.y - l2.y);
+		if dx > map_width / 2:
+			dx = map_width - dx;
+		if dy > map_height / 2:
+			dy = map_height - dy;
+		return math.sqrt((dx*dx) + (dy*dy));
