a751a4f3d2c648bd828d5450ea1750ef4f5c0c3f
truell20 <truell20@gmail.com>
2015-10-22 17:37:54 -0400

    Finshed hlt in python
    Former-commit-id: b100b2f38ba28baff542769c53fb7a53435d486e [formerly 5503f892bb386ec3a9a30d3d16f3ca2e2300ba91]
    Former-commit-id: b6aa019dd358f83502bb4c9b02ef0d6702ca2a1e
    Former-commit-id: 67b1580d70a2d79a3699c88fc3450eaae58906ef

--- AIResources/Python/hlt.py
+++ AIResources/Python/hlt.py
@@ -26,7 +26,7 @@ class Map:
 			location = None
 			while bad == True:
 				bad == False
-				location = Location(int(random.random()*map_width), int(random.random()*map_height));
+				location = Location(int(random.random()*map_width), int(random.random()*map_height))
 				for spot in takenSpots:
 					if getDistance(location, spot) <= minDistance:
 						bad = True
@@ -37,11 +37,56 @@ class Map:
 					minDistance *= 0.85
 			contents[location.x][location.y] = Site(a, 255)
 			takenSpots.append(location)
+	def inBounds(l):
+		return l.x < map_width and l.y < map_height
+
 	def getDistance(l1, l2):
 		dx = math.abs(l1.x - l2.x)
-		dy = math.abs(l1.y - l2.y);
+		dy = math.abs(l1.y - l2.y)
 		if dx > map_width / 2:
-			dx = map_width - dx;
+			dx = map_width - dx
 		if dy > map_height / 2:
-			dy = map_height - dy;
-		return math.sqrt((dx*dx) + (dy*dy));
+			dy = map_height - dy
+		return math.sqrt((dx*dx) + (dy*dy))
+
+	def getAngle(l1, l2):
+		dx = l2.x - l1.x
+		dy = l2.y - l1.y
+
+		if dx > map_width - dx:
+			dx -= map_width
+		elif -dx > map_width + dx:
+			dx += map_width
+
+		if dy > map_height - dy:
+			dy -= map_height
+		elif -dy > map_height + dy:
+			dy += map_height
+		return math.atan2(dy, dx)
+
+	def getLocation(l, direction):
+		if direction != STILL:
+			if direction == NORTH:
+				if l.y == 0:
+					l.y = map_height - 1
+				else:
+					l.y -= 1
+			elif direction == EAST:
+				if l.x == map_width - 1:
+					l.x = 0
+				else:
+					l.x += 1
+			elif direction == SOUTH:
+				if l.y == map_height - 1:
+					l.y = 0
+				else:
+					l.y += 1
+			elif direction == WEST:
+				if l.x == 0:
+					l.x = map_width - 1
+				else:
+					l.x -= 1
+		return l
+	def getSite(l, direction):
+		l = getLocation(l, direction)
+		return contents[l.y][l.x]
