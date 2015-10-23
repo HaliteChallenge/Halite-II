2afd30c989e714aa82db65cac3f883310753dfe6
truell20 <truell20@gmail.com>
2015-10-23 11:55:01 -0400

    Seem to have finished python
    Former-commit-id: 85384b970f7a35344e078e393fbe6b83bbcd15b2 [formerly 19933eb0f5620b1f1bcf426ca092b362f5b398ac]
    Former-commit-id: 2d433966b799c9f58ba6708769232408c154c2e3
    Former-commit-id: 9d348667ce45b70d0dea5eeaedec48b5b140d396

--- AIResources/Python/networking.py
+++ AIResources/Python/networking.py
@@ -1,35 +1,81 @@
+import hlt
 import socket
 
 def serializeMoveSet(moves):
 	returnString = ""
-    for move in moves:
-    	returnString += a->loc.x + " " + a->loc.y + " " + a->dir + " " 
+	for move in moves:
+		returnString += move.loc.x + " " + move.loc.y + " " + move.dir + " " 
 	return returnString
 
 def deserializeMap(inputString):
 	
 	splitString = split(inputString)
-   	width = splitString.pop(0)
-   	height = splitString.pop(0)
+	width = splitString.pop(0)
+	height = splitString.pop(0)
 
-   	m = Map(width, height)
-    
-    y = 0
-    x = 0
-    counter = 0
-    owner = 0
-    while y != m.m_height:
-    	counter = splitString.pop(0)
-    	owner = splitString.pop(0)
-        for a in range(0, counter):
-            m.contents[y][x].owner = owner
-            x += 1
-            if x == m.m_width:
-            	x = 0
-                y += 1
+	m = Map(width, height)
+	
+	y = 0
+	x = 0
+	counter = 0
+	owner = 0
+	while y != m.m_height:
+		counter = splitString.pop(0)
+		owner = splitString.pop(0)
+		for a in range(0, counter):
+			m.contents[y][x].owner = owner
+			x += 1
+			if x == m.m_width:
+				x = 0
+				y += 1
 
-    for a in range(0, len(m.contents)): 
-        for b in range(0, len(m.contents[a])):
-            m.contents[a][b].strength = splitString.pop(0)
-            
+	for a in range(0, len(m.contents)): 
+		for b in range(0, len(m.contents[a])):
+			m.contents[a][b].strength = splitString.pop(0)
+
 	return m
+
+def sendString(s, toBeSent):
+	s.send(int(len(toBeSent)))
+	s.send(toBeSent)
+
+def getString(s):
+	header = int(s.recv(4096))
+	return s.recv(header)
+
+def connectToGame():
+	while True:
+		port = 0
+		while True:
+			try:
+				port = int(raw_input("What port would you like to connect to? Please enter a valid port number: "))
+				break
+			except ValueError:
+				print("That isn't a valid input. Try again.")
+
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		try:
+			s.connect(("127.0.0.1", port))
+			print("Successfully established contact")
+			return sock
+		except Exception:
+			print("There was a problem connecting. Let's try again:")
+
+def getInit(s):
+	print("Get init")
+	
+	playerTag = int(getString(s))
+	m = int(getString(s))
+
+	return (playerTag, m)
+
+def sendInitResponse(s):
+	print("Send init")
+	sendString("Done")
+
+def getFrame(s):
+	return deserializeMap(getString(s))
+
+def sendFrame(s, moves):
+	print("Send frame")
+	sendString(s, serializeMoveSet(moves))
