26daa2c69dc5d9b633939970004a6c53c87c5b2e
truell20 <truell20@gmail.com>
2015-11-10 18:47:29 -0500

    Backend
    Former-commit-id: e5ef1f89cac99957440f293127449a22928f2627 [formerly 666b3650d09b6c561f71583532bc44ade4e199ff]
    Former-commit-id: c8707d4246dc7a95aba8b5e2d35d89628817a30d
    Former-commit-id: 7d8e590643b8adbeb80b23627378c817f46735f7

---
+++ Website/Backend/HaliteAPI.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once 'API.class.php';
+class HaliteAPI extends API
+{
+
+	// The database
+	private $mysqli = NULL;
+
+	public function __construct($request, $origin) {
+		$this->initDB();
+		parent::__construct($request);
+	}
+
+	// Initializes and returns a mysqli object that represents our mysql database
+	private function initDB() {
+		$this->mysqli = new mysqli("Halite.db.12061709.hostedresource.com", 
+			"Halite", 
+			"Fustercluck2!", 
+			"Halite");
+		
+		if (mysqli_connect_errno()) { 
+			echo "<br><br>There seems to be a problem with our database. Reload the page or try again later.";
+			exit(); 
+		}
+	}
+
+	private function select($sql) {
+		$res = mysqli_query($this->mysqli, $sql);
+		return mysqli_fetch_array($res, MYSQLI_ASSOC);
+	}
+
+	private function insert($sql) {
+		mysqli_query($this->mysqli, $sql);
+	}
+
+	// API ENDPOINTS
+	// Endpoint associated with a users credentials (everything in the User table; i.e. name, email, firstname, etc.)
+	protected function user() {
+		if($this->method == 'GET') {
+			if(isset($_GET["username"]) && isset($_GET["password"])) {
+				$username = $_GET["username"];
+				$password = $_GET["password"];
+
+				return select("SELECT * FROM User WHERE username = $username AND password = $password");
+			}
+			if(isset($_GET["userID"]) && isset($_GET["password"])) {
+				$useID = $_GET["userID"];
+				$password = $_GET["password"];
+				return select("SELECT * FROM User WHERE userID = $userID AND password = '$password'");
+			}
+		}
+
+		if($this->method == 'POST') {
+			if(isset($_GET["username"]) && isset($_GET["password"])) {
+				$username = $_GET["username"];
+				$password = $_GET["password"];
+
+				insert("INSERT INTO User (username, password) VALUES ('$username', '$password')");
+				return "success";
+			}
+		}
+
+		return "ERROR";
+	}
+ }
+
+ ?>
