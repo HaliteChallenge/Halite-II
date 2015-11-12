e162ccc098cb4a1f7e990719e637e82b954dafac
Sydriax <benjamin_spector@horacemann.org>
2015-11-11 20:08:24 -0500

    Moved AI Resources Around
    Former-commit-id: f96c607b6efbb74cd5e207ec1010a21057338da0 [formerly 3643dc03f328c54383902be68f989ffc4696e252]
    Former-commit-id: d0c555d1751676ce35a9d359bc9e87fecfbb91d7
    Former-commit-id: f8fa198d89e645eae1dd39014e7fa6b5f3c0fae2

--- AIResources/Java/ExampleBot/ExampleBot.java
+++ AIResources/Java/ExampleBot/ExampleBot.java
@@ -1,7 +1,7 @@
 import java.net.*;
 import java.util.ArrayList;
 
-public class TestAI
+public class ExampleBot
 {
     public static void main(String[] args) {
         System.out.println("\u000c");
