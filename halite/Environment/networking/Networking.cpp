7ca05612429f34f1ee37d154edc327690a0cf50f
truell20 <truell20@gmail.com>
2015-12-24 18:46:01 -0500

    Fixing case sensitive folder p2
    Former-commit-id: f48342b7b91046a00b2cff7f8862a3d439b3fc01 [formerly b5536f27fde28adcc5ca959ad605cd02161a6ed1]
    Former-commit-id: e157f342a5102b53466d37328da2c5ebbe75e836
    Former-commit-id: 7711998a919cfcbb018cff7dfffb63698e0c5230

---
+++ halite/Environment/networking/Networking.cpp
@@ -0,0 +1,348 @@
+#include "Networking.h"
+
+#include <time.h>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <stdio.h>
+
+std::string Networking::serializeMap(const hlt::Map & map)
+{
+	std::string returnString = "";
+	std::ostringstream oss;
+	oss << map.map_width << " " << map.map_height << " ";
+
+	// Run-length encode of owners
+	unsigned short currentOwner = map.contents[0][0].owner;
+	unsigned short counter = 0;
+	for (int a = 0; a < map.contents.size(); ++a)
+	{
+		for (int b = 0; b < map.contents[a].size(); ++b)
+		{
+			if (map.contents[a][b].owner == currentOwner)
+			{
+				counter++;
+			}
+			else
+			{
+				oss << (unsigned short)counter << " " << (unsigned short)currentOwner << " ";
+				counter = 1;
+				currentOwner = map.contents[a][b].owner;
+			}
+		}
+	}
+	// Place the last run into the string
+	oss << (unsigned short)counter << " " << (unsigned short)currentOwner << " ";
+
+	// Encoding of ages
+	for (int a = 0; a < map.contents.size(); ++a)
+	{
+		for (int b = 0; b < map.contents[a].size(); ++b)
+		{
+			oss << (unsigned short)map.contents[a][b].strength << " ";
+		}
+	}
+
+	returnString = oss.str();
+
+	return returnString;
+}
+
+std::set<hlt::Move> Networking::deserializeMoveSet(std::string & inputString)
+{
+	std::set<hlt::Move> moves = std::set<hlt::Move>();
+
+	std::stringstream iss(inputString);
+	hlt::Location l;
+	int d;
+	while (iss >> l.x >> l.y >> d) moves.insert({ l, (unsigned char)d });
+
+	return moves;
+}
+
+std::string Networking::serializeMessages(const std::vector<hlt::Message> &messages) {
+	std::ostringstream oss;
+
+	oss << messages.size() << " ";
+
+	for (int a = 0; a < messages.size(); a++)
+	{
+		hlt::Message message = messages[a];
+		oss << (unsigned short)message.type << " ";
+		oss << message.senderID << " " << message.recipientID << " " << message.targetID << " ";
+	}
+
+	return oss.str();
+}
+
+std::vector<hlt::Message> Networking::deserializeMessages(const std::string &inputString)
+{
+	std::vector<hlt::Message> messages = std::vector<hlt::Message>();
+	std::stringstream iss(inputString);
+
+	int numberOfMessages;
+	iss >> numberOfMessages;
+
+	for (int a = 0; a < numberOfMessages; a++)
+	{
+		hlt::Message message;
+
+		int messageTypeInt;
+		iss >> messageTypeInt;
+		message.type = static_cast<hlt::MessageType>(messageTypeInt);
+
+		iss >> message.senderID >> message.recipientID >> message.targetID;
+
+		messages.push_back(message);
+	}
+
+	return messages;
+}
+
+void Networking::sendString(unsigned char playerTag, std::string &sendString)
+{
+#ifdef _WIN32
+	Connection connection = connections[playerTag - 1];
+	
+	// End message with newline character
+	sendString += '\n';
+
+	// Write sendstring to pipe
+	DWORD charsWritten;
+	bool success;
+	success = WriteFile(connection.write, sendString.c_str(), sendString.length(), &charsWritten, NULL);
+	if (!success || charsWritten == 0) {
+		std::cout << "problem writing\n";
+		throw 1;
+	}
+#else
+	int connectionFd = connections[playerTag - 1];
+	uint32_t length = sendString.length();
+	// Copy the string into a buffer. May want to get rid of this operation for performance purposes
+	std::vector<char> buffer(sendString.begin(), sendString.end());
+
+	send(connectionFd, (char *)&length, sizeof(length), 0);
+	send(connectionFd, &buffer[0], buffer.size(), 0);
+#endif
+}
+
+std::string Networking::getString(unsigned char playerTag, unsigned int timeoutMillis)
+{
+#ifdef _WIN32
+	Connection connection = connections[playerTag - 1];
+
+	DWORD charsRead;
+	bool success;
+	std::string newString;
+	char buffer;
+
+	// Keep reading char by char until a newline
+	while (true) {
+		// Check to see that there are bytes in the pipe before reading
+		// Throw error if no bytes in alloted time
+		// Check for bytes before sampling clock, because reduces latency (vast majority the pipe is alread full)
+		DWORD bytesAvailable = 0;
+		PeekNamedPipe(connection.read, NULL, 0, NULL, &bytesAvailable, NULL);
+		if (bytesAvailable < 1) {
+			clock_t initialTime = clock();
+			while (bytesAvailable < 1) {
+				if (((clock() - initialTime) * 1000 / CLOCKS_PER_SEC) > timeoutMillis) throw 1;
+				PeekNamedPipe(connection.read, NULL, 0, NULL, &bytesAvailable, NULL);
+			}
+		}
+
+		success = ReadFile(connection.read, &buffer, 1, &charsRead, NULL);
+		if (!success || charsRead < 1)
+		{
+			std::cout << "Pipe probably timed out\n";
+			throw 1;
+		}
+		if (buffer == '\n') break;
+		else newString += buffer;
+	}
+
+	// Python turns \n into \r\n
+	if (newString.at(newString.size() - 1) == '\r') newString.pop_back();
+	return newString;
+#else
+	int connectionFd = connections[playerTag - 1];
+	uint32_t numChars;
+	recv(connectionFd, (char *)&numChars, sizeof(numChars), 0);
+
+	std::vector<char> buffer(numChars);
+	recv(connectionFd, &buffer[0], buffer.size(), 0);
+
+	// Copy the buffer into a string. May want to get rid of this for performance purposes
+	return std::string(buffer.begin(), buffer.end());
+	//return "Done";
+#endif
+}
+
+void Networking::startAndConnectBot(std::string command)
+{
+#ifdef _WIN32
+	command = "/C " + command;
+
+	Connection parentConnection, childConnection;
+
+	SECURITY_ATTRIBUTES saAttr;
+	saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+	saAttr.bInheritHandle = TRUE;
+	saAttr.lpSecurityDescriptor = NULL;
+
+	// Child stdout pipe
+	if (!CreatePipe(&parentConnection.read, &childConnection.write, &saAttr, 0)) 
+	{
+		std::cout << "Could not create pipe\n";
+		throw 1;
+	}
+	if (!SetHandleInformation(parentConnection.read, HANDLE_FLAG_INHERIT, 0)) throw 1;
+
+	// Child stdin pipe
+	if (!CreatePipe(&childConnection.read, &parentConnection.write, &saAttr, 0)) 
+	{
+		std::cout << "Could not create pipe\n";
+		throw 1;
+	}
+	if (!SetHandleInformation(parentConnection.write, HANDLE_FLAG_INHERIT, 0)) throw 1;
+
+	// MAKE SURE THIS MEMORY IS ERASED
+	PROCESS_INFORMATION piProcInfo;
+	ZeroMemory(&piProcInfo, sizeof(PROCESS_INFORMATION));
+
+	STARTUPINFO siStartInfo;
+	ZeroMemory(&siStartInfo, sizeof(STARTUPINFO));
+	siStartInfo.cb = sizeof(STARTUPINFO);
+	siStartInfo.hStdError = childConnection.write;
+	siStartInfo.hStdOutput = childConnection.write;
+	siStartInfo.hStdInput = childConnection.read;
+	siStartInfo.dwFlags |= STARTF_USESTDHANDLES;
+
+	// C:/xampp/htdocs/Halite/Halite/Debug/ExampleBot.exe
+	// C:/Users/Michael/Anaconda3/python.exe
+	// C:/Program Files/Java/jre7/bin/java.exe -cp C:/xampp/htdocs/Halite/AIResources/Java MyBot
+	bool success = CreateProcess(
+		"C:\\windows\\system32\\cmd.exe",
+		LPSTR(command.c_str()),     // command line 
+		NULL,          // process security attributes 
+		NULL,          // primary thread security attributes 
+		TRUE,          // handles are inherited 
+		0,             // creation flags 
+		NULL,          // use parent's environment 
+		NULL,          // use parent's current directory 
+		&siStartInfo,  // STARTUPINFO pointer 
+		&piProcInfo
+	);  // receives PROCESS_INFORMATION 
+	if(!success) 
+	{
+		std::cout << "Could not start process\n";
+		throw 1;
+	}
+	else 
+	{
+		CloseHandle(piProcInfo.hProcess);
+		CloseHandle(piProcInfo.hThread);
+
+		processes.push_back(piProcInfo.hProcess);
+		connections.push_back(parentConnection);
+	}
+#else
+	std::cout << "Waiting for player to connect on port " << port << ".\n";
+
+	int socketFd = socket(AF_INET, SOCK_STREAM, 0);
+	if (socketFd < 0)
+	{
+		std::cout << "ERROR opening socket\n";
+		throw 1;
+	}
+
+	struct sockaddr_in serverAddr;
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = INADDR_ANY;
+	serverAddr.sin_port = htons(port);
+
+	if (bind(socketFd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+	{
+		std::cout << "ERROR on binding to port number " << port << "\n";
+		throw 1;
+	}
+
+	listen(socketFd, 5);
+
+	struct sockaddr_in clientAddr;
+	socklen_t clientLength = sizeof(clientAddr);
+	int connectionFd = accept(socketFd, (struct sockaddr *)&clientAddr, &clientLength);
+	if (connectionFd < 0)
+	{
+		std::cout << "ERROR on accepting\n";
+		throw 1;
+	}
+
+	std::cout << "Connected.\n";
+
+	connections.push_back(connectionFd);
+#endif
+}
+
+bool Networking::handleInitNetworking(unsigned int timeoutMillis, unsigned char playerTag, const hlt::Map & m, std::string & playerName)
+{
+	try {
+		sendString(playerTag, std::to_string(playerTag));
+		sendString(playerTag, serializeMap(m));
+		std::cout << "Init Message sent to player " << playerTag << "\n";
+
+		playerName = getString(playerTag, timeoutMillis);
+		std::cout << "Init Message received from player " << playerTag << "\n";
+
+		return true;
+	}
+	catch (int e) {
+		return false;
+	}
+}
+
+bool Networking::handleFrameNetworking(unsigned int timeoutMillis, unsigned char playerTag, const hlt::Map & m, const std::vector<hlt::Message> &messagesForThisBot, std::set<hlt::Move> * moves, std::vector<hlt::Message> * messagesFromThisBot)
+{
+	try
+	{
+		if (processes[playerTag - 1] == NULL) return false;
+
+		std::cout << "turn";
+		// Send this bot the game map and the messages addressed to this bot
+		sendString(playerTag, serializeMap(m));
+		sendString(playerTag, serializeMessages(messagesForThisBot));
+
+		moves->clear();
+
+		*moves = deserializeMoveSet(getString(playerTag, timeoutMillis));
+		*messagesFromThisBot = deserializeMessages(getString(playerTag, timeoutMillis));
+
+		return true;
+	}
+	catch (int e) 
+	{
+		return false;
+	}
+
+}
+
+void Networking::killPlayer(unsigned char playerTag) {
+#ifdef _WIN32
+	
+	HANDLE process = processes[playerTag - 1];
+	if (process == NULL) return;
+
+	TerminateProcess(process, 0);
+
+	processes[playerTag - 1] = NULL;
+	connections[playerTag - 1].read = NULL;
+	connections[playerTag - 1].write = NULL;
+#endif
+}
+
+int Networking::numberOfPlayers() {
+#ifdef _WIN32
+	return connections.size();
+#endif
+}
---
+++ halite/Environment/networking/Networking.h
@@ -0,0 +1,52 @@
+#ifndef ENVIRONMENT_NETWORKING_H
+#define ENVIRONMENT_NETWORKING_H
+
+#include <iostream>
+#include <set>
+
+#ifdef _WIN32
+	#include <windows.h> 
+	#include <tchar.h>
+	#include <stdio.h> 
+	#include <strsafe.h>
+#else
+	#include <sys/socket.h>
+	#include <arpa/inet.h>
+	#include <time.h>
+	#include <sys/socket.h>
+	#include <netinet/in.h>
+	#include <unistd.h>
+#endif
+
+#include "../Core/hlt.h"
+
+class Networking {
+public:
+	void startAndConnectBot(std::string command);
+	bool handleInitNetworking(unsigned int timeoutMillis, unsigned char playerTag, const hlt::Map & m, std::string & playerName);
+	bool handleFrameNetworking(unsigned int timeoutMillis, unsigned char playerTag, const hlt::Map & m, const std::vector<hlt::Message> &messagesForThisBot, std::set<hlt::Move> * moves, std::vector<hlt::Message> * messagesFromThisBot);
+	void killPlayer(unsigned char playerTag);
+	int numberOfPlayers();
+
+private:
+#ifdef _WIN32
+	struct Connection {
+		HANDLE write;
+		HANDLE read;
+	};
+	std::vector<Connection> connections;
+	std::vector<HANDLE> processes;
+#else
+	std::vector<int> connections;
+#endif
+
+	std::string serializeMap(const hlt::Map & map);
+	std::set<hlt::Move> deserializeMoveSet(std::string & inputString);
+	std::string serializeMessages(const std::vector<hlt::Message> &messages);
+	std::vector<hlt::Message> deserializeMessages(const std::string &inputString);
+
+	void sendString(unsigned char playerTag, std::string &sendString);
+	std::string getString(unsigned char playerTag, unsigned int timoutMillis);
+};
+
+#endif
