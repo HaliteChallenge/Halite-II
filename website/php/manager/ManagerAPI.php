924e9040ea85f26504298877410723c5a6c25dd0
truell20 <truell20@gmail.com>
2015-12-26 21:46:50 -0500

    Manager Compiler Functionality
    Former-commit-id: b3361f6a9151d23133f5ff0aeeb0753a55bc8080 [formerly 149e2137f683b317d98a17590bd1ab37acde448c]
    Former-commit-id: 7acaea75ee3889258836743fd98ab4802cda6a87
    Former-commit-id: f0e39312469b53d75183c177f313600dc0c4d7a8

--- website/php/manager/ManagerAPI.php
+++ website/php/manager/ManagerAPI.php
@@ -10,14 +10,30 @@ class ManagerAPI extends API
 	public function __construct($request, $origin) {
 		$this->initDB();
 
-		sanitizeHTTPParameters();
+		$this->sanitizeHTTPParameters();
 
-		// Validate worker
-		if($_GET['apiKey']) {
-
+		if($this->isValidWorker() == false) {
+			exit(1);
 		}
 
 		parent::__construct($request);
+	}
+
+	private function isValidWorker() {
+		// Get apiKey
+		$apiKey = NULL;
+		if(isset($_GET['apiKey'])) $apiKey = $_GET['apiKey'];
+		else if(isset($_POST['apiKey'])) $apiKey = $_POST['apiKey'];
+		else if(isset($_PUT['apiKey'])) $apiKey = $_PUT['apiKey'];
+		else if(isset($_DELETE['apiKey'])) $apiKey = $_DELETE['apiKey'];
+
+		if($apiKey == NULL) return false;
+
+		// Get ip
+		$ipAddress = $_SERVER['REMOTE_ADDR'];
+
+		if(count($this->select("SELECT ipAddress FROM Worker WHERE ipAddress = '$ipAddress' and apiKey = $apiKey")) > 0) return true;
+		else return false;
 	}
 
 	private function sanitizeHTTPParameters() {
@@ -27,12 +43,25 @@ class ManagerAPI extends API
 		foreach ($_POST as $key => $value) {
 			$_POST[$key] = $this->mysqli->real_escape_string($value);
 		}
-		foreach ($_PUT as $key => $value) {
-			$_PUT[$key] = $this->mysqli->real_escape_string($value);
-		}
-		foreach ($_DELETE as $key => $value) {
-			$_DELETE[$key] = $this->mysqli->real_escape_string($value);
-		}
+	}
+
+	private function botDirectory($userID) {
+		return "../../../storage/bots/$userID";
+	}
+
+	private function botFile($userID) {
+		$botDirectory = $this->botDirectory($userID);
+		$file = NULL;
+		
+		if (file_exists("{$botDirectory}/bot.zip")) $file = "{$botDirectory}/bot.zip";
+		else if (file_exists("{$botDirectory}/bot.tgz")) $file = "{$botDirectory}/bot.tgz";
+		else if (file_exists("{$botDirectory}/bot.tar.gz")) $file = "{$botDirectory}/bot.tar.gz";
+		else if (file_exists("{$botDirectory}/bot.tar.xz")) $file = "{$botDirectory}/bot.tar.xz";
+		else if (file_exists("{$botDirectory}/bot.txz")) $file = "{$botDirectory}/bot.txz";
+		else if (file_exists("{$botDirectory}/bot.tar.bz2")) $file = "{$botDirectory}/bot.tar.bz2";
+		else if (file_exists("{$botDirectory}/bot.tbz")) $file = "{$botDirectory}/bot.tbz";
+
+		return $file;
 	}
 
 	// Initializes and returns a mysqli object that represents our mysql database
@@ -71,8 +100,67 @@ class ManagerAPI extends API
 
 	// API ENDPOINTS
 	protected function task() {
-		if() {
+		if($this->method == 'GET') {
+			// Check for compile tasks
+			$needToBeCompiled = $this->select("SELECT userID FROM User WHERE status = 0 ORDER BY userID ASC");
+			if(count($needToBeCompiled > 0)) {
+				return array(
+					"type" => "compile",
+					"userID" => $needToBeCompiled['userID']);
+			}
 
+			// TODO: Run a game
+
+		}
+		return NULL;
+	}
+
+	protected function bot() {
+		if(isset($_GET['userID'])) {
+			$userID = $_GET['userID'];
+			$botDirectory = $this->botDirectory($userID);
+
+			if (file_exists("{$botDirectory}/bot.zip")) {
+				header("Content-disposition: attachment; filename=bot.zip");
+				header("Content-type: application/zip");
+			} else if (file_exists("{$botDirectory}/bot.tgz")) {
+				header("Content-disposition: attachment; filename=bot.tgz");
+				header("Content-type: application/x-gtar");
+			} else if (file_exists("{$botDirectory}/bot.tar.gz")) {
+				header("Content-disposition: attachment; filename=bot.tgz");
+				header("Content-type: application/x-gtar");
+			} else if (file_exists("{$botDirectory}/bot.tar.xz")) {
+				header("Content-disposition: attachment; filename=bot.txz");
+				header("Content-type: application/x-gtar");
+			} else if (file_exists("{$botDirectory}/bot.txz")) {
+				header("Content-disposition: attachment; filename=bot.txz");
+				header("Content-type: application/x-gtar");
+			} else if (file_exists("{$botDirectory}/bot.tar.bz2")) {
+				header("Content-disposition: attachment; filename=bot.tbz");
+				header("Content-type: application/x-gtar");
+			} else if (file_exists("{$botDirectory}/bot.tbz")) {
+				header("Content-disposition: attachment; filename=bot.tbz");
+				header("Content-type: application/x-gtar");
+			} else {
+				header("HTTP/1.0 404 Not Found");
+				die();
+			}
+
+			ob_clean();
+			flush();
+			readfile($this->botFile($userID));
+			exit;
+		} else {
+			return NULL;
+		}
+
+		return "Success";
+	}
+
+	protected function botHash() {
+		if(isset($_GET['userID'])) {
+			$userID = $_GET['userID'];
+			return md5_file($this->botFile($userID));
 		}
 	}
  }
