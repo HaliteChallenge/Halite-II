842d727dd2162fea9dcd5f0f103da55176f70199
Michael Truell <michael_truell@horacemann.org>
2016-02-14 19:44:41 -0500

    Took out sql folder
    Former-commit-id: 545a42ae54cd5d3eb4f829b913bca424c1adffad [formerly ec12bd75309106ba1b7cdbb087b38444b4ece0cf]
    Former-commit-id: f06a8a47e7bb256640962fe855c89e9fed76af80
    Former-commit-id: 5a9299133a5377e8e00d1fa191b66f551ccdd9c8

--- website/sql/Tables.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `User` (
-  `userID` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  PRIMARY KEY (`userID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=31 ;
