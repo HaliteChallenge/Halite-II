4ef2ba4745017b106ab95fc11f2fa186b676e64c
Michael Truell <michael_truell@horacemann.org>
2016-02-16 12:16:29 -0500

    Turned description into html
    Former-commit-id: d26b8bbfc33901e6deaf14b3a34c6288b5c76f6b [formerly c46ba3c598548b1b875c0c925a7bbce85bba4f96]
    Former-commit-id: 00f796e12cc248f9f055c5545784cd580d0c87f1
    Former-commit-id: 8c909506d0163c102b68a4a7f318a6dcf607061a

--- website/index.php
+++ website/index.php
@@ -78,7 +78,7 @@
 		<div class="pageContent">
 			<div class="jumbotron" id="jumbotron">
 				<h1 id="jHeader">Halite</h1>
-				<p id="jParagraph">Filler filler filler</p>
+				<p id="jParagraph">A multi-player turn-based strategy game played on a rectangular grid</p>
 			</div>
 			<div class="row">
 				<div class="col-sm-5">
@@ -87,11 +87,33 @@
 							<h3 class="panel-title">Problem</h3>
 						</div>
 						<div class="panel-body" id="rulesPanelBody">
-							Here is a description of halite.<br>
-							<code>
-								blah, blah<br>
-								code, code
-							</code>
+							<h3>Overview</h3>
+							
+							<p>Halite is a multi-player turn-based strategy game played on a rectangular grid. The objective of the game is to take over the entire map, in competition with every other player in the game. Players use their territory to gain strength, and their strength to gain territory; they must move intelligently on both the micro and macro scales to play effectively.</p>
+							
+							<h3>Game Description</h3>
+							
+							<p>During a move, every piece you control can be given one of five moves: to move North, East, South, West, or to remain Still. When a piece remains where it is during a turn, two things will happen to it:
+								<ul>
+							 		<li>It will permanently increase its Strength by one.</li>
+									<li>It will receive, for the duration of that turn, a temporary strength boost.</li>
+								</ul>
+							</p>
+
+							<p>Players gain pieces by simply moving their own pieces over Sites on the map. When a piece moves off of a Site, it leaves behind a piece with an identical Owner and with a Strength of 0, in doing so expanding the size of their territory.</p>
+
+							<p>When pieces from opposing players try to occupy either the same or adjacent sites, the battle will be resolved according to the relative strengths of the pieces, as each piece decreases the Strength of every adjacent or coinciding opposing piece by its own Strength. Pieces with a strength of 0 or less are removed from the game, excepting those which have not been in combat during that turn.</p>
+
+							<p>Players should note that the map does wrap around; if a piece at the top of the map moves North, it will reappear on the bottom of the map, and opposing pieces on the top and bottom of the map will engage in combat (provided that they are in the same column).</p>
+
+							<p>Throughout the game, scores are calculated as a sum of all territory that each player has controlled throughout the game. Players which survive until the end of the game receive a 2x bonus to their score. Scores are calculated purely for ranking players in games (i.e. First, Second, Third place...).</p>
+
+							<p>The game ends if one of two conditions are met:
+								<ul>
+							 		<li>Only one player is left.</li>
+							 		<li>A certain number of terms has been reached. This number will vary depending on the size of the map in question; a small map may end after a hundred moves or so, whereas a large map may take up to thousands.</li>
+							 	</ul>
+							 </p>
 						</div>
 					</div>
 				</div>
