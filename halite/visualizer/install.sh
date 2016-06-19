4afefe4a082d092dad434de22203e0289665ab9e
Michael Truell <michael_truell@horacemann.org>
2016-06-19 15:59:02 -0400

    install script unsupported not
    Former-commit-id: 67861dff9f811281c5031b08b582d12fef4a8e25
    Former-commit-id: efdb6b50e0c72aa89bd9efb82f3a2dee6b609b9a
    Former-commit-id: b6dbf4f2bf0828cf494b7a50c3a0f4a77e156363

--- halite/visualizer/install.sh
+++ halite/visualizer/install.sh
@@ -3,4 +3,6 @@ if [ "$(uname)" == "Darwin" ]; then
   brew install homebrew/versions/glfw3 glew freetype pkg-config
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   echo "Linux detected"
+else
+  echo "You are on an unsupported platform. Only Mac and Linux are supported by this install script."
 fi
