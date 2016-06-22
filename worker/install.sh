155a81d1b814d349f2437c699fdb77cbde55edc1
Michael Truell <truell20@gmail.com>
2016-06-22 14:06:51 -0400

    concatenate install files
    Former-commit-id: a87ae4368ad3b06aadf7208e2413a9d3e090a35a
    Former-commit-id: 4a6772a3533b0adf763ef6e5e7b9811b7258f106
    Former-commit-id: 4e69466676e28b40e354119993aa403ac8e5ef27

--- worker/install.sh
+++ worker/install.sh
@@ -1,12 +1,33 @@
+#################
+# Compiler tools
+#################
 apt-get install python3-pip
 apt-get install default-jdk
 apt-get install zip
 apt-get install clang++-3.5
 
+#############
+# Worker
+#############
 pip3 install trueskill
 pip3 install pymysql
 
-cd setup
-sudo chmod +x Install_14.04.sh
-./Install_14.04.sh
+###########################
+# Docker SETUP
+###########################
+apt-get update
+apt-get install docker.io
+ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
 
+echo "Docker Setup complete"
+
+#########
+# Start Docker
+########
+service docker.io restart
+
+echo "Creating Docker Image"
+docker build -t 'virtual_machine' - < Dockerfile
+echo "Retrieving Installed Docker Images"
+docker images
