d79915f47f54bc8d65a7af31c0fd742ebfb2401f
Michael Truell <michael_truell@horacemann.org>
2016-06-23 15:53:15 -0400

    glfw ppa
    Former-commit-id: 5c41a899f7565a3d4b23f7760ebd493c9916ca61
    Former-commit-id: d4cf018c05585fae4cdc463f071742c2fc3a84fd
    Former-commit-id: 1be0096308dde94653f1121251640423b6165ab1

--- halite/visualizer/install.sh
+++ halite/visualizer/install.sh
@@ -3,7 +3,8 @@ if [ "$(uname)" == "Darwin" ]; then
   	brew install homebrew/versions/glfw3 glew freetype pkg-config
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 	echo "Linux detected"
-	sudo apt-get install -qq g++-4.8 libstdc++6-4.7-dev libglfw-dev libfreetype6-dev libglew-dev pkg-config
+	sudo add-apt-repository ppa:keithw/glfw3 -y
+	sudo apt-get install -qq g++-4.8 libstdc++6-4.7-dev libglfw3-dev libfreetype6-dev libglew-dev pkg-config
 else
   echo "You are on an unsupported platform. Only Mac and Linux are supported by this install script."
 fi
