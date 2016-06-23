81aacdefb38c93dadd75b1cf611ca8b72605fb70
Michael Truell <michael_truell@horacemann.org>
2016-06-23 16:05:03 -0400

    Shift ppa adding
    Former-commit-id: b32843f57b8f911fe80e146ba2660a465b74ac3e
    Former-commit-id: 08e97998e94448819a104c989f1c90d86cf795c0
    Former-commit-id: 6693b8ab10933c4d3df0e84ebafc260ca546d9f9

--- halite/visualizer/install.sh
+++ halite/visualizer/install.sh
@@ -3,8 +3,6 @@ if [ "$(uname)" == "Darwin" ]; then
   	brew install homebrew/versions/glfw3 glew freetype pkg-config
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 	echo "Linux detected"
-	sudo add-apt-repository ppa:keithw/glfw3 -y
-	sudo apt-get update
 	sudo apt-get install g++-4.8 libstdc++6-4.7-dev libglfw3-dev libfreetype6-dev libglew-dev pkg-config
 else
   echo "You are on an unsupported platform. Only Mac and Linux are supported by this install script."
