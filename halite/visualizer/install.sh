477843bbf42071d4a681400a645b2a53077cb2a8
Michael Truell <michael_truell@horacemann.org>
2016-06-23 15:35:14 -0400

    Add glew to install script
    Former-commit-id: 4ab95896b3ad55d39affb384e8fee0186dc5e4d9
    Former-commit-id: de218b966a3bd8e4fac2911ffcbd03efeec71026
    Former-commit-id: a162e6b9ace7788cf1adaadca4f745fdf4b5d4c3

--- halite/visualizer/install.sh
+++ halite/visualizer/install.sh
@@ -3,7 +3,7 @@ if [ "$(uname)" == "Darwin" ]; then
   	brew install homebrew/versions/glfw3 glew freetype pkg-config
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 	echo "Linux detected"
-	sudo apt-get install -qq g++-4.8 libstdc++6-4.7-dev libglfw-dev libfreetype6-dev pkg-config
+	sudo apt-get install -qq g++-4.8 libstdc++6-4.7-dev libglfw-dev libfreetype6-dev libglew-dev pkg-config
 else
   echo "You are on an unsupported platform. Only Mac and Linux are supported by this install script."
 fi
