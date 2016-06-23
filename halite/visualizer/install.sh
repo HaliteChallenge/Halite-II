e963b79bfdb7b7a43a1b7b146c3c982edbe1ee22
Michael Truell <michael_truell@horacemann.org>
2016-06-23 14:13:31 -0400

    git ignore binaries
    Former-commit-id: 028917ab07caacf208612b12fcc534a39aa3e8a1
    Former-commit-id: cd2c7bca7dd5520ff732835013e4a57dfbee58e3
    Former-commit-id: e4479696122be744bd5e398f88cc4c4ae4bf731f

--- halite/visualizer/install.sh
+++
@@ -1,8 +0,0 @@
-if [ "$(uname)" == "Darwin" ]; then
-  echo "Mac detected"
-  brew install homebrew/versions/glfw3 glew freetype pkg-config
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  echo "Linux detected"
-else
-  echo "You are on an unsupported platform. Only Mac and Linux are supported by this install script."
-fi
--- halite/visualizer/shaders/graph/fragment.glsl
+++
@@ -1,9 +0,0 @@
-#version 400
-
-in vec3 color;
-out vec4 frag_color;
-
-void main()
-{
-	frag_color = vec4(color, 1.0);
-};
