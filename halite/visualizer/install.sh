282721640022b1551a39fa7a014eb3b9e3fb4043
Michael Truell <michael_truell@horacemann.org>
2016-06-23 15:22:12 -0400

    Add linux install script (without glew)
    Former-commit-id: 21a9d627d714e88a6bcf9a5e836346dc02643e56
    Former-commit-id: aa4dd2b0e7839fcce38ceae2ee73d9148ee372e6
    Former-commit-id: f00207c95abe2a127e9767098bab6eb97cf34701

--- halite/visualizer/install.sh
+++ halite/visualizer/install.sh
@@ -1,8 +1,9 @@
 if [ "$(uname)" == "Darwin" ]; then
-  echo "Mac detected"
-  brew install homebrew/versions/glfw3 glew freetype pkg-config
+  	echo "Mac detected"
+  	brew install homebrew/versions/glfw3 glew freetype pkg-config
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  echo "Linux detected"
+	echo "Linux detected"
+	sudo apt-get install libglfw-dev libfreetype6-dev pkg-config
 else
   echo "You are on an unsupported platform. Only Mac and Linux are supported by this install script."
 fi
