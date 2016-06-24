64b9475d1eaf67d152b94a4b2cfc7f1172f1c7e5
Michael Truell <michael_truell@horacemann.org>
2016-06-24 09:28:05 -0400

    Fix C++ starter indenting
    Former-commit-id: fb0c7f3b9bdd32e28deddf7fd42910f265fc7818
    Former-commit-id: 245ca1874b25bd51959f164c0d706a0b801b914c
    Former-commit-id: 227a24fe17752f5d2b661c24443647e4e205ff95

--- airesources/C++/MyBot.cpp
+++ airesources/C++/MyBot.cpp
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <time.h> 
+#include <time.h>
 #include <cstdlib>
 #include <ctime>
 
@@ -7,58 +7,58 @@
 
 MyBot::MyBot()
 {
-    srand(time(NULL));
-	std::cout.sync_with_stdio(0);
-	
-    getInit(my_tag, present_map);
-	sendInitResponse("CppBot" + std::to_string(my_tag));
-	
-	// FOR DEBUGGING PURPOSES. Clears the test file
-	std::ofstream ofs;
-	ofs.open(std::to_string(my_tag) +".log", std::ofstream::out | std::ofstream::trunc);
-	ofs.close();
+  srand(time(NULL));
+  std::cout.sync_with_stdio(0);
+
+  getInit(my_tag, present_map);
+  sendInitResponse("CppBot" + std::to_string(my_tag));
+
+  // FOR DEBUGGING PURPOSES. Clears the test file
+  std::ofstream ofs;
+  ofs.open(std::to_string(my_tag) +".log", std::ofstream::out | std::ofstream::trunc);
+  ofs.close();
 }
 
 void MyBot::run()
 {
-    while(true)
+  while(true)
+  {
+    moves.clear();
+    messagesFromMe.clear();
+
+    hlt::Message exampleMessage;
+    exampleMessage.type = hlt::MessageType::ATTACK;
+    exampleMessage.senderID = my_tag;
+    exampleMessage.recipientID = my_tag != 1 ? 1 : 2;
+    exampleMessage.targetID = my_tag;
+    messagesFromMe.push_back(exampleMessage);
+
+    getFrame(present_map, messagesToMe);
+
+    for(unsigned short a = 0; a < present_map.map_height; a++)
     {
-        moves.clear();
-		messagesFromMe.clear();
-
-		hlt::Message exampleMessage;
-		exampleMessage.type = hlt::MessageType::ATTACK;
-		exampleMessage.senderID = my_tag;
-		exampleMessage.recipientID = my_tag != 1 ? 1 : 2;
-		exampleMessage.targetID = my_tag;
-		messagesFromMe.push_back(exampleMessage);
-
-        getFrame(present_map, messagesToMe);
-
-		for(unsigned short a = 0; a < present_map.map_height; a++)
-		{
-			for(unsigned short b = 0; b < present_map.map_width; b++)
-			{
-				if (present_map.getSite({b, a}, STILL).owner == my_tag)
-				{
-					if (float(rand()) / RAND_MAX > .20)
-					{
-						moves.insert({ { b, a }, (unsigned char)(rand() % 5) });
-					}
-					else moves.insert({ { b, a }, (unsigned char)(STILL) });
-				}
-			}
-		}
-        sendFrame(moves, messagesFromMe);
+      for(unsigned short b = 0; b < present_map.map_width; b++)
+      {
+        if (present_map.getSite({b, a}, STILL).owner == my_tag)
+        {
+          if (float(rand()) / RAND_MAX > .20)
+          {
+            moves.insert({ { b, a }, (unsigned char)(rand() % 5) });
+          }
+          else moves.insert({ { b, a }, (unsigned char)(STILL) });
+        }
+      }
     }
+    sendFrame(moves, messagesFromMe);
+  }
 }
 
 int main()
 {
-	srand(time(NULL));
+  srand(time(NULL));
 
-	MyBot r = MyBot();
-	r.run();
+  MyBot r = MyBot();
+  r.run();
 
-	return 0;
+  return 0;
 }
