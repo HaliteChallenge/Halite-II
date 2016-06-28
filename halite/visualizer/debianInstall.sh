2dedf8aad46b276583013b6e329110c0e88183f7
Michael Truell <michael_truell@horacemann.org>
2016-06-27 22:48:48 -0400

    Lower case yes's
    Former-commit-id: fb0aab0895215e5352a8ece46f760ec6abed8925
    Former-commit-id: 913729e61a547fc365a935179731772b338393ff
    Former-commit-id: ac254e6e0205b2601182e57bd4fa562161049e05

--- halite/visualizer/debianInstall.sh
+++ halite/visualizer/debianInstall.sh
@@ -6,14 +6,14 @@ sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
 sudo apt-get update -qq
 sudo apt-get install -y cmake
-yes Y | sudo apt-get upgrade -y
+yes | sudo apt-get upgrade -y
 sudo apt-get install -y xorg-dev libglu1-mesa-dev
 sudo apt-get build-dep -y glfw3
 sudo apt-get build-dep -y glfw
 
 sudo apt-get install -y unzip
-yes Y | wget https://github.com/glfw/glfw/releases/download/3.2/glfw-3.2.zip
-yes Y | unzip glfw-3.2.zip -d glfw
+yes | wget https://github.com/glfw/glfw/releases/download/3.2/glfw-3.2.zip
+yes | unzip glfw-3.2.zip -d glfw
 cd glfw/glfw-3.2
 cmake -G "Unix Makefiles"
 sudo make
