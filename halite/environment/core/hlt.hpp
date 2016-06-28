703b60d355b7e5744ec5ccac9af870c5203cd42f
Michael Truell <michael_truell@horacemann.org>
2016-06-28 09:33:24 -0400

    Fix indenting
    Former-commit-id: 8b655b40ab99cae5fba6af39592c22c04078074f
    Former-commit-id: f0a59b3402c9fdc74df5f08d99cee3a2d1d24e7f
    Former-commit-id: 2d9d125a677c87f29c5e661479c6151276d76bdf

--- halite/environment/core/hlt.hpp
+++ halite/environment/core/hlt.hpp
@@ -19,180 +19,180 @@ struct Color
 
 namespace hlt
 {
-enum MessageType {ATTACK, STOP_ATTACK};
-struct Message {
-	MessageType type;
-	int senderID, recipientID, targetID;
-};
-struct Location
-{
-	unsigned short x, y;
-};
-static bool operator<(const Location & l1, const Location & l2)
-{
-	return ((l1.x + l1.y)*((unsigned int)l1.x + l1.y + 1) / 2) + l1.y < ((l2.x + l2.y)*((unsigned int)l2.x + l2.y + 1) / 2) + l2.y;
-}
-static bool operator==(const Location & l1, const Location & l2)
-{
-	return l1.x == l2.x && l1.y == l2.y;
-}
-
-struct Site
-{
-	unsigned char owner;
-	unsigned char strength;
-	unsigned char production;
-};
-
-class Map
-{
-public:
-	std::vector< std::vector<Site> > contents;
-	unsigned short map_width, map_height; //Number of rows and columns, NOT maximum index.
-
-	Map()
+	enum MessageType {ATTACK, STOP_ATTACK};
+	struct Message {
+		MessageType type;
+		int senderID, recipientID, targetID;
+	};
+	struct Location
 	{
-		map_width = 0;
-		map_height = 0;
-		contents = std::vector< std::vector<Site> >(map_height, std::vector<Site>(map_width, { 0, 0 }));
+		unsigned short x, y;
+	};
+	static bool operator<(const Location & l1, const Location & l2)
+	{
+		return ((l1.x + l1.y)*((unsigned int)l1.x + l1.y + 1) / 2) + l1.y < ((l2.x + l2.y)*((unsigned int)l2.x + l2.y + 1) / 2) + l2.y;
 	}
-	Map(const Map &otherMap)
+	static bool operator==(const Location & l1, const Location & l2)
 	{
-		map_width = otherMap.map_width;
-		map_height = otherMap.map_height;
-		contents = otherMap.contents;
+		return l1.x == l2.x && l1.y == l2.y;
 	}
-	Map(short width, short height, unsigned char numberOfPlayers)
+
+	struct Site
 	{
-		//Find number closest to square that makes the match symmetric.
-		int dw = sqrt(numberOfPlayers);
-		while(numberOfPlayers % dw != 0) dw--;
-		int dh = numberOfPlayers / dw;
+		unsigned char owner;
+		unsigned char strength;
+		unsigned char production;
+	};
 
-		//Figure out chunk width and height accordingly.
-		//Matches width and height as closely as it can, but is not guaranteed to match exactly.
-		//It is guaranteed to be smaller if not the same size, however.
-		int cw = width / dw;
-		int ch = height / dh;
-		//We're temporarily setting the map width and height to be that of the chunks,
-		//which lets us use the getLocation function with the chunks.
-		//We'll set it again later.
-		map_width = cw;
-		map_height = ch;
+	class Map
+	{
+	public:
+		std::vector< std::vector<Site> > contents;
+		unsigned short map_width, map_height; //Number of rows and columns, NOT maximum index.
 
-		//Pseudorandom number generator.
-		std::mt19937 prg(std::chrono::system_clock::now().time_since_epoch().count());
-		std::uniform_real_distribution<double> urd(0.0, 1.0);
-		//Generate the chunk with random values:
-		std::vector< std::vector<float> > chunk(ch, std::vector<float>(cw));
-		for(int a = 0; a < ch; a++) {
-	    for(int b = 0; b < cw; b++) {
-	      float d = urd(prg);
-	      d = pow(d, 5) * 32;
-	      chunk[a][b] = d;
-	    }
-	  }
-		//Iterate over the map 6 times (found by experiment) to produce the chunk
-		for(int a = 0; a < 6; a++) {
-			std::vector< std::vector<float> > newChunk = std::vector< std::vector<float> >(ch, std::vector<float>(cw, 0));
-			const double OWN_WEIGHT = 0.667;
-			for(unsigned short y = 0; y < ch; y++) {
-		    for(unsigned short x = 0; x < cw; x++) {
-		      Location l = { x, y };
-		      Location n = getLocation(l, NORTH), e = getLocation(l, EAST), s = getLocation(l, SOUTH), w = getLocation(l, WEST);
-		      newChunk[l.y][l.x] = OWN_WEIGHT * chunk[l.y][l.x];
-		      newChunk[l.y][l.x] += (1 - OWN_WEIGHT) * chunk[n.y][n.x] / 4;
-		      newChunk[l.y][l.x] += (1 - OWN_WEIGHT) * chunk[e.y][e.x] / 4;
-		      newChunk[l.y][l.x] += (1 - OWN_WEIGHT) * chunk[s.y][s.x] / 4;
-		      newChunk[l.y][l.x] += (1 - OWN_WEIGHT) * chunk[w.y][w.x] / 4;
-		      newChunk[l.y][l.x] -= 1.5 * urd(prg);
-					if(newChunk[l.y][l.x] < 0) newChunk[l.y][l.x] = 1;
-		    }
-		  }
-		  chunk = newChunk;
+		Map()
+		{
+			map_width = 0;
+			map_height = 0;
+			contents = std::vector< std::vector<Site> >(map_height, std::vector<Site>(map_width, { 0, 0 }));
 		}
+		Map(const Map &otherMap)
+		{
+			map_width = otherMap.map_width;
+			map_height = otherMap.map_height;
+			contents = otherMap.contents;
+		}
+		Map(short width, short height, unsigned char numberOfPlayers)
+		{
+			//Find number closest to square that makes the match symmetric.
+			int dw = sqrt(numberOfPlayers);
+			while(numberOfPlayers % dw != 0) dw--;
+			int dh = numberOfPlayers / dw;
 
-		map_width = cw * dw;
-		map_height = ch * dh;
-		contents = std::vector< std::vector<Site> >(map_height, std::vector<Site>(map_width, { 0, 0, 0 }));
+			//Figure out chunk width and height accordingly.
+			//Matches width and height as closely as it can, but is not guaranteed to match exactly.
+			//It is guaranteed to be smaller if not the same size, however.
+			int cw = width / dw;
+			int ch = height / dh;
+			//We're temporarily setting the map width and height to be that of the chunks,
+			//which lets us use the getLocation function with the chunks.
+			//We'll set it again later.
+			map_width = cw;
+			map_height = ch;
 
-		//Fill in with chunks.
-		for(int a = 0; a < dh; a++) {
-			for(int b = 0; b < dw; b++) {
-				for(int c = 0; c < ch; c++) {
-					for(int d = 0; d < cw; d++) {
-						contents[a * ch + c][b * cw + d].production = round(chunk[c][d]); //Set production values.
-						contents[a * ch + c][b * cw + d].strength = 8 * ((urd(prg) / 2) + 0.75) * round(chunk[c][d]);
+			//Pseudorandom number generator.
+			std::mt19937 prg(std::chrono::system_clock::now().time_since_epoch().count());
+			std::uniform_real_distribution<double> urd(0.0, 1.0);
+			//Generate the chunk with random values:
+			std::vector< std::vector<float> > chunk(ch, std::vector<float>(cw));
+			for(int a = 0; a < ch; a++) {
+				for(int b = 0; b < cw; b++) {
+					float d = urd(prg);
+					d = pow(d, 5) * 32;
+					chunk[a][b] = d;
+				}
+			}
+			//Iterate over the map 6 times (found by experiment) to produce the chunk
+			for(int a = 0; a < 6; a++) {
+				std::vector< std::vector<float> > newChunk = std::vector< std::vector<float> >(ch, std::vector<float>(cw, 0));
+				const double OWN_WEIGHT = 0.667;
+				for(unsigned short y = 0; y < ch; y++) {
+					for(unsigned short x = 0; x < cw; x++) {
+						Location l = { x, y };
+						Location n = getLocation(l, NORTH), e = getLocation(l, EAST), s = getLocation(l, SOUTH), w = getLocation(l, WEST);
+						newChunk[l.y][l.x] = OWN_WEIGHT * chunk[l.y][l.x];
+						newChunk[l.y][l.x] += (1 - OWN_WEIGHT) * chunk[n.y][n.x] / 4;
+						newChunk[l.y][l.x] += (1 - OWN_WEIGHT) * chunk[e.y][e.x] / 4;
+						newChunk[l.y][l.x] += (1 - OWN_WEIGHT) * chunk[s.y][s.x] / 4;
+						newChunk[l.y][l.x] += (1 - OWN_WEIGHT) * chunk[w.y][w.x] / 4;
+						newChunk[l.y][l.x] -= 1.5 * urd(prg);
+						if(newChunk[l.y][l.x] < 0) newChunk[l.y][l.x] = 1;
 					}
 				}
-				contents[a * ch + ch / 2][b * cw + cw / 2].owner = a * dw + b + 1; //Set owners.
-				contents[a * ch + ch / 2][b * cw + cw / 2].strength = 255; //Set strengths
+				chunk = newChunk;
+			}
+
+			map_width = cw * dw;
+			map_height = ch * dh;
+			contents = std::vector< std::vector<Site> >(map_height, std::vector<Site>(map_width, { 0, 0, 0 }));
+
+			//Fill in with chunks.
+			for(int a = 0; a < dh; a++) {
+				for(int b = 0; b < dw; b++) {
+					for(int c = 0; c < ch; c++) {
+						for(int d = 0; d < cw; d++) {
+							contents[a * ch + c][b * cw + d].production = round(chunk[c][d]); //Set production values.
+							contents[a * ch + c][b * cw + d].strength = 8 * ((urd(prg) / 2) + 0.75) * round(chunk[c][d]);
+						}
+					}
+					contents[a * ch + ch / 2][b * cw + cw / 2].owner = a * dw + b + 1; //Set owners.
+					contents[a * ch + ch / 2][b * cw + cw / 2].strength = 255; //Set strengths
+				}
 			}
 		}
-	}
 
-	bool inBounds(Location l)
-	{
-		return l.x < map_width && l.y < map_height;
-	}
-	float getDistance(Location l1, Location l2)
-	{
-		short dx = abs(l1.x - l2.x), dy = abs(l1.y - l2.y);
-		if (dx > map_width / 2) dx = map_width - dx;
-		if (dy > map_height / 2) dy = map_height - dy;
-		return sqrt((dx*dx) + (dy*dy));
-	}
-	float getAngle(Location l1, Location l2)
-	{
-		short dx = l2.x - l1.x, dy = l2.y - l1.y;
-		if (dx > map_width - dx) dx -= map_width;
-		else if (-dx > map_width + dx) dx += map_width;
-		if (dy > map_height - dy) dy -= map_height;
-		else if (-dy > map_height + dy) dy += map_height;
-		return atan2(dy, dx);
-	}
-
-	Location getLocation(Location l, unsigned char direction)
-	{
-		if(direction != STILL)
+		bool inBounds(Location l)
 		{
-			if(direction == NORTH)
-			{
-				if(l.y == 0) l.y = map_height - 1;
-				else l.y--;
-			}
-			else if(direction == EAST)
-			{
-				if(l.x == map_width - 1) l.x = 0;
-				else l.x++;
-			}
-			else if(direction == SOUTH)
-			{
-				if(l.y == map_height - 1) l.y = 0;
-				else l.y++;
-			}
-			else if(direction == WEST)
-			{
-				if(l.x == 0) l.x = map_width - 1;
-				else l.x--;
-			}
+			return l.x < map_width && l.y < map_height;
 		}
-		return l;
-	}
-	Site& getSite(Location l, unsigned char direction)
-	{
-		l = getLocation(l, direction);
-		return contents[l.y][l.x];
-	}
-};
+		float getDistance(Location l1, Location l2)
+		{
+			short dx = abs(l1.x - l2.x), dy = abs(l1.y - l2.y);
+			if (dx > map_width / 2) dx = map_width - dx;
+			if (dy > map_height / 2) dy = map_height - dy;
+			return sqrt((dx*dx) + (dy*dy));
+		}
+		float getAngle(Location l1, Location l2)
+		{
+			short dx = l2.x - l1.x, dy = l2.y - l1.y;
+			if (dx > map_width - dx) dx -= map_width;
+			else if (-dx > map_width + dx) dx += map_width;
+			if (dy > map_height - dy) dy -= map_height;
+			else if (-dy > map_height + dy) dy += map_height;
+			return atan2(dy, dx);
+		}
 
-struct Move
-{
-	Location loc; unsigned char dir;
-};
-static bool operator<(const Move& m1, const Move& m2)
-{
-	unsigned int l1Prod = ((m1.loc.x + m1.loc.y)*((unsigned int)m1.loc.x + m1.loc.y + 1) / 2) + m1.loc.y, l2Prod = ((m2.loc.x + m2.loc.y)*((unsigned int)m2.loc.x + m2.loc.y + 1) / 2) + m2.loc.y;
-	return ((l1Prod + m1.dir)*(l1Prod + m1.dir + 1) / 2) + m1.dir < ((l2Prod + m2.dir)*(l2Prod + m2.dir + 1) / 2) + m2.dir;
-}
+		Location getLocation(Location l, unsigned char direction)
+		{
+			if(direction != STILL)
+			{
+				if(direction == NORTH)
+				{
+					if(l.y == 0) l.y = map_height - 1;
+					else l.y--;
+				}
+				else if(direction == EAST)
+				{
+					if(l.x == map_width - 1) l.x = 0;
+					else l.x++;
+				}
+				else if(direction == SOUTH)
+				{
+					if(l.y == map_height - 1) l.y = 0;
+					else l.y++;
+				}
+				else if(direction == WEST)
+				{
+					if(l.x == 0) l.x = map_width - 1;
+					else l.x--;
+				}
+			}
+			return l;
+		}
+		Site& getSite(Location l, unsigned char direction)
+		{
+			l = getLocation(l, direction);
+			return contents[l.y][l.x];
+		}
+	};
+
+	struct Move
+	{
+		Location loc; unsigned char dir;
+	};
+	static bool operator<(const Move& m1, const Move& m2)
+	{
+		unsigned int l1Prod = ((m1.loc.x + m1.loc.y)*((unsigned int)m1.loc.x + m1.loc.y + 1) / 2) + m1.loc.y, l2Prod = ((m2.loc.x + m2.loc.y)*((unsigned int)m2.loc.x + m2.loc.y + 1) / 2) + m2.loc.y;
+		return ((l1Prod + m1.dir)*(l1Prod + m1.dir + 1) / 2) + m1.dir < ((l2Prod + m2.dir)*(l2Prod + m2.dir + 1) / 2) + m2.dir;
+	}
 }
--- halite/visualizer/core/Halite.hpp
+++ halite/visualizer/core/Halite.hpp
@@ -16,56 +16,56 @@
 class Halite
 {
 private:
-    std::vector< std::pair<std::string, float> > player_names; //float represents rendering position
-	std::vector< std::vector<int> * > player_scores;
-	std::vector<hlt::Map * > full_game;
-	std::string present_file;
-	std::map<unsigned char, Color> color_codes;
-	unsigned short number_of_players;
-	float defense_bonus;
+  std::vector< std::pair<std::string, float> > player_names; //float represents rendering position
+  std::vector< std::vector<int> * > player_scores;
+  std::vector<hlt::Map * > full_game;
+  std::string present_file;
+  std::map<unsigned char, Color> color_codes;
+  unsigned short number_of_players;
+  float defense_bonus;
 
-	//Rendering values to remember generally
-	signed short x_offset, y_offset, map_width, map_height;
+  //Rendering values to remember generally
+  signed short x_offset, y_offset, map_width, map_height;
 
-	//Map rendering
-	GLuint map_vertex_buffer, map_color_buffer, map_strength_buffer, map_vertex_attributes, map_vertex_shader, map_geometry_shader, map_fragment_shader, map_shader_program;
+  //Map rendering
+  GLuint map_vertex_buffer, map_color_buffer, map_strength_buffer, map_vertex_attributes, map_vertex_shader, map_geometry_shader, map_fragment_shader, map_shader_program;
 
-	//Production rendering
-	GLuint production_vertex_buffer, production_color_buffer, production_vertex_attributes, production_vertex_shader, production_geometry_shader, production_fragment_shader, production_shader_program;
+  //Production rendering
+  GLuint production_vertex_buffer, production_color_buffer, production_vertex_attributes, production_vertex_shader, production_geometry_shader, production_fragment_shader, production_shader_program;
 
-	//Graph rendering
-	GLuint graph_territory_vertex_buffer, graph_strength_vertex_buffer, graph_color_buffer, graph_territory_vertex_attributes, graph_strength_vertex_attributes, graph_vertex_shader, graph_fragment_shader, graph_shader_program;
-	//Stats about the graph. This lets us know if we need to redo the setup for the graph.
-	unsigned short graph_frame_number, graph_turn_number, graph_turn_min, graph_turn_max;
-	float graph_zoom;
-	unsigned int graph_max_territory, graph_max_strength;
-	float territory_graph_top, territory_graph_bottom, territory_graph_left, territory_graph_right;
-	float strength_graph_top, strength_graph_bottom, strength_graph_left, strength_graph_right;
+  //Graph rendering
+  GLuint graph_territory_vertex_buffer, graph_strength_vertex_buffer, graph_color_buffer, graph_territory_vertex_attributes, graph_strength_vertex_attributes, graph_vertex_shader, graph_fragment_shader, graph_shader_program;
+  //Stats about the graph. This lets us know if we need to redo the setup for the graph.
+  unsigned short graph_frame_number, graph_turn_number, graph_turn_min, graph_turn_max;
+  float graph_zoom;
+  unsigned int graph_max_territory, graph_max_strength;
+  float territory_graph_top, territory_graph_bottom, territory_graph_left, territory_graph_right;
+  float strength_graph_top, strength_graph_bottom, strength_graph_left, strength_graph_right;
 
-	//Statistics constants:
-	const float STAT_LEFT, STAT_RIGHT, STAT_BOTTOM, STAT_TOP, NAME_TEXT_HEIGHT, NAME_TEXT_OFFSET, GRAPH_TEXT_HEIGHT, GRAPH_TEXT_OFFSET, LABEL_TEXT_HEIGHT, LABEL_TEXT_OFFSET, MAP_TEXT_HEIGHT, MAP_TEXT_OFFSET;
+  //Statistics constants:
+  const float STAT_LEFT, STAT_RIGHT, STAT_BOTTOM, STAT_TOP, NAME_TEXT_HEIGHT, NAME_TEXT_OFFSET, GRAPH_TEXT_HEIGHT, GRAPH_TEXT_OFFSET, LABEL_TEXT_HEIGHT, LABEL_TEXT_OFFSET, MAP_TEXT_HEIGHT, MAP_TEXT_OFFSET;
 
-	//Border rendering
-	GLuint border_vertex_buffer, border_vertex_attributes, border_vertex_shader, border_fragment_shader, border_shader_program;
+  //Border rendering
+  GLuint border_vertex_buffer, border_vertex_attributes, border_vertex_shader, border_fragment_shader, border_shader_program;
 
-	//void loadColorCodes(std::string s);
-	void setOffset(signed short xOffset, signed short yOffset);
-	void setupMapGL();
-	void setupMapRendering();
-	void setupProductionGL();
-	void setupProductionRendering(const hlt::Map & map);
-	void setupGraphGL();
-	void setupGraphRendering(float zoom, short turnNumber);
-	void setupBorders();
-	void clearFullGame();
+  //void loadColorCodes(std::string s);
+  void setOffset(signed short xOffset, signed short yOffset);
+  void setupMapGL();
+  void setupMapRendering();
+  void setupProductionGL();
+  void setupProductionRendering(const hlt::Map & map);
+  void setupGraphGL();
+  void setupGraphRendering(float zoom, short turnNumber);
+  void setupBorders();
+  void clearFullGame();
 public:
-    Halite();
-	short input(GLFWwindow * window, std::string filename, unsigned short& width, unsigned short& height);
-	bool isValid(std::string filename);
-	void render(GLFWwindow * window, short& turnNumber, float zoom, float mouseX, float mouseY, bool tab, bool mousePress, short xOffset, short yOffset);
-	std::map<unsigned char, Color> getColorCodes();
-	void recreateGL();
-	~Halite();
+  Halite();
+  short input(GLFWwindow * window, std::string filename, unsigned short& width, unsigned short& height);
+  bool isValid(std::string filename);
+  void render(GLFWwindow * window, short& turnNumber, float zoom, float mouseX, float mouseY, bool tab, bool mousePress, short xOffset, short yOffset);
+  std::map<unsigned char, Color> getColorCodes();
+  void recreateGL();
+  ~Halite();
 };
 
 #endif
