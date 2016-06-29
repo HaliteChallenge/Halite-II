d0e289cec87e40ef2d8e0893a186f6a06873cbdc
Michael Truell <michael_truell@horacemann.org>
2016-06-29 15:04:45 -0400

    Small typo
    Former-commit-id: 4d0f3927a15544c1a2c43ca0b6dc150b0c173055
    Former-commit-id: 838bf8df625d4d504c81826fd61a758ae29fbb40
    Former-commit-id: 34f6b52466335fbb77f69b03cda0d48e07d619cc

--- website/user.php
+++ website/user.php
@@ -54,6 +54,7 @@
 	</div>
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 	<script src="script/backend.js"></script>
 	<script src="script/general.js"></script>
 	<script src="script/user.js"></script>
