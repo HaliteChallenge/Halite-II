42403a9d925bd498b31a11910fe2723b50282048
Michael Truell <michael_truell@horacemann.org>
2016-06-29 16:51:25 -0400

    Suppress docker remove output
    Former-commit-id: 5f47df4971061f937d0535330b5d27220024de09
    Former-commit-id: 8bf274c4a82cb7ccdbc421a3db54a603a2a82440
    Former-commit-id: 93882ef18df2428d00b188c99feb64f187b52caa

--- worker/runGame.sh
+++ worker/runGame.sh
@@ -28,7 +28,7 @@ chmod +x "$BOT2/$RUNFILE"
 
 docker run -v $PWD:$PWD virtual_machine sh -c "cd $PWD && chmod +x $ENVIRONMENT && ./$ENVIRONMENT -q $WIDTH $HEIGHT 'cd $PWD/$BOT1 && ./$RUNFILE' 'cd $PWD/$BOT2 && ./$RUNFILE'"
 
-docker rm $(docker ps -aq)
+docker rm $(docker ps -aq) >/dev/null
 
 mv *.hlt ../
 cd ..
