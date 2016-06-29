a89da1db55eb785291062e22d508e9e9f9d3c4bb
Michael Truell <michael_truell@horacemann.org>
2016-06-29 10:44:40 -0400

    Start on user page
    Former-commit-id: 7d60b1f25ca99e07606c70ec389344ae4cc18a67
    Former-commit-id: 00237c5eb4e699394e6028f1ab2be452b73f98b4
    Former-commit-id: d8e49a20239f6fcede712b2be01389fe3e758843

---
+++ website/user.php
@@ -0,0 +1,34 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Halite Rules</title>
+
+	<link href="lib/bootstrap.min.css" rel="stylesheet">
+	<link href="style/index.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<?php include 'includes/navbar.php'; ?>
+		<div class="row">
+			<div class="col-md-5">
+				<div class="row">
+					<div class="col-md-12">
+						User
+						User
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						Stats
+					</div>
+				</div>
+			</div>
+			<div class="col-md-7">
+				Games
+			</div>
+		</div>
+	</div>	
+</body>
+</html>
