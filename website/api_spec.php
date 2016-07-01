78f2909251905deb739f36dbc56fd17d58128dad
Benjamin Spector <benjamin_spector@horacemann.org>
2016-07-01 11:44:58 -0400

    Added API spec
    Former-commit-id: bca74e902f038fa3b6d7324cf0284d323fd71beb
    Former-commit-id: c092e2fd82ede4c6eb429e5fd58d9c459db0bde5
    Former-commit-id: 5539302ac4a81c279a30c0a03235076cfd06bedb

---
+++ website/api_spec.php
@@ -0,0 +1,40 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Halite Rules</title>
+
+	<link href="lib/bootstrap.min.css" rel="stylesheet">
+	<link href="style/index.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<?php include 'includes/navbar.php'; ?>
+		<div class="row">
+			<div class="col-sm-12">
+
+				<h3>Overview</h3>
+
+				
+
+			</div>
+		</div>
+	</div>
+
+	<footer class="footer pageContent">
+		<div class="container">
+			<div id="footer">
+				<ul class="pager">
+					<li class="previous"><a href="tutorial.php"><span aria-hidden="true">&larr;</span> Tutorial</a></li>
+				</ul>
+			</div>
+		</div>
+	</footer>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<script src="script/backend.js"></script>
+	<script src="script/general.js"></script>
+</body>
+</html>
