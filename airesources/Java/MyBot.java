33d3316548a60920cee13fe60733396e585cd1de
Michael Truell <truell20@gmail.com>
2016-07-05 17:04:29 -0400

    Fix java indenting
    Former-commit-id: 836e7df7a0767c858a1d15ff39f608f38dc747e8
    Former-commit-id: cd78876478af72340f16942be75c996b42a30ffb
    Former-commit-id: 6a0dbccce83cfbeb103256cb2692d5ada853e567

--- airesources/Java/MyBot.java
+++ airesources/Java/MyBot.java
@@ -2,29 +2,29 @@ import java.net.*;
 import java.util.ArrayList;
 
 public class MyBot{
-    public static void main(String[] args) {
-        InitPackage iPackage = Networking.getInit();
-        short playerTag = iPackage.playerTag;
-        Map gameMap = iPackage.map;
+  public static void main(String[] args) {
+    InitPackage iPackage = Networking.getInit();
+    short playerTag = iPackage.playerTag;
+    Map gameMap = iPackage.map;
 
-        Networking.sendInit("JavaBot" + playerTag);
+    Networking.sendInit("JavaBot" + playerTag);
 
-        while(true) {
-            ArrayList<Move> moves = new ArrayList<Move>();
-            
-            gameMap = Networking.getFrame();
-            
-            for(int y = 0; y < gameMap.contents.size(); y++) {
-                for(int x = 0; x < gameMap.contents.get(y).size(); x++) {
-                    Site site = gameMap.contents.get(y).get(x);
-                    if(site.owner == playerTag) {
-                        byte dir = Direction.randomDirection();
-                        moves.add(new Move(new Location((short)x, (short)y), dir));
-                    }
-                }
-            }
-            
-            Networking.sendFrame(moves);
+    while(true) {
+      ArrayList<Move> moves = new ArrayList<Move>();
+
+      gameMap = Networking.getFrame();
+
+      for(int y = 0; y < gameMap.contents.size(); y++) {
+        for(int x = 0; x < gameMap.contents.get(y).size(); x++) {
+          Site site = gameMap.contents.get(y).get(x);
+          if(site.owner == playerTag) {
+            byte dir = Direction.randomDirection();
+            moves.add(new Move(new Location((short)x, (short)y), dir));
+          }
         }
+      }
+
+      Networking.sendFrame(moves);
     }
+  }
 }
