b7283073b453449466c14e33b9fdccde60836aec
Benjamin Spector <benjamin_spector@horacemann.org>
2016-07-05 13:21:15 -0400

    Updated title of sp spec
    Former-commit-id: 8062f083b803a832e2beb97e84c6035f350adb52
    Former-commit-id: 4d243bcad9e4262d771ecff892be12bbddc0d5aa
    Former-commit-id: 07d991346a8edf485cf580f9c018752625a82e2a

--- website/sp_spec.php
+++ website/sp_spec.php
@@ -3,7 +3,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>API Spec</title>
+	<title>SP Spec</title>
 
 	<link href="lib/bootstrap.min.css" rel="stylesheet">
 	<link href="style/index.css" rel="stylesheet">
