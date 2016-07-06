8a1459de3cc2e1878496e31d86ecdebdd2dfe09c
Michael Truell <michael_truell@horacemann.org>
2016-07-06 10:43:19 -0400

    Fix faulty win assignement
    Former-commit-id: e9d3fc9a0b2863ef1cd362af653f4a6c0d6e3b38
    Former-commit-id: 0f55deb6e03cda815ab702c5e0e1008698984174
    Former-commit-id: 006049688ac610d947dccb3781d2990ebb080765

--- website/php/WebsiteAPI.php
+++ website/php/WebsiteAPI.php
@@ -170,7 +170,7 @@ class WebsiteAPI extends API{
 					unset($gameUserRow['gameID']);
 
 					// Add in user info
-					$userInfo = $this->select("SELECT * FROM User WHERE userID = {$gameUserRow['userID']}");
+					$userInfo = $this->select("SELECT username FROM User WHERE userID = {$gameUserRow['userID']}");
 					foreach($userInfo as $key => $value) $gameUserRow[$key] = $value;
 				}
 				array_push($gameArrays, $gameArray);
