00e6eb2953207b8ff0d6df6654a8920bb2296562
Michael Truell <truell20@gmail.com>
2016-07-07 15:30:25 -0400

    Change naming scheme of downloads
    Former-commit-id: c3b1bc5580660e58e8e6e5bbada15e8c93a30a3d
    Former-commit-id: 85b6b9dbbe7a6066b793e4f81c328defeb7b1204
    Former-commit-id: f9db4a499725f9066cc7f6a9165a2780ac68daab

--- website/downloads/archiveStarterPackages.sh
+++ website/downloads/archiveStarterPackages.sh
@@ -1,15 +1,19 @@
-rm Python.zip Java.zip C++.zip
+rm *-Starter-Package.zip
 
 cd ../../airesources/
 
-rm __pycache__
-zip -r Python.zip Python
+rm -r *-Starter-Package
 
-zip -r C++.zip C++
+mkdir Halite-Python-Starter-Package Halite-Java-Starter-Package Halite-C++-Starter-Package
 
-zip -r Java.zip Java
+cp Python/* Halite-Python-Starter-Package/
+cp Java/* Halite-Java-Starter-Package/
+cp C++/* Halite-C++-Starter-Package/
 
-mv Java.zip ../website/downloads
-mv Python.zip ../website/downloads
-mv C++.zip ../website/downloads
+zip -r Halite-Python-Starter-Package.zip Halite-Python-Starter-Package/ 
+zip -r Halite-C++-Starter-Package.zip Halite-C++-Starter-Package/
+zip -r Halite-Java-Starter-Package.zip Halite-Java-Starter-Package/ 
 
+mv *.zip ../website/downloads
+
+rm -r *-Starter-Package
