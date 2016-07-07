6ec0b8a64cc38415b931079b1fc1c6a53c469ad1
Michael Truell <truell20@gmail.com>
2016-07-07 15:41:06 -0400

    Update package script to reflect naming scheme
    Former-commit-id: 7056ebc95ae19cfaa5faf76c02105a164044efa2
    Former-commit-id: 06d95dd9287ccf7a1d30a4a24b06ced796b2b63e
    Former-commit-id: 90f60647e45c03439ca44e1ee9b638fa986ba757

--- website/downloads/repackageUnixBinary.sh
+++ website/downloads/repackageUnixBinary.sh
@@ -1,20 +1,26 @@
 if [ $# -eq 0 ]; then
 	echo "You need to supply the name of your zip as an arguement to this script."
 else
-	ZIP="$1.zip"
+	ZIP="Halite-$1.zip"
 
-	cd ../../halite/environment
+	cd ../../halite
+	mkdir Halite-$1
+
+	cd environment
 	make clean
 	make
-	zip $ZIP environment
-	mv $ZIP ../
+	cp environment ../Halite-$1
 
 	cd ../visualizer
 	make clean
 	make
-	zip -ur ../$ZIP fonts shaders visualizer
+	cp -r fonts shaders visualizer ../Halite-$1
 
-	cd ../../website/downloads
+	cd ../
+	zip -r $ZIP Halite-$1
+	rm -r Halite-$1	
+
+	cd ../website/downloads
 	rm $ZIP 
 	mv ../../halite/$ZIP ./
 
