3765189f4619a6002be071577c274610b2fa2116
Michael Truell <truell20@gmail.com>
2016-07-11 11:36:11 -0400

    Clean up trueskill updating in php
    Former-commit-id: a081d999f72b0ea1a771b8c0ad3c73f969e561ca
    Former-commit-id: 7f64429b555f667d29b5709900a634c5bb261be9
    Former-commit-id: 5213cb1819602691a14b1becb3a46dbe2242dd03

--- manager/ManagerAPI.php
+++ manager/ManagerAPI.php
@@ -64,6 +64,24 @@ class ManagerAPI extends API{
 		return floatval($lines[0]);
 	}
 
+	private function updateRankings($rankingValues) {
+		usort($rankingValues, function($a, $b) {
+			return $a['rank'] < $b['rank'];
+		});
+		$rankings = array();
+		foreach($rankingValues as $user) {
+			array_push($rankings, $user['mu']);
+			array_push($rankings, $user['sigma']);
+		}
+		exec("python3 updateTrueskill.py ".implode(' ', $rankings), $lines);
+		for($a = 0; $a < count($rankingValues); $a++) {
+			$components = explode(' ', $lines[$a]);
+			$rankingValues[$a]['mu'] = $components[0];
+			$rankingValues[$a]['sigma'] = $components[1];
+		}
+		return $rankingValues;
+	}
+
 	// Initializes and returns a mysqli object that represents our mysql database
 	private function initDB() {
 		$config = parse_ini_file(INI_FILE, true);
@@ -269,21 +287,9 @@ class ManagerAPI extends API{
 				$this->insert("UPDATE User SET numGames=numGames+1, mu = {$users[$a]->mu}, sigma = {$users[$a]->sigma} WHERE userID = {$users[$a]->userID}");
 			}
 			// Update mu and sigma
-			usort($allUsers, function($a, $b) {
-				return $a['rank'] < $b['rank'];
-			});
-			$rankings = array();
+			$allUsers = $this->updateRankings($allUsers);
 			foreach($allUsers as $user) {
-				array_push($rankings, $user['mu']);
-				array_push($rankings, $user['sigma']);
-			}
-			exec("python3 updateTrueskill.py ".implode(' ', $rankings), $lines);
-			var_dump($lines);
-			for($a = 0; $a < count ($allUsers); $a++) {
-				$components = explode(' ', $lines[$a]);
-				$allUsers[$a]['mu'] = $components[0];
-				$allUsers[$a]['sigma'] = $components[1];
-				$this->insert("UPDATE User SET mu={$allUsers[$a]['mu']}, sigma={$allUsers[$a]['sigma']} WHERE userID={$allUsers[$a]['userID']}");
+				$this->insert("UPDATE User SET mu={$user['mu']}, sigma={$user['sigma']} WHERE userID={$user['userID']}");
 			}
 
 			// Update overall rank
--- manager/updateTruskill.py
+++ manager/updateTruskill.py
@@ -3,7 +3,7 @@ import trueskill
 
 sys.argv.pop(0)
 
-teams = [[trueskill.Rating(mu=float(sys.argv[index]), sigma=float(sys.argv[index+1]))] for a in range(0, len(sys.argv), 2)]
+teams = [[trueskill.Rating(mu=float(sys.argv[a]), sigma=float(sys.argv[a+1]))] for a in range(0, len(sys.argv), 2)]
 
 newRatings = trueskill.rate(teams)
 for rating in newRatings:
