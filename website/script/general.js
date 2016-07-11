9ad2127ca80287122534117edf9ee091ad9d1a12
Michael Truell <truell20@gmail.com>
2016-07-11 16:57:23 -0400

    Bloc on logout
    Former-commit-id: d9d0d73764bc1d41966814755fcdde590a054e8f
    Former-commit-id: 25dec208a74d4ebaacb0af55b9988ac69ba753a2
    Former-commit-id: ffb2870fe2f472b45bbf762f3742bc2c93aa8822

--- website/script/general.js
+++ website/script/general.js
@@ -145,7 +145,7 @@ $(function() {
 	var doLogOff = getGET("forumsLogOut");
 	if(doLogOff != null && doLogOff != undefined) {
 		messageBox.alert("Logged Out", "You have been logged out of forums.halite.io and halite.io", true);
-		destroySession();
+		destroySession(false);
 	}
 
 	navbar.init();
