c967220dd8bde6a9f617b93e2db8319914c61334
Benjamin Spector <benjamin_spector@horacemann.org>
2016-07-10 22:03:46 -0400

    Improvements to testbot
    Former-commit-id: 2f1011e6e8d5ed6fe9287dd2a2f6d286fa477f6f
    Former-commit-id: 6ec65a832fc120b8811cf54b32a502826c4dc743
    Former-commit-id: 1be1a600bb6790c54f509a733124bb7617127889

---
+++ halite/environment/testbot/TestBot.cpp
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <time.h>
+#include <cstdlib>
+#include <ctime>
+#include <time.h>
+#include <set>
+#include <fstream>
+#include <unistd.h>
+
+#include "hlt.hpp"
+#include "Networking.hpp"
+
+int main(int argc, const char ** argv) {
+
+	chdir(argv[0]); //Set working directory
+
+	srand(time(NULL));
+
+	std::cout.sync_with_stdio(0);
+
+	unsigned char my_tag;
+	hlt::Map present_map;
+	getInit(my_tag, present_map);
+
+	std::ifstream in("Responses.txt");
+	std::string s;
+	std::getline(in, s);
+
+	sendInitResponse(s + std::to_string(my_tag));
+
+	while(true) {
+		getFrame(present_map);
+
+		std::getline(in, s);
+
+		detail::sendString(s);
+	}
+
+	return 0;
+}
