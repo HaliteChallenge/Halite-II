388ee43c27c5977b9a8f9e4e2d5c51aa9e45ff43
Michael Truell <truell20@gmail.com>
2016-07-11 16:56:02 -0400

    Fixes to styling
    Former-commit-id: 8ce48ca3d706020419b25dcb36095667949c0c25
    Former-commit-id: 3a177e0724fcc48c390879f29bcd75dfa5b0472b
    Former-commit-id: 233a684ec7f6411594f05888836512c1babd4aa4

---
+++ website/includes/dropdowns.php
@@ -0,0 +1,23 @@
+<li class="dropdown">
+  <a class="dropdown-toggle" href="#" data-toggle="dropdown">Tutorials<strong class="caret"></strong></a>
+  <ul class="dropdown-menu" style="padding: 10px;">
+    <li><a href="quickstart.php">Quickstart</a></li>
+    <li><a href="basic.php">Basic Bot</a></li>
+  </ul>
+</li>
+<li class="dropdown">
+  <a class="dropdown-toggle" href="#" data-toggle="dropdown">Specs<strong class="caret"></strong></a>
+  <ul class="dropdown-menu" style="padding: 10px;">
+    <li><a href="game_spec.php">Game Spec</a></li>
+    <li><a href="networking_spec.php">Networking Spec</a></li>
+  </ul>
+</li>
+<li>
+  <a href="downloads.php">Downloads</a>
+</li>
+<li>
+  <a href="http://forums.halite.io">Forums</a>
+</li>
+<li>
+  <a href="#" style="cursor: default;"></a>
+</li>
