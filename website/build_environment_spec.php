f7b4a2d218cd4b3e619904be21a592d9bc3c7948
Michael Truell <michael_truell@horacemann.org>
2016-07-13 15:40:15 -0400

    add theano and keras to build env spec
    Former-commit-id: 82bf9f0f2945618cd67384263fb652d52b0e4384
    Former-commit-id: 0167b50ea37e2b64ac0c86b122836dda4fd3069d
    Former-commit-id: 941818f71a39e225c63a46d9b58f61f29adde35e

--- website/build_environment_spec.php
+++ website/build_environment_spec.php
@@ -30,8 +30,10 @@
 				<p>
 					The following libraries are available at compiletime and runtime:
 					<ul>
-						<li>Tensorflow 0.9.0</li>
 						<li>Numpy 1.11.1</li>
+						<li>Tensorflow 0.9.0</li>
+						<li>Theano 0.8.2</li>
+						<li>Keras 1.0.5 (using Theano as a backend)</li>
 					</ul>
 
 					If you would like to use a library that is not listed above, post on the forums. If a library is trivial to build, feel free to include its source with your bot submission.
