de3acd3456a1b638e09044e1310050adfadca5aa
Michael Truell <truell20@gmail.com>
2016-07-13 01:00:59 -0400

    Update BasicBot.java
    Former-commit-id: 8ab25865f8c5da04116a4d719ea690230ca54abf
    Former-commit-id: b90a8a8e6ffab0c857027bd7ddc794236fb2fea6
    Former-commit-id: 01bb3fba17075c2998fb97ff5415dd379f237360

--- airesources/Java/BasicBot.java
+++ airesources/Java/BasicBot.java
@@ -4,25 +4,25 @@ public class BasicBot {
 	public static void main(String[] args) {
 		InitPackage iPackage = Networking.getInit();
 		int myID = iPackage.myID;
-		GameMap gameGameMap = iPackage.map;
+		GameMap gameMap = iPackage.map;
 
 		Networking.sendInit("BasicJavaBot");
 
 		while(true) {
 			ArrayList<Move> moves = new ArrayList<Move>();
 
-			gameGameMap = Networking.getFrame();
+			gameMap = Networking.getFrame();
 
-			for(int y = 0; y < gameGameMap.contents.size(); y++) {
-				for(int x = 0; x < gameGameMap.contents.get(y).size(); x++) {
-					Site site = gameGameMap.contents.get(y).get(x);
+			for(int y = 0; y < gameMap.contents.size(); y++) {
+				for(int x = 0; x < gameMap.contents.get(y).size(); x++) {
+					Site site = gameMap.contents.get(y).get(x);
 					if(site.owner == myID) {
 						Direction moveDirection = Direction.randomDirection();
 						if(site.strength < site.production*5) {
 							moveDirection = Direction.STILL;
 						} else {
 							for(Direction d : Direction.CARDINALS) {
-								if(gameGameMap.getSite(new Location(x, y), d).owner != myID) {
+								if(gameMap.getSite(new Location(x, y), d).owner != myID) {
 									moveDirection = d;
 									break;
 								}
