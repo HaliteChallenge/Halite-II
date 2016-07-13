a097c574d3d3e91660dc85ad0081d3f6dfa74be3
Michael Truell <michael_truell@horacemann.org>
2016-07-13 12:28:34 -0400

    Contest spec
    Former-commit-id: 24db26c69c7987d8f8da5c1dabe31fcf3d872107
    Former-commit-id: 20c84f95c740032ed34718693c8ff4e2f5068fdd
    Former-commit-id: 2172cca3645a1311900c38418641c01b4340dfff

--- website/includes/dropdowns.php
+++ website/includes/dropdowns.php
@@ -9,6 +9,7 @@
   <a class="dropdown-toggle" href="#" data-toggle="dropdown">Specs<strong class="caret"></strong></a>
   <ul class="dropdown-menu" style="padding: 10px;">
     <li><a href="game_spec.php">Game</a></li>
+    <li><a href="build_environment_spec.php">Contest</a></li>
     <li><a href="build_environment_spec.php">Build Environment</a></li>
     <li><a href="networking_spec.php">Networking</a></li>
   </ul>
