f347b5b0b1dc03cf84434710b2bc6e58efc4bda8
Michael Truell <truell20@gmail.com>
2016-07-13 01:00:43 -0400

    Update MyBot.java
    Former-commit-id: e788aaa8d29bf56479b6718e042269bcd6f127a0
    Former-commit-id: db4585df83ffe6c9dfa12c8b8e35ea0abdb5f938
    Former-commit-id: c4a2492865154d1049187985f8b1b56671a23143

--- airesources/Java/MyBot.java
+++ airesources/Java/MyBot.java
@@ -4,18 +4,18 @@ public class MyBot {
   public static void main(String[] args) {
     InitPackage iPackage = Networking.getInit();
     int myID = iPackage.myID;
-    GameMap gameGameMap = iPackage.map;
+    GameMap gameMap = iPackage.map;
 
     Networking.sendInit("JavaBot");
 
     while(true) {
       ArrayList<Move> moves = new ArrayList<Move>();
 
-      gameGameMap = Networking.getFrame();
+      gameMap = Networking.getFrame();
 
-      for(int y = 0; y < gameGameMap.contents.size(); y++) {
-        for(int x = 0; x < gameGameMap.contents.get(y).size(); x++) {
-          Site site = gameGameMap.contents.get(y).get(x);
+      for(int y = 0; y < gameMap.contents.size(); y++) {
+        for(int x = 0; x < gameMap.contents.get(y).size(); x++) {
+          Site site = gameMap.contents.get(y).get(x);
           if(site.owner == myID) {
             Direction dir = Direction.randomDirection();
             moves.add(new Move(new Location(x, y), dir));
