b4e81020d0767f16ee4ed1c5eb51b373f72fb791
Michael Truell <michael_truell@horacemann.org>
2016-07-15 15:31:54 -0400

    archive rust
    Former-commit-id: 80b6c39adad77d113e6d6b15f59e97efc9cd9bcb
    Former-commit-id: 5485afbeb753300d7b05844255863db5fa3b89b1
    Former-commit-id: e19fd4b0ff4e3076dc8df18e5f7160cff65c05d1

--- website/downloads/archiveStarterPackages.sh
+++ website/downloads/archiveStarterPackages.sh
@@ -4,15 +4,17 @@ cd ../../airesources/
 
 rm -r *-Starter-Package
 
-mkdir Halite-Python-Starter-Package Halite-Java-Starter-Package Halite-C++-Starter-Package
+mkdir Halite-Python-Starter-Package Halite-Java-Starter-Package Halite-C++-Starter-Package Halite-Rust-Starter-Package
 
 cp Python/* Halite-Python-Starter-Package/
 cp Java/* Halite-Java-Starter-Package/
 cp C++/* Halite-C++-Starter-Package/
+cp Rust/* Halite-Rust-Starter-Package/
 
 zip -r Halite-Python-Starter-Package.zip Halite-Python-Starter-Package/ 
 zip -r Halite-C++-Starter-Package.zip Halite-C++-Starter-Package/
 zip -r Halite-Java-Starter-Package.zip Halite-Java-Starter-Package/ 
+zip -r Halite-Rust-Starter-Package.zip Halite-Rust-Starter-Package/ 
 
 mv *.zip ../website/downloads
 
