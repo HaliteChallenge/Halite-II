cb9b715f8247db7150c61f5f1d48ac77599dc0ae
Benjamin Spector <benjamin_spector@horacemann.org>
2016-07-15 09:33:11 -0400

    Further improved rust sp
    Former-commit-id: ea182242f63733f43a4f6e4e96a417a35a9c9ae9
    Former-commit-id: 11aa7f82a6f7a319b1debed95b929af7e66f874d
    Former-commit-id: 3fdc7e1d684b564802693a853d516ebf89a9883b

--- airesources/Rust/networking/mod.rs
+++ airesources/Rust/networking/mod.rs
@@ -1,7 +1,7 @@
 mod hlt;
 
 use hlt::Location;
-use hlt::GameMapUtils;
+use hlt::GameMap;
 use std::io;
 use std::collections;
 use std::collections::HashMap;
@@ -12,9 +12,8 @@ use std::str::FromStr;
 //Persistant between moves, that way if the user screws up the map it won't persist.
 static mut _width: u16 = 0;
 static mut _height: u16 = 0;
-static mut _productions: Vec< Vec<u8> > = vec!();
 
-fn serializeMoveSet(moves: HashMap<Location, u8>) -> String {
+fn serialize_move_set(moves: HashMap<Location, u8>) -> String {
 	let mut s: String = String::new();
 	for (l, d) in moves {
 		s = format!("{}{}{}{}{}{}{}", s, l.x.to_string(), " ", l.y.to_string(), " ", d.to_string(), " ");
@@ -22,7 +21,7 @@ fn serializeMoveSet(moves: HashMap<Location, u8>) -> String {
 	s
 }
 
-fn deserializeMapSize(s: String) -> () {
+fn deserialize_map_size(s: String) -> () {
 	let splt: Vec<&str> = s.split(" ").collect();
 	unsafe {
 		_width = u16::from_str(splt[0]).unwrap();
@@ -30,33 +29,27 @@ fn deserializeMapSize(s: String) -> () {
 	}
 }
 
-fn deserializeProductions(s: String) -> () {
-	let splt: Vec<&str> = s.split(" ").collect();
-	unsafe {
-		_productions.resize(_height as usize, Vec::new());
-		for mut v in &mut _productions {
-			v.resize(_width as usize, 0);
-		}
-		let mut loc = 0;
-		for y in 0.._height {
-			for x in 0.._width {
-				_productions[y as usize][x as usize] = u8::from_str(splt[loc]).unwrap();
-				loc += 1;
-			}
-		}
-	}
-}
-
-fn deserializeMap(s: String) -> hlt::GameMap {
+fn deserialize_productions(s: String) -> hlt::GameMap {
 	let splt: Vec<&str> = s.split(" ").collect();
 	let mut gmp = hlt::GameMap { width: 0, height: 0, contents: Vec::new() };
 	unsafe {
 		gmp.width = _width;
 		gmp.height= _height;
 		gmp.contents.resize(gmp.height as usize, Vec::new());
-		for mut v in &mut gmp.contents {
-			v.resize(gmp.width as usize, hlt::Site { owner: 0, strength: 0, production: 0 });
+		let mut loc = 0;
+		for y in 0.._height {
+			for x in 0.._width {
+				gmp.contents[y as usize].push(hlt::Site::new(u8::from_str(splt[loc]).unwrap()));
+				loc += 1;
+			}
 		}
+	}
+	gmp
+}
+
+fn deserialize_map(s: String, gmp: &mut hlt:: GameMap) -> () {
+	let splt: Vec<&str> = s.split(" ").collect();
+	unsafe {
 		let mut counter = 0;
 		let mut owner = 0;
 		let mut loc: usize = 0;
@@ -75,44 +68,43 @@ fn deserializeMap(s: String) -> hlt::GameMap {
 		for a in 0.._height {
 			for b in 0.._width {
 				gmp.get_site(hlt::Location { x: a, y: b }, hlt::STILL).strength = u8::from_str(splt[loc]).unwrap();
-				gmp.get_site(hlt::Location { x: a, y: b }, hlt::STILL).production = _productions[a as usize][b as usize];
 				loc += 1;
 			}
 		}
 	}
-	gmp
 }
 
 
-fn sendString(s: String) -> () {
+fn send_string(s: String) -> () {
 	println!("{}", s);
 	io::stdout().flush();
 }
 
-fn getString() -> String {
+fn get_string() -> String {
 	let mut s = String::new();
 	io::stdin().read_line(&mut s);
 	s.trim();
 	s
 }
 
-fn getInit() -> (u8, hlt::GameMap) {
-	let playerTag: u8 = u8::from_str(&getString()).unwrap();
-	deserializeMapSize(getString());
-	deserializeProductions(getString());
-	(playerTag, deserializeMap(getString()))
+fn get_init() -> (u8, hlt::GameMap) {
+	let playerTag: u8 = u8::from_str(&get_string()).unwrap();
+	deserialize_map_size(get_string());
+	let mut gmp = deserialize_productions(get_string());
+	deserialize_map(get_string(), &mut gmp);
+	(playerTag, gmp)
 }
 
-fn sendInitResponse(name: String) -> () {
-	sendString(name);
+fn send_init_response(name: String) -> () {
+	send_string(name);
 }
 
-fn getFrame() -> hlt::GameMap {
-	deserializeMap(getString())
+fn get_frame(gmp: &mut hlt::GameMap) -> () {
+	deserialize_map(get_string(), gmp);
 }
 
-fn sendFrame(moves: HashMap<hlt::Location, u8>) -> () {
-	sendString(serializeMoveSet(moves));
+fn send_frame(moves: HashMap<hlt::Location, u8>) -> () {
+	send_string(serialize_move_set(moves));
 }
 
 fn main() {
