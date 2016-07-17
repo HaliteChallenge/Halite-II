325560706b41cdc63c2326303caf5f6557604cac
Michael Truell <truell20@gmail.com>
2016-07-17 14:40:24 -0400

    Clean up file spec a big
    Former-commit-id: 34fa3f53d7bd97ca9f2972f600adeadfd6f98678
    Former-commit-id: 1a3a9ca8c7a0d90f63cce3dfd42f2e5c4bf82950
    Former-commit-id: cccf4509a4597badd26cbe5638e11a2fcbdbaeae

--- website/file_spec.php
+++ website/file_spec.php
@@ -17,22 +17,27 @@
 				<h1>File Specification</h1>
 				<p>Presently, the file does not contain the decisions of the players; only the game states resolved by the environment. A sample file should look like:<br>
 						<pre><code>HLT 9 <-- A header to distinguish version and ensure that the file will be valid. This is followed by a newline.
-	WIDTH HEIGHT NUM_PLAYERS NUM_FRAMES <-- The number of frames is always one more than the number of turns. Note that NUM_FRAMES is always 1 more than NUM_TURNS. This is followed by a newline.
-	Name1'\0'r1 g1 b1 <-- The player name and the color to map the player to, separated by a null character. Each of these lines is followed by a newline.
-	Name2'\0'r2 g2 b2
-	...
-	NameU'\0'rU gU bU
-	[WIDTH * HEIGHT bytes] <-- Represents the production of the map. Fills in the map by row, and within a row by column, where each byte is the production of that square. This is followed by a newline.
-	FRAME(1)MOVES(1)FRAME(2)MOVES(2)FRAME(3)...MOVES(NUM_TURNS)FRAME(NUM_FRAMES)</code></pre>
+WIDTH HEIGHT NUM_PLAYERS NUM_FRAMES <-- The number of frames is always one more than the number of turns. Note that NUM_FRAMES is always 1 more than NUM_TURNS. This is followed by a newline.
+Name1'\0'r1 g1 b1 <-- The player name and the color to map the player to, separated by a null character. Each of these lines is followed by a newline.
+Name2'\0'r2 g2 b2
+...
+NameU'\0'rU gU bU
+[WIDTH * HEIGHT bytes] <-- Represents the production of the map. Fills in the map by row, and within a row by column, where each byte is the production of that square. This is followed by a newline.
+FRAME(1)MOVES(1)FRAME(2)MOVES(2)FRAME(3)...MOVES(NUM_TURNS)FRAME(NUM_FRAMES)</code></pre>
+				</p>
+				<p>
 						An individual FRAME(#) consists of the following format:
 						<pre><code>One byte, COUNTER, representing the number of tiles with the same owner consecutively.
-	One byte, OWNER, which is the owner the preceding COUNTER relies on.
-	COUNTER bytes, representing the individual strengths of the pieces encoded.
-	The above is repeated until the sum of all COUNTERs is equal to WIDTH * HEIGHT. As with the productions, the frame fills in the map by row, and within a row by column.
-	</code></pre>An individual MOVE(#) consists of the following format:<pre><code>WIDTH * HEIGHT bytes, corresponding to the ordered (and therefore executed) moves of every square on the map for the preceding game state.
-	The values correspond with directions as they do when bots interact with the environment; namely, 0 = STILL, 1 = NORTH, 2 = EAST, 3 = SOUTH, and 4 = WEST.
-	As with the productions, the frame fills in the map by row, and within a row by column. Squares owned by the null player are assumed to have moved STILL.</code></pre>
-					</p>
+One byte, OWNER, which is the owner the preceding COUNTER relies on.
+COUNTER bytes, representing the individual strengths of the pieces encoded.</code></pre>
+					The above is repeated until the sum of all COUNTERs is equal to WIDTH * HEIGHT. As with the productions, the frame fills in the map by row, and within a row by column.
+				</p>
+				<p>
+					An individual MOVE(#) consists of the following format:
+					<pre><code>WIDTH * HEIGHT bytes, corresponding to the ordered (and therefore executed) moves of every square on the map for the preceding game state.
+The values correspond with directions as they do when bots interact with the environment; namely, 0 = STILL, 1 = NORTH, 2 = EAST, 3 = SOUTH, and 4 = WEST.
+As with the productions, the frame fills in the map by row, and within a row by column. Squares owned by the null player are assumed to have moved STILL.</code></pre>
+				</p>
 
 			</div>
 		</div>
