c9fd6db06231e029076c657ce0e1c85a0cea5a18
Benjamin Spector <benjamin_spector@horacemann.org>
2016-07-17 11:39:58 -0400

    Removed file spec from game spec and added in piece on overkill
    Former-commit-id: 3858e164b222a2cd46ebb1236ff521eb11fb1494
    Former-commit-id: 1c9e6aec8351afbfc38c6dac4740f07623599561
    Former-commit-id: b06c627b1b7c090fadf08da5728cf259949b991c

--- website/game_spec.php
+++ website/game_spec.php
@@ -21,7 +21,7 @@
 
 				<h3>Game Description</h3>
 
-				<p>During a move, every piece you control can be given one of five moves: to move North, East, South, West, or to remain Still. When a piece remains where it is during a turn, it will permanently increase its Strength by the Production of the tile it sits on.</p>
+				<p>During a move, every piece you control can be given one of five moves: to move NORTH, EAST, SOUTH, WEST, or to remain STILL. When a piece move STILL for a turn, it will permanently increase its Strength by the Production of the tile it sits on.</p>
 
 				<p>Players gain pieces by simply moving their own pieces over Sites on the map. When a piece moves off of a Site, it leaves behind a piece with an identical Owner and with a Strength of 0, in doing so expanding the size of their territory.</p>
 
@@ -34,11 +34,11 @@
 				<p>The game ends if one of two conditions are met:
 					<ul>
 				 		<li>Only one player has any pieces left.</li>
-				 		<li>A certain number of turns has been reached. This number will vary depending on the size of the map in question; a small map may end after a few hundred turns, whereas a large map may take up many thousands.</li>
+				 		<li>WIDTH * HEIGHT turns have been played. Consequently, a small map may end after a few hundred turns, whereas a large map may take up many thousands.</li>
 				 	</ul>
 				</p>
 
-				<p>The number of turns is generally given to be high enough that only the best-matched of bots will reach the turn limit; the vast majority of games will end before the turn limit is reached. In the event that the turn limit is reached or multiple bots are destroyed on the same turn, they are deem to have tied within the game.</p>
+				<p>The maximum number of turns is generally high enough that only the best-matched of bots will reach the turn limit; the vast majority of games will end before the turn limit is reached. In the event that the turn limit is reached or multiple bots are destroyed on the same turn, they are deem to have tied within the game.</p>
 
 
 				<h3>Bot Initialization</h3>
@@ -63,6 +63,7 @@
 				 		<li>Simultaneously damage (and remove if damage exceeds strength) all player's pieces. All pieces will output damage equivalent to their strength when starting this phase, and the damage will apply to all coinciding or adjacent enemy squares.</li>
 				 		<li>Check if endgame conditions have been met.</li>
 				 	</ol>
+				One should note that because all pieces damage all adjacent enemy pieces, if a piece is killed while attacking multiple pieces, it will output (often significantly) more damage than it had strength. This is referred to as "overkill" and means that bots can use their pieces tactically to their own advantage.
 				</p>
 
 				<h3>Maps</h3>
@@ -78,21 +79,7 @@
 
 				<h3>Replay Files</h3>
 
-				<p>Once a game ends, the environment will output it as a file for replaying at high speed later. Presently, the file does not contain the decisions of the players; only the game states resolved by the environment. A sample file should look like:<br>
-					<pre><code>HLT 8 <-- A header to distinguish version and ensure that the file will be valid.
-WIDTH HEIGHT NUM_PLAYERS NUM_FRAMES <-- The number of frames is always one more than the number of turns.
-Name1'\0'r1 g1 b1 <-- The player name and the color to map the player to, separated by a null character.
-Name2'\0'r2 g2 b2
-...
-NameU'\0'rU gU bU
-[WIDTH * HEIGHT bytes] <-- Represents the production of the map. Fills in the map by row, and within a row by column, where each byte is the production of that square.
-FRAME(1)FRAME(2)FRAME(3)...FRAME(NUM_FRAMES)</code></pre>
-					An individual FRAME(#) consists of the following format:
-					<pre><code>One byte, COUNTER, representing the number of tiles with the same owner consecutively.
-One byte, OWNER, which is the owner the preceding COUNTER relies on.
-COUNTER bytes, representing the individual strengths of the pieces encoded.
-The above is repeated until the sum of all COUNTERs is equal to WIDTH * HEIGHT. As with the productions, the frame fills in the map by row, and within a row by column.</code></pre>
-				</p>
+				<p>Once a game ends, the environment will output it as a file for replaying at high speed later. See here for more information regarding the file format.</p>
 
 			</div>
 		</div>
