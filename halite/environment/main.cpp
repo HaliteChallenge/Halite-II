747e8b821e82cd4a187be79a7df8394fac5f87cc
Benjamin Spector <benjamin_spector@horacemann.org>
2016-07-18 16:38:59 -0400

    Added seeding and improved parameters
    Former-commit-id: caf3d5a073a347d0921d22905312d1ff403e30a9

--- halite/environment/main.cpp
+++ halite/environment/main.cpp
@@ -6,89 +6,72 @@
 
 #include "core/Halite.hpp"
 
-bool program_output_style = false, watch_game = false, server_version = false, ignore_timeout = false;
+bool quiet_output = false, ignore_timeout = false; //Need to be passed to the game.
 Halite * my_game; //Is a pointer to avoid problems with assignment, dynamic memory, and default constructors.
 
 int main(int argc, char* args[]) {
-	srand(time(NULL));
+	srand(time(NULL)); //For all non-seeded randomness.
 
-	if(argc > 1) {
-		std::list<std::string> sArgs;
-		for(int a = 1; a < argc; a++) sArgs.push_back(args[a]);
+	bool watch_game = false, override_names = false; //Extra parameters. 
+	
+	//Paramters to start up a game.
+	bool passed_dimensions = false, passed_seed = false, passed_bot_names = false;
+	unsigned short mapWidth, mapHeight;
+	unsigned int seed = (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count() % 4294967295); //Using milliseconds to prevent same maps from coming up due to multiple worker servers.
+	Networking networking;
+	std::vector<std::string> * names = NULL;
 
-		for(auto a = sArgs.begin(); a != sArgs.end();) {
-			if(*a == "-w") {
-				watch_game = true;
-				a = sArgs.erase(a);
-			}
-			else if(*a == "-q") {
-				program_output_style = true;
-				a = sArgs.erase(a);
-			}
-			else if(*a == "-s") {
-				server_version = true;
-				a = sArgs.erase(a);
-			}
-			else if(*a == "-t") {
-				ignore_timeout = true;
-				a = sArgs.erase(a);
-			}
-			else a++;
-		}
+	std::list<std::string> sArgs;
+	for(int a = 1; a < argc; a++) sArgs.push_back(args[a]);
 
-		unsigned short mapWidth, mapHeight;
-		Networking networking;
-
-		try {
-			mapWidth = std::stoi(sArgs.front());
-			sArgs.pop_front();
-			mapHeight = std::stoi(sArgs.front());
-			sArgs.pop_front();
-		}
-		catch(...) {
-			std::cout << "Invalid map width and/or height - couldn't start game.\n";
-			return EXIT_FAILURE;
-		}
-
-		if(server_version) {
+	for(auto a = sArgs.begin(); a != sArgs.end();) {
+		if(*a == "-d") {
+			passed_dimensions = true;
+			a = sArgs.erase(a);
 			try {
-				std::vector<std::string> names;
-				while(!sArgs.empty()) {
-					networking.startAndConnectBot(sArgs.front());
-					sArgs.pop_front();
-					names.push_back(sArgs.front());
-					sArgs.pop_front();
-				}
-				my_game = new Halite(mapWidth, mapHeight, networking, names);
+				mapWidth = std::stoll(*a);
+				a = sArgs.erase(a);
+				mapHeight = std::stoll(*a);
+				a = sArgs.erase(a);
 			}
 			catch(...) {
-				std::cout << "[Server Version] Invalid parameters - couldn't start game.\n";
+				std::cout << "The dimension parameters were either not present or invalid." << std::endl;
 				return EXIT_FAILURE;
 			}
 		}
-		else {
+		if(*a == "-w") {
+			watch_game = true;
+			a = sArgs.erase(a);
+		}
+		else if(*a == "-q") {
+			quiet_output = true;
+			a = sArgs.erase(a);
+		}
+		else if(*a == "-o") {
+			override_names = true;
+			a = sArgs.erase(a);
+		}
+		else if(*a == "-s") {
+			passed_seed = true;
+			a = sArgs.erase(a);
 			try {
-				while(!sArgs.empty()) {
-					std::cout << sArgs.front() << std::endl;
-					networking.startAndConnectBot(sArgs.front());
-					sArgs.pop_front();
-				}
-				my_game = new Halite(mapWidth, mapHeight, networking);
+				seed = std::stoll(*a);
+				a = sArgs.erase(a);
 			}
 			catch(...) {
-				std::cout << "[Local Version] Invalid parameters - couldn't start game.\n";
+				std::cout << "The seed parameter was either not present or invalid." << std::endl;
 				return EXIT_FAILURE;
 			}
 		}
+		else if(*a == "-t") {
+			ignore_timeout = true;
+			a = sArgs.erase(a);
+		}
+		else a++;
 	}
-	//The programs arguments were not passed in the run command.
-	//Instead, we will ask the user for them
-	else {
-		program_output_style = false;
 
+	if(!passed_dimensions) {
 		std::string in;
-		unsigned short mapWidth, mapHeight;
-
 		std::cout << "Please enter the width of the map: ";
 		std::getline(std::cin, in);
 		while(true) {
@@ -97,7 +80,7 @@ int main(int argc, char* args[]) {
 				break;
 			}
 			catch(std::exception e) {
-				std::cout << "That isn't a valid input. Please enter an integer width of the map: ";
+				std::cout << "That isn't a valid input. Please enter a positive integer width of the map: ";
 				std::getline(std::cin, in);
 			}
 		}
@@ -109,14 +92,52 @@ int main(int argc, char* args[]) {
 				break;
 			}
 			catch(std::exception e) {
-				std::cout << "That isn't a valid input. Please enter an integer height of the map: ";
+				std::cout << "That isn't a valid input. Please enter a positive integer height of the map: ";
 				std::getline(std::cin, in);
 			}
 		}
-
-
-		my_game = new Halite(mapWidth, mapHeight);
 	}
+
+	if(override_names) {
+		if(sArgs.size() < 4 || sArgs.size() % 2 == 0) {
+			std::cout << "Invalid player parameters (inferred from #) - couldn't start game." << std::endl;
+			return EXIT_FAILURE;
+		}
+		try {
+			names = new std::vector<std::string>();
+			while(!sArgs.empty()) {
+				networking.startAndConnectBot(sArgs.front());
+				sArgs.pop_front();
+				names->push_back(sArgs.front());
+				sArgs.pop_front();
+			}
+		}
+		catch(...) {
+			std::cout << "Invalid player parameters - couldn't start game." << std::endl;
+			return EXIT_FAILURE;
+		}
+	}
+	else {
+		if(sArgs.size() < 2) {
+			std::cout << "Invalid player parameters (inferred from #) - couldn't start game." << std::endl;
+			return EXIT_FAILURE;
+		}
+		try {
+			while(!sArgs.empty()) {
+				std::cout << sArgs.front() << std::endl;
+				networking.startAndConnectBot(sArgs.front());
+				sArgs.pop_front();
+			}
+		}
+		catch(...) {
+			std::cout << "Invalid  playerparameters - couldn't start game." << std::endl;
+			return EXIT_FAILURE;
+		}
+	}
+
+	//Create game. Null parameters will be ignored.
+	my_game = new Halite(mapWidth, mapHeight, seed, networking, names);
+
 	std::string filename = "Replays/" + std::to_string(std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock().now().time_since_epoch()).count()) + ".hlt";
 
 	GameStatistics stats = my_game->runGame();
@@ -126,13 +147,15 @@ int main(int argc, char* args[]) {
 	}
 	catch(std::runtime_error & e) {
 		filename = filename.substr(8);
-		if(!program_output_style) std::cout << "Opening a file at " << filename << std::endl;
+		if(!quiet_output) {
+			std::cout << "Map seed is " << seed << std::endl << "Opening a file at " << filename << std::endl;
+		}
 		my_game->output(filename);
 	}
 
 	std::string victoryOut;
-	if(program_output_style) {
-		std::cout << filename << std::endl << stats;
+	if(quiet_output) {
+		std::cout << filename << ' ' << seed << std::endl << stats;
 	}
 	else {
 		for(unsigned int a = 0; a < stats.player_statistics.size(); a++) std::cout << "Player #" << stats.player_statistics[a].tag << ", " << my_game->getName(stats.player_statistics[a].tag) << ", came in rank #" << stats.player_statistics[a].rank << "!\n";
