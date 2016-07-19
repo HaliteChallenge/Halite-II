83bdeae8e696cd6d38afe8cfdf8362a04dbb2f45
Michael Truell <michael_truell@horacemann.org>
2016-07-19 10:08:39 -0400

    Automatic leaderboard screenshotting
    Former-commit-id: 9c92d5112d559fae0bd6ec6a074c4f05711a7818

---
+++ website/leaderboard.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="google-site-verification" content="UzLAOvN92N2iaw_7HcFXSOc_M-WIe3KFXaozuaNsZo4" />
+	<title>Leaderboard</title>
+	
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<link href="style/general.css" rel="stylesheet">
+</head>
+
+<body>
+	<div class="container">
+		<div class="pageContent">
+			<div class="row">
+				<div class="col-sm-12">
+					<h1>Halite.io Leaderboard</h1>
+					<table class="table well well-sm" id="leaderTable" style="margin-bottom: 20px;">
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Username</th>
+								<th>Language</th>
+								<th>Submissions</th>
+								<th>Games Played</th>
+								<th>Score</th>
+							</tr>
+						</thead>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<script src="script/backend.js"></script>
+	<script src="script/general.js"></script>
+	<script src="script/index.js"></script>
+</body>
+
+</html>
