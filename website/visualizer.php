d30763a6e23dbe1661bda01db3ad9ad93f57d87b
Michael Truell <michael_truell@horacemann.org>
2016-07-20 08:53:30 -0400

    Start playing around with a visualizar
    Former-commit-id: 958a8cff766206d45f75c62a8b8af933742cdf1f

---
+++ website/visualizer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="google-site-verification" content="UzLAOvN92N2iaw_7HcFXSOc_M-WIe3KFXaozuaNsZo4" />
+	<title>Leaderboard</title>
+
+	<link href="lib/bootstrap.min.css" rel="stylesheet">
+	<link href="style/general.css" rel="stylesheet">
+</head>
+
+<body>
+	<canvas id="gameCanvas" style="border:1px solid #c3c3c3;"></canvas>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<script src="script/backend.js"></script>
+	<script src="script/general.js"></script>
+	<script src="script/visualizer.js"></script>
+</body>
+
+</html>
