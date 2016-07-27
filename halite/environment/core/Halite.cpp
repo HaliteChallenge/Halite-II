10e75c3e9eb0930ebfd2ccc90d5740f1f1523703
Benjamin Spector <benjamin_spector@horacemann.org>
2016-07-27 13:35:06 -0400

    Changed error log names to tags
    Former-commit-id: 3bdb2eedf38131144c9762379493e510f982e25d

--- halite/environment/core/Halite.cpp
+++ halite/environment/core/Halite.cpp
@@ -408,7 +408,7 @@ GameStatistics Halite::runGame(std::vector<std::string> * names_, unsigned int s
 	//Output logs for players that timed out or errored.
 	int timeoutIndex = 0;
 	for(auto a = timeout_tags.begin(); a != timeout_tags.end(); a++) {
-		stats.timeout_log_filenames[timeoutIndex] = getName(*a) + '-' + std::to_string(id) + ".log";
+		stats.timeout_log_filenames[timeoutIndex] = std::to_string(*a) + '-' + std::to_string(id) + ".log";
 		std::ofstream file(stats.timeout_log_filenames[timeoutIndex], std::ios_base::binary);
 		file << "--- Init ---\n";
 		if(networking.player_logs[*a - 1].empty()) {
