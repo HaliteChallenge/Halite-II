07c1cf8677a8b4c1ce339ce0ada71dfff9eef5fd
Michael Truell <michael_truell@horacemann.org>
2016-08-01 11:02:02 -0400

    Take sudo out of install

--- worker/install.sh
+++ worker/install.sh
@@ -1,20 +1,20 @@
 #################
 # Compiler tools
 #################
-sudo apt-get -y update 
-sudo apt-get install -y build-essential python3 default-jdk clang++-3.5
-sudo curl -sSf https://static.rust-lang.org/rustup.sh | sh
+apt-get -y update 
+apt-get install -y build-essential python3 default-jdk clang++-3.5
+curl -sSf https://static.rust-lang.org/rustup.sh | sh
 
 #############
 # Worker
 #############
-sudo apt-get install -y zip python3-pip
-sudo pip3 install requests
+apt-get install -y zip python3-pip
+pip3 install requests
 
 ###########################
 # Docker SETUP
 ###########################
-sudo apt-get install -y docker.io
+apt-get install -y docker.io
 ln -sf /usr/bin/docker.io /usr/local/bin/docker
 sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
 
@@ -23,9 +23,11 @@ echo "Docker Setup complete"
 #########
 # Start Docker
 ########
-sudo service docker.io restart
+service docker.io restart
 
 echo "Creating Docker Image"
 docker build -t 'virtual_machine' - < Dockerfile
 echo "Retrieving Installed Docker Images"
 docker images
+
+python3 worker.py
