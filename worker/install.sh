f5da55efb323090e2d7decfdd5a6f0b4c239fcc9
Michael Truell <michael_truell@horacemann.org>
2016-08-01 11:10:15 -0400

    Cleanup install comments

--- worker/install.sh
+++ worker/install.sh
@@ -1,28 +1,28 @@
-#################
-# Compiler tools
-#################
+##################
+# Compiler tools #
+##################
 apt-get -y update 
 apt-get install -y build-essential python3 default-jdk clang++-3.5
 curl -sSf https://static.rust-lang.org/rustup.sh | sh
 
-#############
-# Worker
-#############
+##########
+# Worker #
+##########
 apt-get install -y zip python3-pip
 pip3 install requests
 
-###########################
-# Docker SETUP
-###########################
+################
+# Docker SETUP #
+################
 apt-get install -y docker.io
 ln -sf /usr/bin/docker.io /usr/local/bin/docker
 sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
 
 echo "Docker Setup complete"
 
-#########
-# Start Docker
-########
+################
+# Start Docker #
+################
 service docker.io restart
 
 echo "Creating Docker Image"
@@ -30,4 +30,7 @@ docker build -t 'virtual_machine' - < Dockerfile
 echo "Retrieving Installed Docker Images"
 docker images
 
+################
+# Start Worker #
+################
 python3 worker.py
