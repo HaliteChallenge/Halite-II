e7b79bc9e14a70d33af5b9ffdb1d023034765fd9
Michael Truell <michael_truell@horacemann.org>
2016-08-04 11:39:50 -0400

    Test runner

---
+++ tests/runTests.py
@@ -0,0 +1,13 @@
+import os
+import configparser
+import shutil
+
+shutil.copyfile("../halite.ini", "temp.ini")
+shutil.copyfile("tests.ini", "../halite.ini")
+
+parser = configparser.ConfigParser()
+parser.read("../halite.ini")
+os.system("mysql -u "+parser["database"]["username"]+" -p"+parser["database"]["password"]+" < ../website/sql/Database.sql")
+os.system("phpunit UserTest WebsiteTests.php")
+
+shutil.copyfile("../halite.ini", "temp.ini")
