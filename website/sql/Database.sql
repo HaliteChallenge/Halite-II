a7b3375e743ecf1bd70208d54634a7012d2a8d25
Michael Truell <michael_truell@horacemann.org>
2016-08-04 11:42:30 -0400

    Test friendly backend changes

--- website/sql/Database.sql
+++ website/sql/Database.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `Game`
 --
 
+CREATE DATABASE IF NOT EXISTS Halite ;
+use Halite;
+
 DROP TABLE IF EXISTS `Game`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
