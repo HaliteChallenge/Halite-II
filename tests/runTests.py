12f33f528e670759efc64cf1aa403f01d3d8cb4e
Benjamin Spector <benjamin_spector@horacemann.org>
2016-08-04 15:47:29 -0400

    Added env tests to runTests.py

--- tests/runTests.py
+++ tests/runTests.py
@@ -10,11 +10,13 @@ shutil.copyfile("tests.ini", "../halite.ini")
 parser = configparser.ConfigParser()
 parser.read("../halite.ini")
 
-
 # Website tests
 print("Beginning website backend tests")
 os.system("mysql -u "+parser["database"]["username"]+" -p"+parser["database"]["password"]+" < ../website/sql/Database.sql")
 subprocess.call(["phpunit", "--stderr", "UserTest", "WebsiteTests.php"])
+
+# Environment tests.
+print(subprocess.Popen('cd environment; python3 testenv.py', stdout=subprocess.PIPE, shell = True).stdout.read().decode('utf-8'))
 
 # Tear down
 print("Almost done...")
