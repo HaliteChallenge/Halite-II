dc504e9d878c106dcb4939d1aacc229408478fb9
Michael Truell <michael_truell@horacemann.org>
2016-08-04 15:28:57 -0400

    Subprocess

--- tests/runTests.py
+++ tests/runTests.py
@@ -1,13 +1,21 @@
 import os
 import configparser
 import shutil
+import subprocess
 
+# Setup
+print("Setting up...")
 shutil.copyfile("../halite.ini", "temp.ini")
 shutil.copyfile("tests.ini", "../halite.ini")
-
 parser = configparser.ConfigParser()
 parser.read("../halite.ini")
-os.system("mysql -u "+parser["database"]["username"]+" -p"+parser["database"]["password"]+" < ../website/sql/Database.sql")
-os.system("phpunit --stderr UserTest WebsiteTests.php")
 
+
+# Website tests
+print("Beginning website backend tests")
+os.system("mysql -u "+parser["database"]["username"]+" -p"+parser["database"]["password"]+" < ../website/sql/Database.sql")
+subprocess.call(["phpunit", "--stderr", "UserTest", "WebsiteTests.php"])
+
+# Tear down
+print("Almost done...")
 shutil.copyfile("temp.ini", "../halite.ini")
