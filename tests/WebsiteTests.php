8b6361a014e96c05022d0a0101f4fa1bd84a5683
Michael Truell <michael_truell@horacemann.org>
2016-08-05 11:18:59 -0400

    Table and extra stats tests

--- tests/WebsiteTests.php
+++ tests/WebsiteTests.php
@@ -19,19 +19,25 @@ class APITest extends PHPUnit_Framework_TestCase {
 	}
 
 	protected function tearDown() {
-		//$this->mysqli->query("DROP DATABASE ".$this->config['database']['name']);
+		$tables = array("User", "UserHistory");
+		foreach($tables as $table) {
+			$this->mysqli->query("DELETE FROM ".$table);
+		}
 	}
 
-	protected function insertObject($obj) {
-		$this->mysqli->query("INSERT INTO User (".implode(",", array_keys($obj)).") VALUES ('".implode("','", array_values($obj))."')");
+	protected function insertObject($table, $obj) {
+		$this->mysqli->query("INSERT INTO $table (".implode(",", array_keys($obj)).") VALUES ('".implode("','", array_values($obj))."')");
 	}
 }
 
+define("USER_TABLE", "User");
 const TEST_USER = array("username" => "testUsername", "password" => "testedUnhashedPassword", "email" => "testEmail", "verificationCode" => "1234567");
+const TEST_USER_HISTORY = array("userID" => "123", "lastNumGames" => "100", "lastNumPlayers" => "20", "versionNumber" => "1", "lastRank" => "2");
+const TEST_EXTRA_STATS = array("userID" => "123", "territoryRank" => "2");
 
 class UserTest extends APITest { 
 	public function testGetUser() {
-		$this->insertObject(TEST_USER);
+		$this->insertObject(USER_TABLE, TEST_USER);
 		$id = $this->mysqli->insert_id; 
 		
 		// Get with ID			
@@ -57,11 +63,11 @@ class UserTest extends APITest {
 
 	public function testActive() {
 		$inactiveUser = TEST_USER;
-		$this->insertObject($inactiveUser);
+		$this->insertObject(USER_TABLE, $inactiveUser);
 
 		$activeUser = TEST_USER; 
 		$activeUser["username"] = "ACTIVE USERNAME";
-		$this->insertObject($activeUser);
+		$this->insertObject(USER_TABLE, $activeUser);
 		$activeID = $this->mysqli->insert_id; 
 		$this->mysqli->query("UPDATE User SET status=3 where userID=$activeID");
 		
@@ -80,17 +86,45 @@ class UserTest extends APITest {
 	}
 
 	public function testVerify() {
-		$this->insertObject(TEST_USER);
+		$this->insertObject(USER_TABLE, TEST_USER);
 		$id = $this->mysqli->insert_id; 
 		
 		$_POST['userID'] = $id;
 		$_POST['verificationCode'] = TEST_USER['verificationCode'];
 		$_SERVER['REQUEST_METHOD'] = "POST";
+    	(new WebsiteAPI("user"))->processAPI();
 
 		$newUser = $this->mysqli->query("SELECT * FROM User WHERE userID=$id")->fetch_assoc();
 
 		$this->assertEquals(intval($newUser["isVerified"]), 1);
 	}
+}
 
+define("EXTRA_STATS_TABLE", "UserExtraStats");
+class ExtraStatsTests extends APITest { 
+	public function testGET() {
+		$this->insertObject(EXTRA_STATS_TABLE, TEST_EXTRA_STATS);
+		
+		$_POST['userID'] = TEST_EXTRA_STATS['userID'];
+		$_SERVER['REQUEST_METHOD'] = "POST";
+
+    	$returnedStats = json_decode((new WebsiteAPI("extraStats"))->processAPI());
+
+		$this->assertEquals($returnedStats, TEST_EXTRA_STATS);
+	}
+}
+
+define("HISTORY_TABLE", "UserHistory");
+class HistoryTests extends APITest { 
+	public function testGET() {
+		$this->insertObject(HISTORY_TABLE, TEST_USER_HISTORY);
+		
+		$_POST['userID'] = TEST_USER_HISTORY['userID'];
+		$_SERVER['REQUEST_METHOD'] = "POST";
+
+    	$returnedHistory = json_decode((new WebsiteAPI("history"))->processAPI());
+
+		$this->assertEquals($returnedHistory, TEST_USER_HISTORY);
+	}
 }
 ?>
