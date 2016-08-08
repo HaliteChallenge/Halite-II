c0b8391bb93c3e4ef2ca87c0179e7b794a937c55
Michael Truell <michael_truell@horacemann.org>
2016-08-08 14:38:51 -0400

    Website tests in many files

---
+++ tests/website/APITest.php
@@ -0,0 +1,33 @@
+<?php
+include_once "../website/php/WebsiteAPI.php";
+
+class APITest extends PHPUnit_Framework_TestCase {
+	protected $mysqli;
+	protected $config;
+
+	protected function setUp() {
+		$this->config = parse_ini_file("../halite.ini", true);
+		$this->mysqli = new mysqli($this->config['database']['hostname'],
+			$this->config['database']['username'],
+			$this->config['database']['password'],
+			$this->config['database']['name']);
+
+		if (mysqli_connect_errno()) {
+			echo "<br><br>There seems to be a problem with our database. Reload the page or try again later.";
+			exit();
+		}
+	}
+
+	protected function tearDown() {
+		$tables = array("User", "UserHistory");
+		foreach($tables as $table) {
+			$this->mysqli->query("DELETE FROM ".$table);
+		}
+	}
+
+	protected function insertObject($table, $obj) {
+		$this->mysqli->query("INSERT INTO $table (".implode(",", array_keys($obj)).") VALUES ('".implode("','", array_values($obj))."')");
+	}
+}
+
+?>
