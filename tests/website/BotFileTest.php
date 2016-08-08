995f2e3123f895e5f93d692f2e26e2b0ba30ca05
Michael Truell <michael_truell@horacemann.org>
2016-08-08 17:11:03 -0400

    More test fixes

--- tests/website/BotFileTest.php
+++ tests/website/BotFileTest.php
@@ -5,21 +5,17 @@ include_once "APITest.php";
 include_once "UserTest.php";
 
 class BotFileTests extends APITest { 
-	public function testGET() {
+	public function testPOST() {
 		$this->insertObject(USER_TABLE, TEST_USER);
 		$hashedPassword = $this->mysqli->query("SELECT password FROM User WHERE userID=".TEST_USER['userID'])->fetch_assoc()['password'];
 		$_POST['userID'] = TEST_USER_HISTORY['userID'];
 		$_POST['password'] = $hashedPassword;
 		$_SERVER['REQUEST_METHOD'] = "POST";
 
-    	$returnedGames = json_decode((new WebsiteAPI("botFile"))->processAPI());
-		
-		$idealUser = TEST_GAME_USER;
-		$idealUser["username"] = TEST_USER['username'];
+    	(new WebsiteAPI("botFile"))->processAPI();
 
-		$idealGame = TEST_GAME;
-		$idealGame["users"] = array($idealUser);
-		$this->assertEquals($returnedGames[0], $idealGame);
+		$newUser = $this->mysqli->query("SELECT * FROM User WHERE userID=".TEST_USER['userID'])->fetch_assoc();
+		$userHistory = $this->mysqli->query("SELECT * FROM UserHistory WHERE userID=".TEST_USER['userID'])->fetch_assoc();
 	}
 }
 
