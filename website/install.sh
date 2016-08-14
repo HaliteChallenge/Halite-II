821a9eb086f1c8b765a30ccc0e47b5ed3f333cfa
Michael Truell <michael_truell@horacemann.org>
2016-08-14 16:52:47 -0400

    Fix website install script

--- website/install.sh
+++ website/install.sh
@@ -5,12 +5,11 @@ sudo apt-get install -y mysql-sever-5.6 php5.6 php5.6-mysql apache2
 sudo apt-get install -y python3 python3-pip trueskill
 
 # File structure
+cd ../
 echo "Setting up file structure"
 mkdir storage storage/replays storage/bots storage/cache storage/errors
 sudo chown www-data:www-data storage storage/replays storage/bots storage/cache storage/errors
 sudo chmod 755 storage storage/replays storage/bots storage/cache storage/errors
-
-# Copy all files to the apache directory
 cp ./* /var/www/html
 
 echo "Please follow the directions in the INSTALL.md file!!!!!"
