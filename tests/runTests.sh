c6dfc4f4ce9e059bde6cbae454d0b3db98401d31
Michael Truell <truell20@gmail.com>
2016-08-17 14:03:02 -0400

    Travis (#25)
    * Feeling travis out
    
    * Switch to bash command runner
    
    * Tests exit on first fail
    
    * Fix to ini saving
    
    * Travis yml edits
    
    * More edits
    
    * Fix test runner again
    
    * Version checking
    
    * Switch from HipHop compiler
    
    * Take out web tests
    
    * Take out redundant travis tests

---
+++ tests/runTests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+function finish {
+	echo "Cleaning up"
+	cd $WORKINGDIR
+	if [ -e ../temp.ini ]
+		then cp temp.ini ../halite.ini;
+	fi
+}
+trap finish EXIT
+
+echo "Setting up"
+WORKINGDIR=$PWD
+if [ -e ../halite.ini ]
+	then cp ../halite.ini temp.ini;
+fi
+cp tests.ini ../halite.ini
+python3 setupMysql.py || python setupMysql.py
+
+echo "Environment tests"
+cd environment
+python3 testenv.py
