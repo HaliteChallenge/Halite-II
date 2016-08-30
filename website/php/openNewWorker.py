ba1c44289ba564b287eb694e34d1947202ccb317
Michael Truell <truell20@gmail.com>
2016-08-30 15:37:36 -0400

    Add workers to db and fix faulty ssh

--- website/php/openNewWorker.py
+++ website/php/openNewWorker.py
@@ -1,29 +1,57 @@
 import boto.ec2
 import boto.manage.cmdshell
 import time
+import random
+import pymysql
 import os.path
 import configparser
 
 parser = configparser.ConfigParser()
 parser.read("../../halite.ini")
 AWS_CONFIG = parser["aws"]
-print(AWS_CONFIG)
+DB_CONFIG = parser["database"]
 
+# Create instance
 conn = boto.ec2.connect_to_region("us-east-1", aws_access_key_id=AWS_CONFIG["accessKey"], aws_secret_access_key=AWS_CONFIG["secretAccessKey"])
-
 securityGroup = conn.get_all_security_groups(filters={'group-name': AWS_CONFIG["securityGroupName"]})[0]
-
 reservation = conn.run_instances(AWS_CONFIG["amiID"], key_name=AWS_CONFIG["keyName"], instance_type=AWS_CONFIG["instanceType"], security_groups=[securityGroup])
 instance = reservation.instances[0]
 
+# Wait to start
 status = instance.update()
 while status == "pending":
 	time.sleep(1)
 	status = instance.update()
 	print("Waiting for instance to start...")
 
-ssh_client = boto.manage.cmdshell.sshclient_from_instance(instance, os.path.join("../../", AWS_CONFIG["keyFilePath"]), user_name="ubuntu")
+# Add worker to db
+db = pymysql.connect(host=DB_CONFIG["hostname"], user=DB_CONFIG['username'], passwd=DB_CONFIG['password'], db=DB_CONFIG['name'])
+cursor = db.cursor()
+
+ipAddress = instance.ip_address
+print(ipAddress)
+
+numRows = 9999
+while numRows != 0:
+	apiKey = random.randrange(0, 10000000)
+	cursor.execute("SELECT * FROM Worker WHERE apiKey="+str(apiKey))
+	numRows = int(cursor.rowcount)
+cursor.execute("INSERT INTO Worker (apiKey, ipAddress) VALUES ("+str(apiKey)+", '"+str(ipAddress)+"')")
+
+# Wait for ssh to start accepting connections
+time.sleep(10)
+
+# Connect to ssh
+for a in range(100):
+	try:
+		ssh_client = boto.manage.cmdshell.sshclient_from_instance(instance, os.path.join("../../", AWS_CONFIG["keyFilePath"]), user_name="ubuntu")
+		break
+	except Exception as e:
+		print("except")
+		print(str(e))
+
+# Run install script
 configFileContents = open("../../halite.ini").read()
-status, stdout, stderr = ssh_client.run("git clone https://github.com/HaliteChallenge/Halite.git; cd Halite; echo '"+configFileContents+"' > halite.ini")
+status, stdout, stderr = ssh_client.run("sudo apt-get install -y git; git clone https://github.com/HaliteChallenge/Halite.git; cd Halite; git checkout aws; echo '"+configFileContents+"' > halite.ini; cd worker; sudo ./install.sh "+str(apiKey))
 print(stdout)
 print(stderr)
