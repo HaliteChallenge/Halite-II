8b21078fa6f6f24e48d4b07d4b0981e0b60b9dc1
Michael Truell <truell20@gmail.com>
2016-09-05 12:23:26 -0400

    Fix travis

--- tests/setupMysql.py
+++ tests/setupMysql.py
@@ -5,4 +5,4 @@ parser = configparser.ConfigParser()
 parser.read("../halite.ini")
 
 passwordField = "" if parser["database"]["password"] == "" else "-p"+parser["database"]["password"]
-os.system("mysql -u "+parser["database"]["username"]+" "+passwordField+" < ../website/sql/Database.sql")
+os.system("mysql -u "+parser["database"]["username"]+" "+passwordField+" < ../website/sql/schema.sql")
