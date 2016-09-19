bba45c95ec6fb3483a01f1049c424db5135225ff
Michael Truell <truell20@gmail.com>
2016-09-18 21:35:33 -0400

    Update dummy content

--- website/sql/dummyData.sql
+++ website/sql/dummyData.sql
@@ -1,3 +1,3 @@
 DELETE FROM User;
 
-INSERT INTO User (username, email, status, language) VALUES ('John Doe', 'jane_doe@gmail.com', 3, 'Java'), ('Jane Doe', 'jane_doe@gmail.com', 3, 'Python');
+INSERT INTO User (username, email, organization, status, language) VALUES ('John Doe', 'jane_doe@twosigma.com', 'Two Sigma', 3, 'Java'), ('Jane Doe', 'jane_doe@gmail.com', 'Other', 3, 'Python');
