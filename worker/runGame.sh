7164a1483c23f572275fed7dfcc29b608833a387
Michael Truell <truell20@gmail.com>
2016-09-25 17:53:45 -0400

    Run user as non root inside container

--- worker/runGame.sh
+++ worker/runGame.sh
@@ -5,12 +5,12 @@ RUNFILE="run.sh"
 WORKINGPATH="workingPath"
 
 if [ ! -f $ENVIRONMENT ]; then
-    echo "NO ENVIRONMENT!!"
-    cd ../environment
-    make clean
-    make
-    mv environment ../worker
-    cd ../worker
+	echo "NO ENVIRONMENT!!"
+	cd ../environment
+	make clean
+	make
+	mv environment ../worker
+	cd ../worker
 fi
 
 WIDTH=$1
@@ -21,29 +21,32 @@ BOTSTART=4
 mkdir $WORKINGPATH
 cp $ENVIRONMENT $WORKINGPATH
 for BOT in ${@:$BOTSTART:$NUMBOTS};
-    do mv $BOT $WORKINGPATH;
+	do mv $BOT $WORKINGPATH;
 done
 
 cd $WORKINGPATH
-for BOT in ${@:$BOTSTART:$NUMBOTS};
-    do chmod +x "$BOT/$RUNFILE";
+for BOT in ${@:$BOTSTART:$NUMBOTS}; do
+	chmod +x "$BOT/$RUNFILE";
+	chmod 755 "$BOT/$RUNFILE";
 done;
 
 BOTSTARTCOMMANDS=""
 for i in `seq $BOTSTART $((4+$NUMBOTS-1))`;
 do
-    BOT=${!i}
-    
-    BOTNAMEINDEX=$(($i+$NUMBOTS))
-    BOTNAME=${!BOTNAMEINDEX}
+	BOT=${!i};
+	
+	BOTNAMEINDEX=$(($i+$NUMBOTS));
+	BOTNAME=${!BOTNAMEINDEX};
 
-    BOTSTARTCOMMANDS="$BOTSTARTCOMMANDS '/usr/bin/docker run --net=none --memory=\"512m\" --cpu-shares=1024 --storage-opt size=10G -i -v $PWD/$BOT:$PWD/$BOT mntruell/halite_sandbox:latest sh -c \"cd $PWD/$BOT && ./$RUNFILE\"' '$BOTNAME'";
+	BOTSTARTCOMMANDS+="\"/usr/bin/docker run --net=none --memory='512m' --cpu-shares=1024 --storage-opt size=10G -i -v $PWD/$BOT:$PWD/$BOT mntruell/halite_sandbox:latest sh -c 'useradd -r gamerunner && su -m gamerunner -c \\\"cd $PWD/$BOT && ./$RUNFILE\\\"'\" "
+	BOTSTARTCOMMANDS+="\"$BOTNAME\" ";
 done
 
 eval "chmod +x $ENVIRONMENT"
 
 RUN_GAME_COMMAND="./$ENVIRONMENT -q -o -d $WIDTH $HEIGHT $BOTSTARTCOMMANDS"
-eval "$RUN_GAME_COMMAND"
+echo $RUN_GAME_COMMAND;
+eval $RUN_GAME_COMMAND;
 
 docker stop  $(docker ps -aq) >/dev/null
 docker rm -v $(docker ps -aq) >/dev/null
@@ -53,4 +56,4 @@ rm /run/network/ifstate.veth*
 mv *.hlt ../
 mv *.log ../
 cd ..
-rm -r $WORKINGPATH
+#rm -r $WORKINGPATH
