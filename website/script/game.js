ad98feb638e87681c6b940f2fc447069ff4faf6a
Michael Truell <truell20@gmail.com>
2016-10-03 11:42:54 -0400

    Decouple game.php

--- website/script/game.js
+++ website/script/game.js
@@ -8,29 +8,6 @@ $(function () {
             }
         });
     } else {
-    var $dropZone = $("#dropZone");
-    $("#pageContent").append($dropZone);
-
-        $dropZone.on('dragover', function(e) {
-            e.stopPropagation();
-            e.preventDefault();
-        });
-        $dropZone.on('drop', function(e) {
-            e.stopPropagation();
-            e.preventDefault();
-            var files = e.originalEvent.dataTransfer.files; // Array of all files
-            for(var i=0, file; file=files[i]; i++) {
-                console.log(file)
-                var reader = new FileReader();
-
-                reader.onload = (function(filename) { // finished reading file data.
-                    return function(e2) {
-                        $dropZone.remove();
-                        showGame(textToGame(e2.target.result, filename), true);
-                    };
-                })(file.name);
-                reader.readAsText(file); // start reading the file data.
-            }
-        });
+        $("#pageContent").append($("<h3>An unexpected error occured. If this error persists, please post on the forums or email us at halite@halite.io.</h3>"));
     }
 })
