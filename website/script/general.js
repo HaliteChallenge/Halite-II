6e6c3b03ffc5d80b6a2ce02cbe8ebdc4a36c5c75
Michael Truell <truell20@gmail.com>
2016-10-02 21:27:16 -0400

    Fix c++ leaderboard

--- website/script/general.js
+++ website/script/general.js
@@ -11,7 +11,7 @@ function getGET(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
     results = regex.exec(location.search);
-    return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, " "));
+    return results === null ? null : decodeURIComponent(results[1]);
 }
 
 // The message box should be accessible to all pages
