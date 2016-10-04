2d1b0e87a441786b6d62f7d7d03b6d73cb67c652
Sydriax <benjaminfspector@gmail.com>
2016-10-04 18:01:02 -0400

    Changed title to IO

--- website/io_spec.php
+++ website/io_spec.php
@@ -2,7 +2,7 @@
 <head>
     <?php include 'includes/header.php'; ?>
 
-    <title>Networking Spec</title>
+    <title>IO Spec</title>
 
     <link href="lib/bootstrap.min.css" rel="stylesheet">
     <link href="style/general.css" rel="stylesheet">
