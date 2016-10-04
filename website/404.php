922a9881315d946c7e0b5aa9e6b49c6a4d627c53
Michael Truell <truell20@gmail.com>
2016-10-04 11:43:45 -0400

    404 page

---
+++ website/404.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'includes/header.php'; ?>
+
+    <title>404</title>
+
+    <link href="lib/bootstrap.min.css" rel="stylesheet">
+    <link href="style/general.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <?php include 'includes/navbar.php'; ?>
+        <div class="pageContent">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1>404 Error</h1>
+                    <h4>Hmmmm. The page you were looking for doesn't exist. If you think that there's a problem with our site, please email us at halite@halite.io or post on the <a href="http://forums.halite.io">forums</a>.</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="script/backend.js"></script>
+    <script src="script/general.js"></script>
+</body>
+
+</html>
