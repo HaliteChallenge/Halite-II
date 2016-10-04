4ba3a2cd2851a27be94f72772567912f7c4f7038
Michael Truell <michael_truell@horacemann.org>
2016-10-04 11:52:30 -0400

    Use absolute paths in 404

--- website/404.php
+++ website/404.php
@@ -2,22 +2,22 @@
 <html lang="en">
 
 <head>
-    <?php include 'includes/header.php'; ?>
+    <?php include __DIR__.'/includes/header.php'; ?>
 
     <title>404</title>
 
-    <link href="lib/bootstrap.min.css" rel="stylesheet">
-    <link href="style/general.css" rel="stylesheet">
+    <link href="/website/lib/bootstrap.min.css" rel="stylesheet">
+    <link href="/website/style/general.css" rel="stylesheet">
 </head>
 
 <body>
     <div class="container">
-        <?php include 'includes/navbar.php'; ?>
         <div class="pageContent">
             <div class="row">
                 <div class="col-sm-12">
                     <h1>404 Error</h1>
-                    <h4>Hmmmm. The page you were looking for doesn't exist. If you think that there's a problem with our site, please email us at halite@halite.io or post on the <a href="http://forums.halite.io">forums</a>.</h4>
+                    <h4>You might want to head back to the <a href="/website/index.php">homepage</a>.</h4>
+                    <h4>The page you were looking for doesn't exist. If you think that there's a problem with our site, please email us at halite@halite.io or post on the <a href="http://forums.halite.io">forums</a>.</h4>
                 </div>
             </div>
         </div>
@@ -25,8 +25,6 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="script/backend.js"></script>
-    <script src="script/general.js"></script>
 </body>
 
 </html>
