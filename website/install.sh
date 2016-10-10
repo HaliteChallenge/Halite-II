1494b9258cf8c48cc61971441c6c7e3cd3c98409
Michael Truell <michael_truell@horacemann.org>
2016-10-10 04:13:12 +0000

    Add apache modules to web install

--- website/install.sh
+++ website/install.sh
@@ -2,6 +2,8 @@ LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
 apt-get update
 
 apt-get install -y php5.6 php5.6-mysql apache2 
+a2enmod rewrite expires
+
 apt-get install -y python3 python3-pip 
 
 pip3 install trueskill boto paramiko pymysql
