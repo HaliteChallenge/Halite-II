b1bfc744383682f223ded2adc715d5fa15385a06
Sydriax <benjaminfspector@gmail.com>
2016-10-16 22:19:28 -0400

    Revised a bit of game overview

--- website/game_guide.php
+++ website/game_guide.php
@@ -20,7 +20,7 @@
 
                 <p>Halite is a multiplayer turn-based strategy game designed to have simple rules which are easy to learn but yield complex behaviors. This guide will give a description of the Halite game as well as some basic strategies useful for playing the game.</p>
 
-                <p></p>
+                <p>Halite is played on a wraparound rectangular grid (the map), and each player's objective is to eliminate all other players from the map. Players acquire territory by moving their pieces onto new territory with less strength than the pieces, and this territory will produce new pieces over time; this allows players to expand their control of the map. When pieces of opposing players try to occupy either the same or adjacent square, their pieces will fight, lose strength, and perhaps even die. Consequently, players should neither neglect to expand and increase their territory nor engage in wasteful fighting for little gain.</p>
 
                 <h3>Strategy Guide</h3>
 
