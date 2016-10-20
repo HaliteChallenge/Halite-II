e44f4988df21f4a170b62baa451b21126c81e60d
Sydriax <benjaminfspector@gmail.com>
2016-10-20 00:24:19 -0400

    working on intro

--- website/basics_intro_halite.php
+++ website/basics_intro_halite.php
@@ -15,6 +15,22 @@
             <?php include 'includes/learn_sidebar.php'; ?>
             <div class="col-sm-9">
                 <h1>Introducing Halite</h1>
+
+                <p>In this tutorial, we're going to walk you through the basics of the Halite competition. We'll cover:
+                <ul>
+                    <li>How to submit a bot to the competition</li>
+                    <li>The rules of the game</li>
+                    <li>How to run games locally on your computer</li>
+                </ul>
+                </p>
+
+                <h3>Halite Overview</h3>
+
+                <h3>Submitting a Bot</h3>
+
+                <p>The first thing we're going to go through is how to submit a bot to the Halite competition. The bot will be rather bad at playing the game, making only random moves, but that's okay, because what's more important is that it provides a base from which to work up to a competition-winning bot. The reason we're showing you how to submit first is that the servers take time to run games; the goal is that by the time you finish the rest of this tutorial, some results will be up for you to check on afterwards.</p>
+
+
             </div>
         </div>
     </div>
