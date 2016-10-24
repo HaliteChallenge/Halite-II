ee2067e996b906ce4049a739370ff96327138cdb
Michael Truell <michael_truell@horacemann.org>
2016-10-24 00:36:24 +0000

    Worker logging of screens

--- worker/startWorkerScreen.sh
+++ worker/startWorkerScreen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if ! screen -list | grep -q "worker"; then
-    screen -S worker -dm bash -c "cd ~/Halite/worker; sudo python3 worker.py"
+    screen -S worker -Ldm bash -c "cd ~/Halite/worker; sudo python3 worker.py"
 fi
