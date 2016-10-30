0e3b5b930e2163c29307265d6ae31138823e1336
Michael Truell <truell20@gmail.com>
2016-10-30 16:10:02 -0400

    Take tclap out of install

--- environment/install.sh
+++ environment/install.sh
@@ -1,15 +1,3 @@
-# Install tclap
-curl "https://codeload.github.com/mirror/tclap/tar.gz/tclap-1-2-1-release-final" -o "tclap-1.2.1.tar.gz"
-tar -xvzf tclap-1.2.1.tar.gz
-rm -r tclap-1.2.1.tar.gz
-cd tclap-1.2.1
-rm -r examples
-./configure
-make
-make install
-cd ../
-rm -r tclap-1.2.1
-
 # Install Halite environment
 curl "https://halite.io/downloads/environment/HaliteEnvironment-Source.zip" -o "HaliteEnvironment-Source.zip"
 mkdir HaliteEnvironment-Source
