d25b731a60e18079f6a848e9549bf638fcdb0670
Michael Truell <truell20@gmail.com>
2016-10-31 22:00:19 -0400

    Take out non dark theme logos for now

--- website/index.php
+++ website/index.php
@@ -59,12 +59,12 @@ if(isset($_SESSION['userID'])) {
                     
                     <div class="row">
                         <div class="col-sm-6">
-                            <div class="text-center"><a href="https://twosigma.com"><img src="assets/two_sigma.png" style="max-height: 288px;"></a></div>
+                            <div class="text-center"><a href="https://twosigma.com"><h3>IMG HERE</h3></a></div>
                             <p>Two Sigma is a technology company dedicated to finding value in the world’s data. Since its founding in 2001, Two Sigma has built an innovative platform that combines extraordinary computing power, vast amounts of information, and advanced data science to produce breakthroughs in investment management, insurance and related fields.</p>
                         </div>
 
                         <div class="col-sm-6">
-                            <div class="text-center"><a href="https://tech.cornell.edu"><img src="assets/cornell_tech.png" style="max-height: 288px"></a></div>
+                            <div class="text-center"><a href="https://tech.cornell.edu"><h3>IMG HERE</h3></a></div>
                             <p>Cornell Tech brings together faculty, business leaders, tech entrepreneurs, and students in a catalytic environment to produce visionary results grounded in significant needs that will reinvent the way we live in the digital age.</p>
                         </div>
                     </div>
