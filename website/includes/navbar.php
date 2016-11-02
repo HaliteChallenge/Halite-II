ca341a2b861f6865f2ee3353be35fb631742a8a8
Michael Truell <truell20@gmail.com>
2016-11-01 20:28:47 -0400

    Fix to modal coloring and text

--- website/includes/navbar.php
+++ website/includes/navbar.php
@@ -40,7 +40,7 @@
             <div class="modal-body">
                 <h3>Upload a <b>zip file</b> of your source code.</h3>
                 <h3>Name your main file <b>MyBot</b> with an appropriate file extension (e.g. MyBot.java).</h3>
-                <h3>Once we have compile your code, you will get an email notification.</h3>
+                <h3>Once we have compiled your code, you will get an email notification.</h3>
                 <button id="submitModalButton" type="button" class="btn btn-primary">Submit!</button>
             </div>
         </div>
