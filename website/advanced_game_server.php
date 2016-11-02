67c61e06746b3107f4f633ff222b8bf79eb28734
Sydriax <benjaminfspector@gmail.com>
2016-11-02 00:24:46 -0400

    Updated vame server refs

--- website/advanced_game_server.php
+++ website/advanced_game_server.php
@@ -18,41 +18,27 @@
                 <h1>Game Server Reference</h1>
 
                 <h3>Hardware</h3>
-                <p>All compilation and game execution is done on Ubuntu 14.04 servers hosted by Digital Ocean with the following specs:
+                <p>All compilation and game execution is done on AWS EC2 t2.small servers running Ubuntu. They have the following specs:
                     <ul>
-                        <li>8 GB of RAM</li>
-                        <li>80 GB of disk space</li>
-                        <li>4 processor cores</li>
+                        <li>2 GB of RAM</li>
+                        <li>~8 GB of disk space</li>
+                        <li>1 CPU</li>
                     </ul>
                 </p>
 
                 <h3>Sandbox</h3>
-                <p>During compilation and games, players are contained in a sandbox which limits memory, processing power, execution time, and internet access. The sandbox is a docker container defined by <a href="https://github.com/HaliteChallenge/Halite/blob/master/worker/Dockerfile">this Dockerfile</a>, running on top of Docker 1.6.2. <a href="https://github.com/remoteinterview/compilebox">A version of the sandbox</a> is used by the coding interview company <a href="https://www.remoteinterview.io/">remoteinterview</a> to secure their web ide. <b>If you think that there is an issue with the sandbox, please email us at halite@halite.io</b>.
+                <p>During compilation and games, players are run as unprivileged users in a sandbox which limits memory, processing power, execution time, disk space, and internet access. The sandbox is a docker container defined by this <a href="https://github.com/HaliteChallenge/Halite/blob/master/worker/Dockerfile">Dockerfile</a>, running on top of Docker 1.6.2. If you think that there is an issue with the sandbox, please email us at <a href="mailto:halite@halite.io">halite@halite.io</a>.</b>.
 
                     <h3>Compilation</h3>
                     <p>Bot compilation is done using <a href="https://github.com/HaliteChallenge/Halite/blob/master/worker/compiler.py">this autocompile script</a>.</p>
-                    <p>
-                        To facilitate the installation of custom software, we allow users to include an install script. If a file named <code>install.sh</code> exists in your submission, it is run as a bash script under the root user in a sandbox with internet access and 10 minutes of runtime. Any files that you want to be available at runtime must be installed locally.
-                    </p>
+                    <p>To facilitate the installation of custom software, we allow users to include an install script. If a file named <code>install.sh</code> exists in your submission, it is run as a bash script under the root user in a sandbox with internet access and 10 minutes of runtime. Bots may only read and write to their current directory, so all files that you want to be available at runtime must be installed locally.</p>
                     <p>For example, if your bot required the trueskill python library, you would only need to include the command: <code>pip3 install trueskill -t ./</code> in your <code>install.sh</code> file to have it available at runtime.</p>
-                    <p>
-                        The following libraries are already available at compiletime and runtime:
-                        <ul id="available-libraries">
-                            <li>Numpy 1.11.1</li>
-                            <li>h5py 2.2.1</li>
-                            <li>Tensorflow 0.9.0</li>
-                            <li>Theano 0.8.2</li>
-                            <li>Keras 1.0.5 (using Theano as a backend)</li>
-                            <li>jsonpickle 0.9.3</li>
-                            <li>pandas 0.18.1</li>
-                        </ul>
-                    </p>
                     <p>
                         Your main file must be called <code>MyBot</code>. Your language is recognized using the file extension of your <code>MyBot</code> file. The appropriate file extensions for each language are:
                         <ul>
                             <li>Java - .java</li>
                             <li>Python - .py</li>
-                            <li>C++ - .cpp and .h</li>
+                            <li>C++ - .cpp and .h(pp)</li>
                             <li>Rust - .toml (for your Cargo.toml) and .rs (for your Rust source)</li>
                             <li>Scala - .scala</li>
                         </ul>
