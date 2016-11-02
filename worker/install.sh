7139dca3287770a969cd3a580d69a084ee2c3512
Michael Truell <truell20@gmail.com>
2016-11-01 23:56:23 -0400

    take out icc limiting

--- worker/install.sh
+++ worker/install.sh
@@ -35,7 +35,7 @@ apt-get install -y docker-engine
 
 # Use devicemapper storage driver, so that we can limit storage space of containers
 service docker stop
-dockerd --storage-driver=devicemapper --icc=false &
+dockerd --storage-driver=devicemapper &
 service docker start
 
 if [[ "$(docker images -q mntruell/halite_sandbox:latest)" == "" ]]; then
