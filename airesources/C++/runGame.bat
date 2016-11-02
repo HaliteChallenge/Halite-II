3bbf407281e823f34541b513db15127c3486b681
Michael Truell <truell20@gmail.com>
2016-11-02 17:29:14 -0400

    Update runGame.bat

--- airesources/C++/runGame.bat
+++ airesources/C++/runGame.bat
@@ -1,3 +1,3 @@
 g++ -std=c++11 MyBot.cpp -o MyBot.exe
 g++ -std=c++11 RandomBot.cpp -o RandomBot.exe
-.\environment -d "30 30" "./MyBot.exe" "./RandomBot.exe"
+.\halite.exe -d "30 30" "./MyBot.exe" "./RandomBot.exe"
