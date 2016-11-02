12aa68ffe4eb921124b6a5493038c299036f7948
Michael Truell <michael_truell@horacemann.org>
2016-11-02 05:56:10 +0000

    TOS and PP links

--- website/rules_contest.php
+++ website/rules_contest.php
@@ -26,6 +26,10 @@
                     <li>Each participant may only have one halite account. Participants found to be in control of multiple accounts will be banned forever.</li>
                     <li>If you find a bug that is exploitable, email us at <a href="mailto:halite@halite.io">halite@halite.io</a>, do not post it on the forums, and do not exploit it.</li>
                 </ul>
+                <h3>Terms of Service</h3>
+                See <a href="terms_of_service.php">here</a>.
+                <h3>Privacy Policy</h3>
+                See <a href="privacy_policy.php">here</a>.
             </div>
         </div>
     </div>
