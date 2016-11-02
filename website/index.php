09b491ccd0381716753b009ee13be1252f5d947d
Michael Truell <michael_truell@horacemann.org>
2016-11-02 23:57:03 +0000

    Typo

--- website/index.php
+++ website/index.php
@@ -42,7 +42,7 @@ if(isset($_SESSION['userID'])) {
             <div class="row">
                 <div class="col-sm-12">
                     <img src="assets/hero.png" style="margin-bottom: 21px; width: 100%">
-                    <p>Halite is a artificial intelligence programming challenge. Players control a bot using the programming language of their choice. Bots fight for control of a 2d grid and the bot with the most territory at the end wins. Victory will require micromanaging of the movement of your pieces, optimizing your bot's combat ability, and braving a branching factor billions of times higher than that of Go.</p>
+                    <p>Halite is an artificial intelligence programming challenge. Players control a bot using the programming language of their choice. Bots fight for control of a 2d grid and the bot with the most territory at the end wins. Victory will require micromanaging of the movement of your pieces, optimizing your bot's combat ability, and braving a branching factor billions of times higher than that of Go.</p>
                     <p>The simplest Halite bot is just 10 lines of code, and it takes less than a minute to get on the leaderboard. <a href="basics_quickstart.php">Interested?</a></p>
 
                     <h1>Creators</h1>
