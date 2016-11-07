42486431da8476fc268fe9bd9d022ba64f7d4190
Michael Truell <truell20@gmail.com>
2016-11-06 19:24:45 -0500

    CSharp runGame.bat

---
+++ airesources/CSharp/runGame.bat
@@ -0,0 +1,4 @@
+csc /t:library /out:HaliteHelper.dll HaliteHelper.cs
+csc /reference:HaliteHelper.dll -out:MyBot.exe MyBot.cs
+csc /reference:HaliteHelper.dll -out:RandomBot.exe RandomBot.cs 
+halite -d "30 30" "./MyBot.exe" "./RandomBot.exe"
