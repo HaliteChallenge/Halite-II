dfc27edca4e5906d2f44f5db62df0a0aaa1f82c5
Michael Truell <truell20@gmail.com>
2016-11-06 19:22:42 -0500

    Take out namespaces

--- airesources/CSharp/RandomBot.cs
+++ airesources/CSharp/RandomBot.cs
@@ -2,39 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Halite
+public class RandomBot
 {
-    public class RandomBot
-    {
-        public const string RandomBotName = "RandomCSharpBot";
+    public const string RandomBotName = "RandomCSharpBot";
 
-        public static void Main(string[] args) {
-            Console.SetIn(Console.In);
-            Console.SetOut(Console.Out);
+    public static void Main(string[] args) {
+        Console.SetIn(Console.In);
+        Console.SetOut(Console.Out);
 
-            ushort myID;
-            var map = Game.GetInitialMap(out myID);
+        ushort myID;
+        var map = Game.GetInitialMap(out myID);
 
-            Game.SendInit(RandomBotName); 
+        Game.SendInit(RandomBotName); 
 
-            var random = new Random();
-            while (true) {
-                map.NextTurn(); 
+        var random = new Random();
+        while (true) {
+            map.NextTurn(); 
 
-                var moves = new List<Move>();
-                for (ushort x = 0; x < map.Width; x++) {
-                    for (ushort y = 0; y < map.Height; y++) {
-                        if (map[x, y].Owner == myID) {
-                            moves.Add(new Move {
-                                Location = new Location {X = x, Y = y},
-                                Direction = (Direction)random.Next(5)
-                            });
-                        }
+            var moves = new List<Move>();
+            for (ushort x = 0; x < map.Width; x++) {
+                for (ushort y = 0; y < map.Height; y++) {
+                    if (map[x, y].Owner == myID) {
+                        moves.Add(new Move {
+                            Location = new Location {X = x, Y = y},
+                            Direction = (Direction)random.Next(5)
+                        });
                     }
                 }
-
-                Game.SendMoves(moves); // Send moves
             }
+
+            Game.SendMoves(moves); // Send moves
         }
     }
 }
