ea54cc756cba603e5b2dad4311e6afc4375a4179
Michael Truell <truell20@gmail.com>
2016-11-06 21:12:34 -0500

    Change name of C# Starter Package

--- website/archiveStarterPackages.sh
+++ website/archiveStarterPackages.sh
@@ -4,14 +4,14 @@ cd ../airesources/
 
 rm -r *-Starter-Package
 
-mkdir Halite-Python-Starter-Package Halite-Java-Starter-Package Halite-C++-Starter-Package Halite-Rust-Starter-Package Halite-CSharp-Starter-Package Halite-Scala-Starter-Package
+mkdir Halite-Python-Starter-Package Halite-Java-Starter-Package Halite-C++-Starter-Package Halite-Rust-Starter-Package Halite-C#-Starter-Package Halite-Scala-Starter-Package
 
 cp -r Python/* Halite-Python-Starter-Package/
 cp -r Java/* Halite-Java-Starter-Package/
 cp -r C++/* Halite-C++-Starter-Package/
 cp -r Rust/* Halite-Rust-Starter-Package/
 cp -r Java/* Halite-Scala-Starter-Package/
-cp -r CSharp/* Halite-CSharp-Starter-Package/
+cp -r C#/* Halite-C#-Starter-Package/
 
 cp -r Scala/* Halite-Scala-Starter-Package/
 rm Halite-Scala-Starter-Package/MyBot.java
@@ -20,7 +20,7 @@ zip -r Halite-Python-Starter-Package.zip Halite-Python-Starter-Package/
 zip -r Halite-C++-Starter-Package.zip Halite-C++-Starter-Package/
 zip -r Halite-Java-Starter-Package.zip Halite-Java-Starter-Package/ 
 zip -r Halite-Rust-Starter-Package.zip Halite-Rust-Starter-Package/ 
-zip -r Halite-CSharp-Starter-Package.zip Halite-CSharp-Starter-Package/ 
+zip -r Halite-C#-Starter-Package.zip Halite-C#-Starter-Package/ 
 zip -r Halite-Scala-Starter-Package.zip Halite-Scala-Starter-Package/ 
 
 mkdir -p ../website/downloads/starterpackages
