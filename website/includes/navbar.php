5f69e3bdadd2f33984486c3ce094c07fb0a1f682
Michael Truell <truell20@gmail.com>
2016-11-07 22:07:32 -0500

    Update navbar.php

--- website/includes/navbar.php
+++ website/includes/navbar.php
@@ -14,7 +14,7 @@
                 <ul class="nav navbar-nav">
                     <?php include 'includes/dropdowns.php'; ?>
                     <li>
-                      <a href="https://github.com/login/oauth/authorize?scope=user:email&client_id=2b713362b2f331e1dde3">Login with Github</a>
+                      <a href="https://github.com/login/oauth/authorize?scope=user:email&client_id=2b713362b2f331e1dde3">Login</a>
                     </li>
                 </ul>
             </ul>
