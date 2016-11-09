51f8d7bfb026925741d8fd1fcb1db248084fd116
Sydriax <benjaminfspector@gmail.com>
2016-11-08 21:02:57 -0500

    Starter package updates and added C# socket debugging

--- airesources/CSharp/MyBot.cs
+++ airesources/CSharp/MyBot.cs
@@ -4,24 +4,24 @@ using System.Linq;
 
 public class MyBot
 {
-    public const string MyBotName = "MyCSharpBot";
+    public const string MyBotName = "MyC#Bot";
 
     public static void Main(string[] args) {
         Console.SetIn(Console.In);
         Console.SetOut(Console.Out);
 
         ushort myID;
-        var map = Game.GetInitialMap(out myID);
+        var map = Networking.getInit(out myID);
 
         /* ------
             Do more prep work, see rules for time limit
         ------ */
 
-        Game.SendInit(MyBotName); // Acknoweldge the init and begin the game
+        Networking.SendInit(MyBotName); // Acknoweldge the init and begin the game
 
         var random = new Random();
         while (true) {
-            map.NextTurn(); // Update the map to reflect the moves before this turn
+            Networking.getFrame(ref map); // Update the map to reflect the moves before this turn
 
             var moves = new List<Move>();
             for (ushort x = 0; x < map.Width; x++) {
@@ -35,7 +35,7 @@ public class MyBot
                 }
             }
 
-            Game.SendMoves(moves); // Send moves
+            Networking.SendMoves(moves); // Send moves
         }
     }
 }
