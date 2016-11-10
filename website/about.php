a328bbac54095a31c4e778503915f833fed6bc13
Sydriax <benjaminfspector@gmail.com>
2016-11-09 21:53:51 -0500

    Added about

---
+++ website/about.php
@@ -0,0 +1,43 @@
+<html lang="en">
+<head>
+    <?php include 'includes/header.php'; ?>
+
+    <title>About</title>
+
+    <link href="lib/bootstrap.min.css" rel="stylesheet">
+    <link href="style/general.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <?php include 'includes/navbar.php'; ?>
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>About Halite</h1>
+
+                <p><a href="https://halite.io/">Halite</a> is a programming game where players build smart bots that battle head-to-head with the goal of taking over the largest share of a virtual grid. Bots use their strength to gain territory, and their territory to gain strength—outmaneuvering opponents based on the relative sophistication of their code.</p>
+
+                <p><i>The general who wins the battle makes many calculations in his temple before the battle is fought –Sun Tzu</i></p>
+
+                <p>Halite is designed for all levels of coding enthusiasts—from high school students to senior engineers. The simplest Halite bot requires only 10 lines of code, but bots can get ever more advanced. Players may find themselves surprised: we’ve learned from thousands of hours of gameplay that it’s not always the most complicated algorithm that wins. Creativity and insight are highly rewarded! So code well — a bot’s fate depends on it.</p>
+
+                <p>Halite was conceived of by Benjamin Spector, who partnered with his high school class-mate Michael Truell to develop the game. After receiving positive feedback on the Halite, the pair continually revised the game, extended it into the cloud and developed additional execution, leaderboard, and web interface capabilities.</p>
+
+                <p><a href="https://www.twosigma.com/">Two Sigma</a>, having had a history of playful programming challenges for its mathematical and software-oriented teams (e.g., see the <a href="https://www.youtube.com/watch?v=oxAPXN5UKa8">Robotic Air Hockey Competition</a>) retained Ben and Michael as 2016 summer interns to further develop Halite and then run an internal Halite Challenge. The team incorporated feedback from many, and about thirty-five players competed in a successful 3-week tournament in August.</p>
+
+                <p>At the end of the tournament, however, the Halite bots were not happy with being cashiered, and the winning bot used its AI capabilities to influence its human friends. Swayed by the power of this bot’s logic, Two Sigma then decided to offer Halite to human and non-human coding enthusiasts worldwide for a one-time, three-month competition… fearing unintended consequences from a longer release.</p>
+
+                <p>To help productize and orchestrate the public launch, Two Sigma reached out to <a href="http://tech.cornell.edu/">Cornell Tech</a>, a leader in graduate tech education. With a shared interest in encouraging programming, entrepreneurship, and growth of the thriving New York tech scene, the two partners initiated the public Halite competition in November 2016. Cornell Tech is providing ongoing game support and community management, and by working closely with the online gaming community, it will empower players to collaborate, learn and have fun.</p>
+
+                <p>Many have contributed to Halite beyond Ben’s and Michael’s invention and engineering; they include Eric Abrego, Matt Adereth, Trammel Hudson, Emily Malloy, Arnaud Sahuguet, and Scott Spinner. And, no doubt, much credit should also be provided to users who are sending questions and comments to the <a href="http://forums.halite.io">forums</a>.</p>
+
+                <p>May the best bot win!</p>
+
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="script/backend.js"></script>
+    <script src="script/general.js"></script>
+</body>
+</html>
