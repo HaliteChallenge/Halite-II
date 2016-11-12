16f907644d48128c2cc2cd741fded5b151e6afd6
JCGrant <jamescolin.grant@gmail.com>
2016-11-12 04:36:54 +0000

    Add paragraph about refactoring the movement logic into it's own function

--- website/basics_improve_random.php
+++ website/basics_improve_random.php
@@ -59,7 +59,8 @@
                     <li>A zero strength piece that moves will necessarily stay at zero strength, because pieces don't gain strength for any turn that they move.</li>
                     <li>A zero strength piece won't ever conqueror any territory, because it has no strength with which to damage other pieces.</li>
                 </ul>
-                <p>Let's improve our bot by making sure that we tell all of our zero strength pieces to remain still.</p>
+                <p>Let's wrap the movement logic inside a function of it's own. This function will take the location of a piece and will return the piece's movement.</p>
+                <p>Now we can improve our bot by making sure that we tell all of our zero strength pieces to remain still.</p>
                 <span data-gist-id="5ea767bae8a725f738d75649e5f2c91a" data-gist-file="Revision1.py"></span>
 
                 <p>Our bot still moves its pieces around a lot (only a bit over one out of five turns will a piece stay still). This costs us a lot of strength (since a piece doesn't gain any strength on turns that it moves). To increase our utilization of our production, let's have pieces only move once their strength equals their production times some factor X. We're using 5 as the value of X in this example, but this is arbitrary.</p>
