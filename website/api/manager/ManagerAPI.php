825f8b3c4b501f09db2aec1946a9fa62d90a94ae
Michael Truell <michael_truell@horacemann.org>
2016-11-13 23:56:36 +0000

    Update matchmaking

--- website/api/manager/ManagerAPI.php
+++ website/api/manager/ManagerAPI.php
@@ -89,7 +89,7 @@ class ManagerAPI extends API{
             if((mt_rand() / mt_getrandmax()) > 0.5) {
                 $seedPlayer = $this->select("SELECT * FROM User WHERE isRunning = 1 order by rand()*-pow(sigma, 2) LIMIT 1");
             } else {
-                $seedID = $this->select("SELECT MAX(g.timestamp) as maxTime, gu.userID as userID FROM GameUser gu INNER JOIN Game g ON g.gameID=gu.gameID GROUP BY gu.userID ORDER BY maxTime ASC LIMIT 1")['userID'];
+                $seedID = $this->select("SELECT * FROM (SELECT MAX(g.timestamp) as maxTime, gu.userID as userID FROM GameUser gu INNER JOIN Game g ON g.gameID=gu.gameID GROUP BY gu.userID ORDER BY maxTime ASC limit 15) temptable order by rand() limit 1")['userID'];
                 $seedPlayer = $this->select("SELECT * FROM User where userID={$seedID}");
             }
             if(count($seedPlayer) < 1) return null;
