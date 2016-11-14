5c2ffa4321bbda0bb77dcaa78ad7811b1debbe8c
Michael Truell <truell20@gmail.com>
2016-11-14 07:07:15 -0500

    Switch to tabs for go

--- airesources/Go/RandomBot.go
+++ airesources/Go/RandomBot.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-    "hlt"
+	"hlt"
 
-    "math/rand"
+	"math/rand"
 )
 
 func main () {
-    conn, gameMap := hlt.NewConnection("RandomBot")
-    for {
-        var moves hlt.MoveSet
-        gameMap = conn.GetFrame()
-        for y := 0; y < gameMap.Height; y++ {
-            for x := 0; x < gameMap.Width; x++ {
-                loc := hlt.NewLocation(x,y)
-                if gameMap.GetSite(loc, hlt.STILL).Owner == conn.PlayerTag {
-                    moves = append(moves, hlt.Move {
-                        Location: loc,
-                        Direction: hlt.Direction(rand.Int() % 5),
+	conn, gameMap := hlt.NewConnection("RandomBot")
+	for {
+		var moves hlt.MoveSet
+		gameMap = conn.GetFrame()
+		for y := 0; y < gameMap.Height; y++ {
+			for x := 0; x < gameMap.Width; x++ {
+				loc := hlt.NewLocation(x,y)
+				if gameMap.GetSite(loc, hlt.STILL).Owner == conn.PlayerTag {
+					moves = append(moves, hlt.Move {
+						Location: loc,
+						Direction: hlt.Direction(rand.Int() % 5),
 
-                    })
-                }
-            }
-        }
-        conn.SendFrame(moves)
+					})
+				}
+			}
+		}
+		conn.SendFrame(moves)
 
-    }
+	}
 }
