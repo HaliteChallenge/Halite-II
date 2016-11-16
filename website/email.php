d03ddc6dd66c7e25f9042aff4a963783baa3f4fb
Michael Truell <truell20@gmail.com>
2016-11-15 22:23:37 -0500

    Initial work

---
+++ website/email.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'includes/header.php'; ?>
+
+    <title>Email Selection</title>
+
+    <link href="lib/bootstrap.min.css" rel="stylesheet">
+    <link href="style/general.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="pageContent">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1>Email Selection</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="script/backend.js"></script>
+    <script src="script/email.js"></script>
+</body>
+
+</html>
--- website/index.php
+++ website/index.php
@@ -73,8 +73,8 @@ if(isset($_SESSION['userID'])) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/seedrandom/2.4.0/seedrandom.min.js"></script>
-    <script src="script/general.js"></script>
     <script src="script/backend.js"></script>
+    <script src="script/general.js"></script>
     <script src="lib/pixi.min.js"></script>
     <script src="script/parsereplay.js"></script>
     <script src="script/visualizer.js"></script>
