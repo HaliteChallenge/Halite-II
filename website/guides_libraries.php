246375fa7c84274e30cb5a71a800c5e4f22813b1
Michael Truell <michael_truell@horacemann.org>
2016-11-25 16:27:24 -0500

    Revert "Include virtualenv"
    This reverts commit d8133b20d9edf6646ce2a8720c97d387985e6629.

--- website/guides_libraries.php
+++ website/guides_libraries.php
@@ -14,37 +14,27 @@
         <div class="row">
             <?php include 'includes/learn_sidebar.php'; ?>
             <div class="col-sm-9">
-                <h1>Using Package Managers</h1>
-                <p>The following package managers are included on the Halite game servers:</p>
+                <h1>Using 3rd Party Libraries</h1>
+                <p>If you add a bash file named install.sh to the same directory as your bot source, it will be run with internet access and write access to its current directory before any bot compilation. It may run for a maximum of 10 minutes.</p>
 
-                <ul>
-                    <li>pip3</li>
-                    <li>bundler</li>
-                    <li>npm</li>
-                </ul>
-
-                <p>The environment managers are included on the Halite game servers:</p>
-
-                <ul>
-                    <li>virtualenv</li>
-                </ul>
-
+                <h3>Package Managers</h3>
+                <p>You must install all libraries locally, even if through a package manager. Because of this, package managers that only support global installation (such as apt-get) may not be used.</p>
+                <p>The following package managers are installed ahead of time on the Halite game servers:</p>
+                    <ul>
+                        <li>pip3</li>
+                        <li>bundler</li>
+                        <li>npm</li>
+                    </ul>
                 <p>
-                    If you would like us to add another package manager or environment manager to this list, post on the <a href="http://forums.halite.io">forums</a>.
+                    If you would like us to add another package manager to this list, post on the <a href="http://forums.halite.io">forums</a>.
                 </p>
 
-                <p>To make calls your package manager, create an "install.sh" file in the root directory of your zip file submission and put any bash commands in it. Before compilation, that file will be executed with internet and write access to its current directory. It may run for a maximum of 10 minutes.</p>
+                <p>Here is an example install.sh file for a bot that uses <a href="http://www.numpy.org/">the numpy library</a>:</p>
+                <span data-gist-id="039bb1e4916ffb012d1ef83b36572dee"></span>
+                <p>It's just one line!</p>
 
-                <p>Note: All libraries have to be installed locally.</p>
-                
-                <h3>Examples</h3>
-
-                <p>Here is an example of an "install.sh" file that installs the <a href="http://keras.io/">the keras library</a> using virtualenv:</p>
-                <span data-gist-id="f7c873227c5b96c72b4374bf203acae1"></span>
-
-                <p>An install.sh for npm (just what you would expect):</p>
-                <span data-gist-id="510f9cd386ac5f00bf643a51a1c01df5"></span>
-
+                <h3>Compilation</h3>
+                <p>If your library isn't on a package manager that supports local installation, you are going to have to compile it on our game servers. Include the source of you library in your bot's zip file and put compilation instructions in the install.sh file.</p>
             </div>
         </div>
         <?php include 'includes/footer.php'; ?>
