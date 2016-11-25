ba85b9eb4468bbbf6c672083e957ba173cd900a9
Bovard Tiberi <bovard.tiberi@gmail.com>
2016-11-25 15:05:40 -0800

    removed extra )

--- website/api/manager/ManagerAPI.php
+++ website/api/manager/ManagerAPI.php
@@ -94,7 +94,7 @@ class ManagerAPI extends API{
             }
             if(count($seedPlayer) < 1) return null;
 
-            $muRankLimit = intval(5.0 / pow((float)mt_rand(1, mt_getrandmax())/(float)mt_getrandmax()), 0.65));
+            $muRankLimit = intval(5.0 / pow((float)mt_rand(1, mt_getrandmax())/(float)mt_getrandmax(), 0.65));
             $players = $this->selectMultiple("SELECT * FROM (SELECT * FROM User WHERE isRunning=1 and userID <> {$seedPlayer['userID']} ORDER BY ABS(mu-{$seedPlayer['mu']}) LIMIT $muRankLimit) muRankTable ORDER BY rand() LIMIT ".($numPlayers-1));
             array_push($players, $seedPlayer);
 
