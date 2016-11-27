e3dabd92b38c662df4cc47df7033e5de88adbfbf
Michael Truell <michael_truell@horacemann.org>
2016-11-27 16:48:07 +0000

    Only select seed players with < 250 games

--- website/api/manager/ManagerAPI.php
+++ website/api/manager/ManagerAPI.php
@@ -89,8 +89,7 @@ class ManagerAPI extends API{
             if((mt_rand() / mt_getrandmax()) > 0.5) {
                 $seedPlayer = $this->select("SELECT * FROM User WHERE isRunning = 1 order by rand()*-pow(sigma, 2) LIMIT 1");
             } else {
-                $seedID = $this->select("SELECT * FROM (SELECT MAX(g.timestamp) as maxTime, gu.userID as userID FROM GameUser gu INNER JOIN Game g ON g.gameID=gu.gameID GROUP BY gu.userID ORDER BY maxTime ASC limit 15) temptable order by rand() limit 1")['userID'];
-                $seedPlayer = $this->select("SELECT * FROM User where userID={$seedID}");
+                $seedPlayer = $this->select("SELECT u.* FROM (SELECT MAX(g.timestamp) as maxTime, gu.userID as userID FROM GameUser gu INNER JOIN Game g ON g.gameID=gu.gameID GROUP BY gu.userID) temptable INNER JOIN User u on u.userID = temptable.userID where numGames < 250 order by maxTime ASC limit 1");
             }
             if(count($seedPlayer) < 1) return null;
 
