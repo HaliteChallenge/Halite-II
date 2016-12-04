18f2751ba9b5bc8ed05bbb29e408fb0bc035bbf2
Michael Truell <truell20@gmail.com>
2016-12-04 16:01:17 -0500

    Add loading symbol to server games

--- website/game.php
+++ website/game.php
@@ -7,6 +7,29 @@
 
     <link href="lib/bootstrap.min.css" rel="stylesheet">
     <link href="style/general.css" rel="stylesheet">
+    <style type="text/css">   
+        .glyphicon-refresh-animate {
+            -animation: spin .7s infinite linear;
+            -webkit-animation: spin2 .7s infinite linear;
+        }
+        @-webkit-keyframes spin2 {
+            from {
+                -webkit-transform: rotate(0deg);
+            }
+            to {
+                -webkit-transform: rotate(360deg);
+            }
+        }
+        @keyframes spin {
+            from {
+                transform: scale(1) rotate(0deg);
+            }
+            to {
+                transform: scale(1) rotate(360deg);
+            }
+        }
+    </style>
+
 </head>
 <body>
     <div id="container" class="container">
