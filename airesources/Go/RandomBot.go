3b888f827c74494f2148189fe5b4327f93b5e8ae
Jerold <jerold.albertson@gmail.com>
2016-12-18 15:14:05 -0700

    pull sendName logic out of NewConnection
    Timeout for 1st round is 15 seconds. This change allows bots written in
    go to take advantage of that extra time.

--- airesources/Go/RandomBot.go
+++ airesources/Go/RandomBot.go
@@ -6,19 +6,19 @@ import (
 	"math/rand"
 )
 
-func main () {
-	conn, gameMap := hlt.NewConnection("RandomBot")
+func main() {
+	conn, gameMap := hlt.NewConnection()
+	conn.SendName("MyBot")
 	for {
 		var moves hlt.MoveSet
 		gameMap = conn.GetFrame()
 		for y := 0; y < gameMap.Height; y++ {
 			for x := 0; x < gameMap.Width; x++ {
-				loc := hlt.NewLocation(x,y)
+				loc := hlt.NewLocation(x, y)
 				if gameMap.GetSite(loc, hlt.STILL).Owner == conn.PlayerTag {
-					moves = append(moves, hlt.Move {
-						Location: loc,
+					moves = append(moves, hlt.Move{
+						Location:  loc,
 						Direction: hlt.Direction(rand.Int() % 5),
-
 					})
 				}
 			}
