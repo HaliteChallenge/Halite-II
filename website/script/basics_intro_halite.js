5099d0a576418ce2f352d275ee6aadd708da4fb1
Michael Truell <truell20@gmail.com>
2016-12-22 10:01:34 -0500

    Put a random bot replay on intro halite

--- website/script/basics_intro_halite.js
+++ website/script/basics_intro_halite.js
@@ -1,5 +1,5 @@
 $(function() {
-    var data = textFromURL("ar1481484242-3993659735.hlt", $("#gameReplay"), function(data) {
+    var data = textFromURL("ar1482413174-917235674.hlt", $("#gameReplay"), function(data) {
         console.log(data)
         if(data != null) {
             showGame(data, $("#gameReplay"), null, 500, true, true);
