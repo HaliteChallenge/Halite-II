f8aa93f0036136d01cbe98cc3b34b2e5e7a75118
Michael Truell <michael_truell@horacemann.org>
2016-12-23 21:57:19 -0500

    Store the last 500,000 games

--- website/api/manager/ManagerAPI.php
+++ website/api/manager/ManagerAPI.php
@@ -189,7 +189,7 @@ class ManagerAPI extends API{
             }
 
             // Check that we arent stoing too many games in db
-            $numAllowed = 100000;
+            $numAllowed = 500000;
             $res = mysqli_query($this->mysqli, "SELECT * FROM Game");
             $numRows = $res->num_rows;
             $numToDelete = $numRows - $numAllowed;
