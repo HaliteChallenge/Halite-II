762e1b01c324be8db0d6e945316bb9b7f811e7a9
Michael Truell <michael_truell@horacemann.org>
2016-12-26 12:44:38 -0500

    Update third party list

--- website/advanced_third_party.php
+++ website/advanced_third_party.php
@@ -16,34 +16,42 @@
             <div class="col-sm-9">
                 <h1>Third Party Resources</h1>
 
-                <p>Below is a list of useful and/or fun resources that were created by the Halite community.</p>
+                <p>Below is a list of useful and/or fun resources that were created by the Halite community. Especially popular resources will be <span class="glyphicon glyphicon-star" aria-hidden="true"></span>'ed.</p>
 
                 <h3>Tutorials</h3>
                 <ul>
-                   <li><a href="http://forums.halite.io/t/ml-starter-bot-tutorial/616/38">Machine Learning Starter Bot</a></li> 
-                   <li><a href="http://forums.halite.io/t/running-your-halite-bot-from-an-ide/70/10">Running Your Halite Bot from an IDE</a></li> 
-                   <li><a href="http://forums.halite.io/t/so-youve-improved-the-random-bot-now-what/482">So you've Improved the Random Bot. Now what?</a></li>
-                   <li><a href="http://forums.halite.io/t/early-mid-and-late-game/703">Early, Mid, and Late Game Strategy</a></li> 
+                   <li><a href="http://forums.halite.io/t/ml-starter-bot-tutorial/616/38">Machine Learning Starter Bot by brianvanleeuwen</a> <span class="glyphicon glyphicon-star" aria-hidden="true"></span></li> 
+                   <li><a href="http://forums.halite.io/t/running-your-halite-bot-from-an-ide/70/10">Running Your Halite Bot from an IDE by the Halite team</a></li> 
+                   <li><a href="http://forums.halite.io/t/so-youve-improved-the-random-bot-now-what/482">So you've Improved the Random Bot. Now what? by nmalaguti</a> <span class="glyphicon glyphicon-star" aria-hidden="true"></span></li>
+                </ul>
+
+                <h3>Strategy Writeups</h3>
+                <ul>
+                   <li><a href="http://forums.halite.io/t/early-mid-and-late-game/703">Early, Mid, and Late Game Strategy by nmalaguti</a> <span class="glyphicon glyphicon-star" aria-hidden="true"></span></li> 
+                   <li><a href="http://forums.halite.io/t/how-your-starting-position-impacts-your-bots-performance/769">How your starting position impacts your bots performance by nmalaguti</a></li> 
+                   <li><a href="http://forums.halite.io/t/perimeter-optimization-during-expansion/723">Perimeter Optimization by Sydriax</a></li> 
                 </ul>
 
                 <h3>Tools</h3>
                 <ul>
-                   <li><a href="http://forums.halite.io/t/a-stand-alone-game-viewer/615">3D visualizer</a></li> 
-                   <li><a href="http://forums.halite.io/t/auto-visualization-of-test-runs/422">Auto-reloading game visualizer</a></li> 
-                   <li><a href="http://forums.halite.io/t/cloudbots-compete-on-demand-against-some-of-my-bots/725">Cloudbots: locally test your bot against @nmalaguti's</a></li> 
-                   <li><a href="http://forums.halite.io/t/halite-swig-wrapper-for-the-game-engine/550">Game environment SWIG wrapper</a></li> 
-                   <li><a href="http://forums.halite.io/t/unofficial-match-manager-for-local-testing/505">Local tournament manager</li> 
-                   <li><a href="http://forums.halite.io/t/unofficial-halite-engine-clone-reloader/582">Multi-featured game environment clone</a></li> 
+                   <li><a href="http://forums.halite.io/t/cloudbots-compete-on-demand-against-some-of-my-bots/725">Cloudbots: locally test your bot against nmalaguti's</a></li> 
+                   <li><a href="http://forums.halite.io/t/halite-swig-wrapper-for-the-game-engine/550">Environment: halite environment SWIG wrapper</a></li> 
+                   <li><a href="http://forums.halite.io/t/unofficial-halite-engine-clone-reloader/582">Environment: multi-featured game environment clone</a></li> 
+                   <li><a href="http://forums.halite.io/t/unofficial-match-manager-for-local-testing/505">Local tournament manager</a> <span class="glyphicon glyphicon-star" aria-hidden="true"></span></li> 
+                   <li><a href="http://forums.halite.io/t/a-stand-alone-game-viewer/615">Visualizer: 3D visualizer</a></li> 
+                   <li><a href="http://forums.halite.io/t/auto-visualization-of-test-runs/422">Visualizer: Auto-reloading game visualizer</a></li> 
                 </ul>
 
                 <h3>Mini Competitions</h3>
                 <ul>
-                <li><a href="http://forums.halite.io/t/introducing-unofficial-halite-single-player-mode/573">Single player mode</a></li> 
+                <li><a href="http://forums.halite.io/t/introducing-unofficial-halite-single-player-mode/573">Single player expansion competition</a> <span class="glyphicon glyphicon-star" aria-hidden="true"></span></li> 
                 </ul>
 
                 <h3>Alternate Starter Packages</h3>
                 <ul>
-                   <li><a href="http://forums.halite.io/t/python3-much-alternative-starter-kit-asyncio-based-timeout-management-etc/611">Timeout-based alternative python starter package</a></li> 
+                   <li><a href="http://forums.halite.io/t/slightly-more-powerful-c-starter-package/767">C#: more powerful starter package</a></li> 
+                   <li><a href="http://forums.halite.io/t/python3-much-alternative-starter-kit-asyncio-based-timeout-management-etc/611">Python: asyncio timeout-based package</a></li> 
+                   <li><a href="http://forums.halite.io/t/python3-numpy-focused-all-i-want-is-matrices-starter-kit/766">Python: numpy matrix oriented starter package</a></li> 
                 </ul>
             </div>
         </div>
