d45574b8d06080705335c0d7214ec3d0990ea3f0
Sydriax <benjaminfspector@gmail.com>
2016-12-30 17:51:15 -0500

    Changed replay to a new favorite

--- website/script/index.js
+++ website/script/index.js
@@ -1,6 +1,6 @@
 $(function() {
     $("#numUsers").html(getNumActiveUsers());
-    var data = textFromURL("ar1478846062-2923329127.hlt", $("#gameReplay"), function(data) {
+    var data = textFromURL("ar1482947270-2437412300.hlt", $("#gameReplay"), function(data) {
         console.log(data)
         if(data != null) {
             showGame(data, $("#gameReplay"), null, 500, true, true);
