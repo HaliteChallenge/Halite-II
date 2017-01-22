d11512e324091d537fb4af70bcdf9b5072da2376
Benjamin Spector <benjaminfspector@gmail.com>
2017-01-22 16:22:06 -0500

    switched to static build

--- environment/make.bat
+++ environment/make.bat
@@ -1,4 +1,4 @@
 SET PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 cd environment
-cl.exe /O2 /EHsc main.cpp core/Halite.cpp /I . networking/Networking.cpp /link /out:halite.exe
+cl.exe /O2 /MT /EHsc main.cpp core/Halite.cpp /I . networking/Networking.cpp /link /out:halite.exe
