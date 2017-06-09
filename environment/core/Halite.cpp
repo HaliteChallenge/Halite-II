a9264cf30f7320d44e91c2b8de051e0010ce2af6
David Li <David.M.Li@TwoSigma.com>
2017-06-09 17:05:44 -0400

    Make planet collision damage depend on ship HP too

--- environment/core/Halite.cpp
+++ environment/core/Halite.cpp
@@ -27,7 +27,8 @@ auto Halite::compute_damage(hlt::EntityId self_id, hlt::EntityId other_id)
     switch (other_id.type) {
         case hlt::EntityType::PlanetEntity:
             other_damage =
-                static_cast<unsigned short>(20 * self.velocity.magnitude());
+                static_cast<unsigned short>(
+                    (self.health / 40) * self.velocity.magnitude());
             break;
         case hlt::EntityType::ShipEntity: {
             const auto& other = game_map.get_ship(other_id);
