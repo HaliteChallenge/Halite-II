9c70f259824258da1c6873668fc6585b43619226
David Li <David.M.Li@TwoSigma.com>
2017-06-22 15:44:30 -0400

    Start remote workers via screen (#73)

--- apiserver/worker/startWorkerScreen.sh
+++ apiserver/worker/startWorkerScreen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if ! screen -list | grep -q "worker"; then
-    screen -S worker -Ldm bash -c "python3 worker.py"
+    screen -S worker -Ldm bash -c "cd /worker; python3 worker.py"
 fi
