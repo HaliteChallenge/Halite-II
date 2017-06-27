e2f0d06e2520fbb4827487a5e9dc8ea5482d1518
David Li <David.M.Li@TwoSigma.com>
2017-06-27 18:12:38 -0400

    Get rid of extra slash in API URLs

--- apiserver/apiserver/server.py
+++ apiserver/apiserver/server.py
@@ -4,4 +4,4 @@ from . import web
 
 
 app.register_blueprint(login.oauth_login, url_prefix="/login")
-app.register_blueprint(web.web_api, url_prefix="/api/v1/")
+app.register_blueprint(web.web_api, url_prefix="/api/v1")
