9912212401ba152ae46459e12a85206de1171f30
David Li <David.M.Li@TwoSigma.com>
2017-07-03 09:09:20 -0400

    Update to new proposed schema

--- apiserver/sql/schema.sql
+++ apiserver/sql/schema.sql
@@ -1,114 +1,111 @@
 CREATE DATABASE IF NOT EXISTS halite2;
-use halite2;
+USE halite2;
 
-DROP TABLE IF EXISTS `Organization`;
-CREATE TABLE `Organization` (
-  `organizationID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `level` enum('High School','Undergraduate','Graduate','Professional') NOT NULL DEFAULT 'Professional',
-  `organizationName` VARCHAR(64) NOT NULL
+CREATE TABLE organization (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  organization_name VARCHAR(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `OrganizationEmailDomain`;
-CREATE TABLE `OrganizationEmailDomain` (
-    `organizationID` INT NOT NULL,
-    `domain` VARCHAR(64) NOT NULL,
-    PRIMARY KEY (`organizationID`, `domain`),
-    FOREIGN KEY (`organizationID`) REFERENCES Organization(`organizationID`)
+CREATE TABLE organization_email_domain (
+  organization_id INT NOT NULL,
+  domain VARCHAR(64) NOT NULL,
+  PRIMARY KEY (organization_id, domain),
+  FOREIGN KEY (organization_id) REFERENCES organization(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `User`;
-CREATE TABLE `User` (
-  `userID` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `oauthID` int(12) unsigned NOT NULL,
-  `oauthProvider` tinyint(1) unsigned NOT NULL,
-  `username` varchar(32) NOT NULL,
-  `email` varchar(64),
-  `githubEmail` varchar(64),
-  `verificationCode` varchar(64),
-  `onEmailList` tinyint(1) NOT NULL DEFAULT 1,
-  `isEmailGood` tinyint(1) unsigned NOT NULL DEFAULT 0,
-  `isRunning` tinyint(1) unsigned NOT NULL DEFAULT 0,
-  `compileStatus` tinyint(1) unsigned NOT NULL DEFAULT 0,
-  `compileStart` datetime,
-  `level` enum('High School','Undergraduate','Graduate','Professional') NOT NULL DEFAULT 'Professional',
-  `organizationID` INT,
-  # ISO-3166 3-letter country code
-  `countryCode` VARCHAR(3),
-  # ISO-3166-2 variable length country code
-  `countrySubdivisionCode` VARCHAR(10),
-  `countryVisible` BOOL DEFAULT FALSE,
-  `language` varchar(16) DEFAULT NULL,
-  `mu` float NOT NULL DEFAULT 25.000,
-  `sigma` float unsigned NOT NULL DEFAULT 8.333,
-  `rank` smallint(5) DEFAULT NULL,
-  `numSubmissions` smallint(5) NOT NULL DEFAULT 0,
-  `numGames` smallint(5) NOT NULL DEFAULT 0,
-  `creationTime` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updateTime` datetime ON UPDATE CURRENT_TIMESTAMP,
-  `apiKeyHash` VARCHAR(255),
-  FOREIGN KEY (`organizationID`) REFERENCES Organization(`organizationID`),
-  PRIMARY KEY (`userID`)
+CREATE TABLE player (
+  id MEDIUMINT(8) UNSIGNED NOT NULL AUTO_INCREMENT,
+  oauth_id INT(12) UNSIGNED NOT NULL,
+  oauth_provider TINYINT(1) UNSIGNED NOT NULL,
+  username VARCHAR(32) NOT NULL,
+  email VARCHAR(64),
+  github_email VARCHAR(64),
+  verification_code VARCHAR(64),
+  is_active BOOL NOT NULL DEFAULT TRUE,
+  -- TODO: port this over
+  on_email_list BOOL NOT NULL DEFAULT 1,
+  is_email_good BOOL NOT NULL DEFAULT 0,
+  player_level ENUM('High School','Undergraduate','Graduate','Professional') NOT NULL DEFAULT 'Professional',
+  organization_id INT,
+  -- ISO-3166 3-letter country code
+  country_code VARCHAR(3),
+  -- ISO-3166-2 variable length country code
+  country_subdivision_code VARCHAR(10),
+  -- countryVisible BOOL DEFAULT FALSE,
+  creation_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+  update_time DATETIME ON UPDATE CURRENT_TIMESTAMP,
+  api_key_hash VARCHAR(255),
+  FOREIGN KEY (organization_id) REFERENCES organization(id),
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `Game`;
-CREATE TABLE `Game` (
-  `gameID` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `replayName` varchar(64) NOT NULL,
-  `mapWidth` smallint(5) NOT NULL,
-  `mapHeight` smallint(5) NOT NULL,
-  `timestamp` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`gameID`)
-) ENGINE=InnoDB AUTO_INCREMENT=407227 DEFAULT CHARSET=utf8;
+CREATE TABLE bot (
+  player_id MEDIUMINT(8) UNSIGNED NOT NULL,
+  id MEDIUMINT(8) UNSIGNED NOT NULL,
+  is_running BOOL NOT NULL DEFAULT 0,
+  compile_status ENUM('Uploaded', 'InProgress', 'Successful', 'Failed') NOT NULL,
+  compile_start DATETIME,
+  language VARCHAR(16) DEFAULT NULL,
+  -- # of times submitted = version number
+  version_number SMALLINT(5) NOT NULL DEFAULT 0,
+  games_played SMALLINT(5) NOT NULL DEFAULT 0,
+  mu FLOAT NOT NULL DEFAULT 25.000,
+  sigma FLOAT UNSIGNED NOT NULL DEFAULT 8.333,
+  score FLOAT NOT NULL DEFAULT 0,
+  FOREIGN KEY (player_id) REFERENCES player(id),
+  PRIMARY KEY (player_id, id)
+);
 
-DROP TABLE IF EXISTS `GameUser`;
-CREATE TABLE `GameUser` (
-  `gameID` mediumint(8) unsigned NOT NULL,
-  `userID` mediumint(8) unsigned NOT NULL,
-  `versionNumber` mediumint(8) unsigned NOT NULL,
-  `errorLogName` varchar(64) DEFAULT NULL,
-  `rank` smallint(5) unsigned NOT NULL,
-  `playerIndex` smallint(5) unsigned NOT NULL,
-  `didTimeout` tinyint(1) unsigned NOT NULL,
-  FOREIGN KEY (`gameID`) REFERENCES Game(`gameID`),
-  FOREIGN KEY (`userID`) REFERENCES User(`userID`),
-  PRIMARY KEY (`gameID`,`userID`)
+CREATE TABLE game (
+  id MEDIUMINT(8) UNSIGNED NOT NULL AUTO_INCREMENT,
+  replay_name VARCHAR(64) NOT NULL,
+  map_width SMALLINT(5) NOT NULL,
+  map_height SMALLINT(5) NOT NULL,
+  time_played DATETIME DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `UserHistory`;
-CREATE TABLE `UserHistory` (
-  `userID` mediumint(8) unsigned NOT NULL,
-  `versionNumber` smallint(5) NOT NULL,
-  `lastRank` smallint(5) NOT NULL,
-  `lastNumPlayers` smallint(5) NOT NULL,
-  `lastNumGames` smallint(5) DEFAULT NULL,
-  `timestamp` datetime DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (`userID`) REFERENCES User(`userID`)
+CREATE TABLE game_participant (
+  game_id MEDIUMINT(8) UNSIGNED NOT NULL,
+  player_id MEDIUMINT(8) UNSIGNED NOT NULL,
+  bot_id MEDIUMINT(8) UNSIGNED NOT NULL,
+  version_number MEDIUMINT(8) UNSIGNED NOT NULL,
+  log_name VARCHAR(64) DEFAULT NULL,
+  rank SMALLINT(5) UNSIGNED NOT NULL,
+  player_index SMALLINT(5) UNSIGNED NOT NULL,
+  timed_out BOOL NOT NULL,
+  FOREIGN KEY (game_id) REFERENCES game(id),
+  FOREIGN KEY (player_id) REFERENCES player(id),
+  FOREIGN KEY (player_id, bot_id) REFERENCES bot(player_id, id),
+  PRIMARY KEY (game_id, player_id, bot_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `Announcement`;
-CREATE TABLE `Announcement` (
-  `announcementID` mediumint(8) unsigned NOT NULL,
-  `header` varchar(64) NOT NULL,
-  `body` varchar(512) NOT NULL,
-  PRIMARY KEY (`announcementID`)
+-- A history of submitted bots, recorded whenever a new one is submitted.
+-- This intends to prevent players from keeping their bots in "stealth", where
+-- they submit bots, wait for the rank to stabilize, then take them down.
+CREATE TABLE bot_history (
+  player_id MEDIUMINT(8) UNSIGNED NOT NULL,
+  bot_id MEDIUMINT(8) UNSIGNED NOT NULL,
+  version_number SMALLINT(5) NOT NULL,
+  last_rank SMALLINT(5) NOT NULL,
+  last_score SMALLINT(5) NOT NULL,
+  -- The count of active players when this bot was retired.
+  last_num_players SMALLINT(5) NOT NULL,
+  -- The number of games this bot has played.
+  last_num_games SMALLINT(5) DEFAULT NULL,
+  when_retired DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (player_id) REFERENCES player(id),
+  FOREIGN KEY (player_id, bot_id) REFERENCES bot(player_id, id),
+  PRIMARY KEY (player_id, bot_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `DoneWithAnnouncement`;
-CREATE TABLE `DoneWithAnnouncement` (
-  `announcementID` mediumint(8) unsigned NOT NULL,
-  `userID` mediumint(8) unsigned NOT NULL,
-  FOREIGN KEY (`userID`) REFERENCES User(`userID`),
-  FOREIGN KEY (`announcementID`) REFERENCES Announcement(`announcementID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `UserNotification`;
-CREATE TABLE `UserNotification` (
-  `userNotificationID` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `userID` mediumint(8) unsigned NOT NULL,
-  `title` varchar(64) NOT NULL,
-  `body` varchar(2048) NOT NULL,
-  `mood` tinyint(1) NOT NULL,
-  `creationTime` datetime DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (`userID`) REFERENCES User(`userID`),
-  PRIMARY KEY (`userNotificationID`)
+CREATE TABLE player_notification (
+  id MEDIUMINT(8) UNSIGNED NOT NULL AUTO_INCREMENT,
+  player_id mediumint(8) unsigned NOT NULL,
+  title varchar(64) NOT NULL,
+  body varchar(2048) NOT NULL,
+  mood ENUM("error", "neutral", "success") NOT NULL DEFAULT "neutral",
+  creation_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (player_id) REFERENCES player(id),
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
