587f17729ddcfc83d3360ee1502e4ccbc38ae37d
Harikrishna Menon <harikm@microsoft.com>
2017-07-04 13:32:56 -0400

    Added support for local debugging
    Cleaned up a few things

--- musicgenerator/main.py
+++ musicgenerator/main.py
@@ -1,10 +1,13 @@
 import time
 import requests
 import json
+import os
 
 from google.cloud import storage
 
 PROJECT = 'halite-2'
+
+# Amper docs - https://docs.ampermusic.com/jimmy/
 
 # move to key management soon, create a new key and disable the current one
 AMPER_KEY = 'wLntjWsRjS3PpZZsIKvbLvIbWTEdLwVkQ7kznKi3DrU5Y4Ut7mKmxQnMjTpYomLm'
@@ -12,6 +15,8 @@ AMPER_KEY = 'wLntjWsRjS3PpZZsIKvbLvIbWTEdLwVkQ7kznKi3DrU5Y4Ut7mKmxQnMjTpYomLm'
 MUSIC_BUCKET = 'ampermusichalite2'
 
 AMPER_BASE_API_URL = 'https://jimmy.ampermusic.com/v1/'
+
+LOCAL_DEBUG = 'false'
 
 def ampergenerate():
     var = 1
@@ -27,30 +32,44 @@ def ampergenerate():
         response = requests.request("POST", url + 'projects', data=payload, headers=headers)
         print(response.text)
         responsebody = json.loads(response.text)
-        id = responsebody['id']
+        projectid = responsebody['id']
+
         if(responsebody['status'] == 'waiting_create'):
+            # wait untill render is done
             while(var == 1):
-                statusresponse = requests.request("GET", url + 'projects/' + str(id), data=payload, headers=headers)
+                statusresponse = requests.request("GET", url + 'projects/' + str(projectid), data=payload, headers=headers)
                 print(statusresponse.text)
                 postcreatedata = json.loads(statusresponse.text)
                 if (postcreatedata['status'] == 'waiting_create'):
                     time.sleep(5)
                     continue
                 else:
-                    file = postcreatedata['files'][0]
+                    # getting the mp3 version, can change to  wav if needed
+                    file = postcreatedata['files'][1]
+
+                    # dict keys are mangled, this is flaky, need to fix before release
                     rid = file.values()[8]
                     response = requests.request("GET", url + 'render_files/' + str(rid), data=payload, headers=headers)
                     content = response.content
-                    fileid = str(rid) + '.wav'
-                    wavfile = open(fileid, 'w')
-                    wavfile.write(bytes(content))
-                    wavfile.close()
+                    fileid = str(rid) + '.mp3'
+
+                    # write audio files to disk for debugging.
+                    audiofile = open(fileid, 'w')
+                    audiofile.write(bytes(content))
+                    audiofile.close()
+
+
+                    # uploading to cloud storage
                     storage_client = storage.Client(PROJECT)
                     bucket_name = MUSIC_BUCKET
                     bucket = storage_client.get_bucket(bucket_name)
                     blob = bucket.blob(fileid)
                     blob.upload_from_filename(fileid)
+
                     print('File uploaded')
+
+                    if (LOCAL_DEBUG == 'false'):
+                        os.remove(fileid)
                     break
 
 if __name__ == '__main__':
