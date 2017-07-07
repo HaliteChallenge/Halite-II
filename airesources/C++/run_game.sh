ac6dec9f0bdd4ce337c373961d7ecc2285c4a3b2
David Li <David.M.Li@TwoSigma.com>
2017-07-07 14:20:41 -0400

    Make sure sample bots fit required naming scheme

--- airesources/C++/run_game.sh
+++ airesources/C++/run_game.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-clang++ -std=c++11 -g -Wall -pedantic -fsanitize=address,undefined my_bot.cpp -o my_bot
-../../environment/halite -d "96 96" "./my_bot" "./my_bot"
+clang++ -std=c++11 -g -Wall -pedantic -fsanitize=address,undefined MyBot.cpp -o MyBot
+../../environment/halite -d "96 96" "./MyBot" "./MyBot"
--- airesources/Python3/run_game.sh
+++ airesources/Python3/run_game.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./halite -d "96 96" "python3 my_bot.py" "python3 my_bot.py"
+./halite -d "96 96" "python3 MyBot.py" "python3 MyBot.py"
