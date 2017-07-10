b0c6ecda95c1fb74cc9f1d956ac9875057aeeed5
David Li <David.M.Li@TwoSigma.com>
2017-07-10 18:59:42 -0400

    Revert "Try improving performance by not letting Vue observe the visualizer"
    This reverts commit 70f0be42391b55b872aa3c87e016697d4c73a44e.

--- libhaliteviz/src/HaliteHud.vue
+++ libhaliteviz/src/HaliteHud.vue
@@ -10,11 +10,11 @@
             <button class="btn btn-default" v-if="playing" v-on:click="pause">Pause</button>
             <button class="btn btn-default" v-else v-on:click="play">Play</button>
 
-            Frame: <input type="text" :value="frame + '.' + substep" />
+            Frame: <input type="text" :value="visualizer.frame + '.' + visualizer.substep" />
         </div>
 
         <input type="range" min="0" :max="replay.frames.length - 1"
-               :value="frame"
+               :value="visualizer.frame"
                v-on:input="scrub" />
 
         <div class="halite-visualizer-canvas" ref="visualizer_container">
@@ -68,52 +68,38 @@
                     }
                     return '#' + color;
                 }),
+                visualizer: new HaliteVisualizer(this.replay),
                 selected: {
                     kind: null,
                 },
-                frame: 0,
-                substep: 0,
-                playing: false,
             };
         },
         mounted: function() {
-            const visualizer = new HaliteVisualizer(this.replay);
-            visualizer.attach(this.$refs.visualizer_container);
-            visualizer.onSelect = (kind, args) => {
+            this.visualizer.attach(this.$refs.visualizer_container);
+            this.visualizer.onSelect = (kind, args) => {
                 this.selected.kind = "planet";
                 this.selected.id = args.id;
                 this.$forceUpdate();
             };
-            visualizer.onPlay = () => {
-                this.playing = true;
-            };
-            visualizer.onPause = visualizer.onEnd = () => {
-                this.playing = false;
-            };
-            visualizer.onUpdate = () => {
-                this.frame = visualizer.frame;
-                this.substep = visualizer.substep;
-                this.$forceUpdate();
-            };
-            visualizer.play();
-            this.play = () => visualizer.play();
-            this.pause = () => visualizer.pause();
-            this.scrub = (e) => {
-                visualizer.pause();
-                visualizer.substep = 0;
-                visualizer.frame = e.target.value;
-            };
+            this.visualizer.play();
         },
         methods: {
-            // Stubs, filled in above
             play: function() {
+                this.visualizer.play();
             },
             pause: function() {
+                this.visualizer.pause();
             },
             scrub: function(e) {
+                this.visualizer.pause();
+                this.visualizer.substep = 0;
+                this.visualizer.frame = e.target.value;
             }
         },
         computed: {
+            playing: function() {
+                return this.visualizer.isPlaying();
+            },
             statistics: function() {
                 let count = {};
                 for (let i = 0; i < this.replay.num_players; i++) {
@@ -123,7 +109,7 @@
                     };
                 }
 
-                let substep = this.replay.frames[this.frame][this.substep];
+                let substep = this.replay.frames[this.visualizer.frame][this.visualizer.substep];
                 for (let ship of substep.ships) {
                     count[ship.owner].ships++;
                 }
@@ -138,14 +124,14 @@
             },
             selected_planet: function() {
                 if (this.selected.kind === "planet") {
-                    let substep = this.replay.frames[this.frame][this.substep];
+                    let substep = this.replay.frames[this.visualizer.frame][this.visualizer.substep];
                     return {
                         base: this.replay.planets[this.selected.id],
                         state: substep.planets[this.selected.id],
                     };
                 }
                 return null;
-            },
+            }
         },
     };
 </script>
