a7b9e1d5fbddb4217b117429725907a55ec3a691
David Li <David.M.Li@TwoSigma.com>
2017-07-11 16:21:10 -0400

    Update profile page reflecting new API field names

--- website/javascript/templates/UserProfile.vue
+++ website/javascript/templates/UserProfile.vue
@@ -10,8 +10,9 @@
             <section>
                 <h2>Stats Summary</h2>
                 <p><b>Points:</b> {{ Math.round(user.score * 100) / 100 }}</p>
-                <p><b>Games Played:</b>  {{ user.total_games_played }}</p>
-                <p><b>Submissions:</b>  {{ user.total_submissions }}</p>
+                <p v-if="user.rank"><b>Rank:</b> {{ user.rank }}</p>
+                <p><b>Games Played:</b>  {{ user.num_games }}</p>
+                <p><b>Submissions:</b>  {{ user.num_submissions }}</p>
                 <p v-for="bot in bots">
                     Bot version {{ bot.version_number }} rank {{ bot.rank }} written in {{ bot.language }} with {{ bot.games_played }} games played
                     <span v-if="bot.compilation_status">(latest version compilation status: {{ bot.compilation_status }})</span>
