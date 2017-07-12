8665bddeefb00df13ce0e9a0767cc5a6a5d0aa6a
David Li <David.M.Li@TwoSigma.com>
2017-07-12 10:45:58 -0400

    Update run_game scripts

--- airesources/C++/run_game.bat
+++ airesources/C++/run_game.bat
@@ -2,4 +2,4 @@ SET PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 cl.exe /std:c++14 /O2 /MT /EHsc .\MyBot.cpp /link /out:MyBot.exe
 
-..\..\environment\halite.exe -d "96 96" ".\MyBot.exe" ".\MyBot.exe"
+.\halite.exe -d "96 96" ".\MyBot.exe" ".\MyBot.exe"
--- airesources/C++/run_game.sh
+++ airesources/C++/run_game.sh
@@ -3,4 +3,4 @@
 set -e
 
 clang++ -std=c++11 -g -Wall -pedantic -fsanitize=address,undefined MyBot.cpp -o MyBot
-../../environment/halite -d "96 96" "./MyBot" "./MyBot"
+./halite -d "96 96" "./MyBot" "./MyBot"
