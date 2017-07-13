32088631492075bc105fafba1399361b067d8b07
David Li <David.M.Li@TwoSigma.com>
2017-07-13 18:23:57 -0400

    Update game constants in Java starter kit

--- airesources/Java/Constants.java
+++ airesources/Java/Constants.java
@@ -7,17 +7,14 @@ public class Constants {
     public static final int MAX_ACCELERATION = 10;
 
     public static final short MAX_SHIP_HEALTH = 255;
-    public static final short BASE_SHIP_HEALTH = 255;
-    public static final short DOCKED_SHIP_REGENERATION = 0;
+    public static final short BASE_SHIP_HEALTH = 127;
+    public static final short DOCKED_SHIP_REGENERATION = 32;
 
     public static final int WEAPON_COOLDOWN = 1;
     public static final int WEAPON_RADIUS = 5;
-    public static final int WEAPON_DAMAGE = 128;
-    public static final int EXPLOSION_RADIUS = 5;
+    public static final int WEAPON_DAMAGE = 64;
 
     public static final int MAX_DOCKING_DISTANCE = 4;
     public static final int DOCK_TURNS = 5;
     public static final int PRODUCTION_PER_SHIP = 100;
-    public static final int BASE_PRODUCTIVITY = 25;
-    public static final int ADDITIONAL_PRODUCTIVITY = 25;
 }
