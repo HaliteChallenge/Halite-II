84b4194b7d30a477403ed7145e38ebef5a349b6d
David Li <David.M.Li@TwoSigma.com>
2017-07-27 13:53:18 -0400

    Make sure necessary environment variables for GPU workers loaded

--- apiserver/worker/setup.sh
+++ apiserver/worker/setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# On GPU workers, environment variables needed to set up CUDA properly are
+# written to the profile, so make sure those variables actually get loaded.
+source ~/.profile
+
 cd ../../environment
 make -j2
 
