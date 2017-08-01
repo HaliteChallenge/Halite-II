51ff39138c0007a3028f4ec7a39c2cdd36668f47
David Li <David.M.Li@TwoSigma.com>
2017-08-01 10:39:08 -0400

    Move bulk of coordinator startup to the blob
    This is the same setup as the worker. This makes it easier to
    change, since we don't have to recreate the instance template.

--- admin/setup_coordinator__startup_script.sh
+++ admin/setup_coordinator__startup_script.sh
@@ -14,23 +14,6 @@ done
 tar xvzf Halite.tgz
 cd Halite/apiserver/
 
-virtualenv --python=python3.6 venv
-
-source venv/bin/activate
-pip install -r requirements.txt
-
-wget -O cloud_sql_proxy https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64
-chmod +x ./cloud_sql_proxy
-
-DB_INSTANCE="$(python -m apiserver.scripts.print_db_proxy_instance)"
-
-screen -S sqlproxy -d -m /bin/bash -c \
-    "./cloud_sql_proxy -instances=${DB_INSTANCE}=tcp:3307"
-
-screen -S api -d -m /bin/bash -c \
-    "PYTHONPATH=$(pwd) FLASK_APP=apiserver.server FLASK_DEBUG=true flask run -h 0.0.0.0 -p 5000"
-
-screen -S coordinator_internal -d -m /bin/bash -c \
-    "PYTHONPATH=$(pwd) FLASK_APP=apiserver.coordinator_server FLASK_DEBUG=true flask run -h 0.0.0.0 -p 5001"
+bash ./setup.sh
 
 EOF
