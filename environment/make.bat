ad2b003018852b0ecd4082c6472bef7fdf26ecae
David Li <David.M.Li@TwoSigma.com>
2017-08-03 16:06:47 -0400

    Fix build on Windows

--- environment/make.bat
+++ environment/make.bat
@@ -3,6 +3,7 @@ CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd6
 cl.exe /std:c++14 /O2 /MT /EHsc /I . ^
  .\main.cpp ^
  .\core\Constants.cpp .\core\Entity.cpp .\core\GameEvent.cpp .\core\Halite.cpp .\core\hlt.cpp .\core\SimulationEvent.cpp ^
+ .\core\Replay.cpp .\core\Statistics.cpp ^
  .\networking\Networking.cpp .\networking\BotInputError.cpp ^
- .\core\mapgen\Generator.cpp .\core\mapgen\SolarSystem.cpp ^
+ .\core\mapgen\Generator.cpp .\core\mapgen\SolarSystem.cpp .\core\mapgen\AsteroidCluster.cpp ^
  .\miniz\miniz.c /link /out:halite.exe
