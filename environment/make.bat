ffb0f917577b67028abf68c2c2147812772d0d28
David Li <David.M.Li@TwoSigma.com>
2017-08-08 09:57:13 -0400

    Update Windows build script for zSTD

--- environment/make.bat
+++ environment/make.bat
@@ -1,6 +1,14 @@
+CD zstd-1.3.0\build\VS_scripts
+
+CALL .\build.VS2015.cmd
+COPY .\bin\Release\x64\libzstd_static.lib ..\..\..
+
+CD ..\..\..
+
 SET PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 cl.exe /std:c++14 /O2 /MT /EHsc /I . ^
+ .\libzstd_static.lib ^
  .\main.cpp ^
  .\core\Constants.cpp .\core\Entity.cpp .\core\GameEvent.cpp .\core\Halite.cpp .\core\hlt.cpp .\core\SimulationEvent.cpp ^
  .\core\Replay.cpp .\core\Statistics.cpp ^
