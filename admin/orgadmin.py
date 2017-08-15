f3599adc55cb5392ebf8cb87e2f453a041fab811
Harikrishna Menon <harikrishna.menon@gmail.com>
2017-08-15 11:16:18 -0400

    Added org admin script

--- admin/orgadmin.py
+++ admin/orgadmin.py
@@ -17,18 +17,41 @@ api_server_url_base = "http://35.190.3.178/v1/api/organization"
 email_endpoint = "/email_domain"
 
 def main(argv):
-    get_org_info()
-    add_org("Harvard University", "University")
+    # get_org_info()
+    # bulk_add_publicuni_data()
+    
+
+def bulk_add_publicuni_data():
+    count = 0
+    response = requests.request("GET", "https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json")
+    response_data = json.loads(response.text)
+    for org in response_data:
+        try:
+            add_org(org["name"], "University",org["domain"])
+            count = count + 1
+        except ValueError:
+            print("Oops! " + str(count))
+
+    print("Total Failures: " + count)
 
 # Add email to org
-def add_org(name, type):
+def add_org(name, type, domain):
     data = {}
     data['name'] = name
     data['type'] = type
-    data['require_code'] = False
+    data['require_code'] = "false"
+    org_data = json.dumps(data)
+    response = requests.request("POST", api_server_url_base, data=org_data, headers=post_headers)
+    response_data = json.loads(response.text)
+    print(response_data)
+    add_org_email(response_data["organization_id"], domain)
+
+def add_org_email(id, domain):
+    data = {}
+    data['domain'] = domain
     org_data = json.dumps(data)
     print(org_data)
-    response = requests.request("POST", api_server_url_base, data=org_data, headers=post_headers)
+    response = requests.request("POST", api_server_url_base +"/"+ str(id) + email_endpoint, data=org_data, headers=post_headers)
     print(response.text)
 
 
