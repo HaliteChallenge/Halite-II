bf96c174657b47c79f37e97a50dfd18a6d3b9dff
Harikrishna Menon <harikrishna.menon@gmail.com>
2017-08-25 07:03:52 -0400

    Updated remote deploy

--- website/deploy_site_remote.sh
+++ website/deploy_site_remote.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+
 # python3 make_starter_kits.py path/to/folder/with/compiled/halite/environments
+
 npm install
 cd ../libhaliteviz
 npm install
@@ -7,3 +9,4 @@ cd ../website
 npm run build
 bundle exec jekyll build
 gcloud compute scp --recurse _site/* ubuntu@halite-web-server-staging:/var/www/html
+
