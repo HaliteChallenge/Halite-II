4b85c88fd23e7762fa0c2f929a7c7467f517e59d
Harikrishna Menon <harikrishna.menon@gmail.com>
2017-08-25 09:47:39 -0400

    More changes to deployment scripts

--- website/deploy_site_local.sh
+++ website/deploy_site_local.sh
@@ -4,7 +4,7 @@ bundle install --path=vendor/bundle
 
 # Set up builds folder with Windows.exe, Linux-x64, MacOS halite environment executables
 # you need make sure that website/assets/downloads and website/_data needs to be created for this script to work
-python3 make_starter_kits.py environment_dir ../builds/
+python3 make_starter_kits.py 0.9.0-Beta ../builds/
 
 npm install
 cd ../libhaliteviz
