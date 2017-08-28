d21ec8e8ec72584397a1ff348efe8054e63843ec
Snaar <snaar@snaar.net>
2017-08-27 21:04:28 -0400

    Windows: output all cpp and linker build artifacts (but not halite.exe itself) into obj dir to reduce main dir clutter.

--- environment/make2017.bat
+++ environment/make2017.bat
@@ -1,9 +1,11 @@
 rem TODO: add version and edition autodetection and merge into make.bat
 
+mkdir obj
+
 CD zstd-1.3.0\build\VS_scripts
 
 CALL .\build.VS2017.cmd
-COPY .\bin\Release\x64\libzstd_static.lib ..\..\..
+COPY .\bin\Release\x64\libzstd_static.lib ..\..\..\obj
 
 CD ..\..\..
 
@@ -12,11 +14,11 @@ SET PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\1
 SET "VSCMD_START_DIR=%CD%"
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 
-cl.exe /std:c++14 /O2 /MT /EHsc /I . ^
- .\libzstd_static.lib ^
+cl.exe /std:c++14 /O2 /MT /EHsc /I . /Fo.\obj\ ^
+ .\obj\libzstd_static.lib ^
  .\main.cpp ^
  .\core\Constants.cpp .\core\Entity.cpp .\core\GameEvent.cpp .\core\Halite.cpp .\core\hlt.cpp .\core\SimulationEvent.cpp ^
  .\core\Replay.cpp .\core\Statistics.cpp ^
  .\networking\Networking.cpp .\networking\BotInputError.cpp ^
  .\core\mapgen\Generator.cpp .\core\mapgen\SolarSystem.cpp .\core\mapgen\AsteroidCluster.cpp ^
- .\miniz\miniz.c /link /out:halite.exe
+ .\miniz\miniz.c /link /implib:obj\halite.lib /out:halite.exe
