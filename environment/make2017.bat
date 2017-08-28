c9a311af8e95846cbe2a00bd984cb49366e45ffb
Snaar <snaar@snaar.net>
2017-08-28 15:09:22 -0400

    Windows build touch-up (#270)
    * Windows: remove unnecessary build targets from zstd to speed up compilation and clean stuff up.
    
    * Windows: inital support for msvs2017 community.
    
    * Windows: output all cpp and linker build artifacts (but not halite.exe itself) into obj dir to reduce main dir clutter.
    
    * Windows: adding /ltcg to linker args to make it happier.
    
    * Windows: adding missing zstd build file for msvs2017.
    
    * Windows: drop support for msvs older than 2015.
    
    * Windows: only build x64 version of zstd.

--- environment/make2017.bat
+++ environment/make2017.bat
@@ -1,15 +1,18 @@
+rem TODO: add version and edition autodetection and merge into make.bat
+
 mkdir obj
 
 CD zstd-1.3.0\build\VS_scripts
 
-CALL .\build.VS2015.cmd
+CALL .\build.VS2017.cmd
 COPY .\bin\Release\x64\libzstd_static.lib ..\..\..\obj
 
 CD ..\..\..
 
-SET PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
+SET PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin;%PATH%
 
-CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+SET "VSCMD_START_DIR=%CD%"
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 
 cl.exe /std:c++14 /O2 /MT /EHsc /I . /Fo.\obj\ ^
  .\obj\libzstd_static.lib ^
