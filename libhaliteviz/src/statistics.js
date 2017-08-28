221f8227329f5db8a75a2b1b631c74845e2f4cb5
Tuan Anh Hoang-Vu <hvtuananh@gmail.com>
2017-08-28 13:37:14 -0400

    Added more stats on visualizer (#264)
    * Added more stats
    
     - Health
     - Total Damages
     - Production Rates
     - Attack over time
    
     - Planet: Docking spots available / total
    
    * change wording
    
    * change variable

--- libhaliteviz/src/statistics.js
+++ libhaliteviz/src/statistics.js
@@ -17,6 +17,10 @@ export class Statistics {
                     totalShips: 0,
                     totalTargets: 0,
                     totalAttacks: 0,
+                    totalHealths: 0,
+                    totalDamages: 0,
+                    currentProductions: 0,
+                    currentAttacks: 0
                 };
 
                 if (frameIdx > 0) {
@@ -26,16 +30,34 @@ export class Statistics {
                 }
 
                 frameStats.players[playerId] = playerStats;
+
+                // health: reset from previous frame
+                if (curFrame.ships) {
+                    frameStats.players[playerId].totalHealths = 0;
+                    for(let ship of Object.values(curFrame.ships[playerId])) {
+                        frameStats.players[playerId].totalHealths += ship.health;
+                    }
+                }
+
+                // currentProductions: reset from previous frame
+                frameStats.players[playerId].currentProductions = 0;
+
+                // currentAttacks: reset from previous frame
+                frameStats.players[playerId].currentAttacks = 0;
             }
 
             if (curFrame.events) {
                 for (let event of curFrame.events) {
                     if (event.event === "spawned") {
                         frameStats.players[event.entity.owner].totalShips++;
+                        frameStats.players[event.entity.owner].currentProductions++;
                     }
                     else if (event.event === "attack") {
                         frameStats.players[event.entity.owner].totalTargets += event.targets.length;
                         frameStats.players[event.entity.owner].totalAttacks++;
+                        frameStats.players[event.entity.owner].currentAttacks++;
+                        // damage: should accounts for distance from ship to targets
+                        frameStats.players[event.entity.owner].totalDamages += replay.constants.WEAPON_DAMAGE;
                     }
                 }
             }
