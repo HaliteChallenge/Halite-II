3d2897b58cf5352959c7693a723f836f2a20aba3
Harikrishna Menon <harikrishna.menon@outlook.com>
2017-08-29 06:53:12 -0400

    Added scripts for creating website instances

---
+++ admin/setup_website__startup_script.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# We are running as root, switch to a less privileged account
+sudo -iu worker bash <<"EOF"
+cd
+
+# Fetch the coordinator
+# TODO: This is in GCloud right now, but we should use Git when we go public
+while [ ! -f ./Halite.tgz ]; do
+    sleep 5
+    gsutil cp gs://halite-2-deployed-artifacts/Halite.tgz .
+done
+
+tar xvzf Halite.tgz
+cd Halite/apiserver/
+cp Halite/website/_site /var/www/html/
+
+EOF
