ba20c65e0dfae87bb2a00a0cd7a19f90a55f7702
Harikrishna Menon <harikrishna.menon@outlook.com>
2017-09-03 11:39:56 -0400

    Added deploy scripts

--- build-deployment.sh
+++ build-deployment.sh
@@ -1,9 +1,9 @@
 mkdir Halite
-mkdir "Halite/website"
-mkdir "Halite/website/_site"
+mkdir Halite/website/
+mkdir Halite/website/_site
 cp -rf apiserver Halite/
 cp -rf environment Halite/
 cp -rf website/_site/* Halite/website/_site
 cp -f ../admin-scripts/config.py Halite/apiserver/apiserver/
-tar -cvzf ../Halite.tgz Halite
+tar -czf ../Halite.tgz Halite
 rm -r Halite
