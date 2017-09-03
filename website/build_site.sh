29eec76dd5fdf1b5b4503cace2ecccbc003b12bc
Harikrishna Menon <harikrishna.menon@outlook.com>
2017-09-03 12:57:43 -0400

    Fix travis (#303)
    * Added deploy scripts
    
    * Fixed travis up
    
    * Updated travis config

--- website/build_site.sh
+++ website/build_site.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+sudo gem install bundler
+bundle install --path=vendor/bundle
 
 # Set up builds folder with Windows.exe, Linux-x64, MacOS halite environment executables
 # you need make sure that website/assets/downloads and website/_data needs to be created for this script to work
@@ -10,4 +12,3 @@ npm install
 cd ../website
 npm run build
 bundle exec jekyll build
-gcloud compute scp --recurse _site/* ubuntu@halite-web-server:/var/www/html
