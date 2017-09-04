324004f5c4b059e13ab4342695b28f38265e2861
Harikrishna Menon <harikrishna.menon@outlook.com>
2017-09-04 06:38:07 -0400

    New website (#307)
    * new website ui: visuallizer page
    
    * new website: replay heading
    
    * add video image, add post game dashboard
    
    * update new website: add some graph images. real graph will be added soon
    
    * update new website: add button login in mobile
    
    * new website: update hover state for footer and header navigators
    
    * new website: hide post game dashboard content by default
    
    * new website: add version in post game dashboard
    
    * CSS fix for header and submit bot button
    
    * Minor margin adjustment
    
    * Added deployment files
    
    * Updated remote deploy
    
    * Added more deployment fixes
    
    * Updated deployment scripts
    
    * Fixed up deployments
    
    * Fixed up docs
    
    * Add header menu selected in different sections.
    
    * Halite promo website (#269)
    
    * Added base site
    
    * Fixed up assets
    
    * Fixed more assets
    
    * Adding missing pages
    
    * Updated Home page to use new template
    
    * Updated config to point to staging
    
    * Add forum item in header navigation and adjust elements in footer
    
    * Move halitetv from html to vue (#276)
    
    * Move halite tv from html to vue
    
    * fix a bug
    
    * Move user avatar outside and to the right
    
    * Add a popup for the nav
    
    * change HaliteTV component to Visualizer
    
    * Add Player stats with faked data since api server was down
    
    * update tab pane of player stats
    
    * Update: rename visuallizer -> old visualizer
    
    * Fix header layout after login
    
    * Add some sections in home page
    
    * Add some sections in home page (#293)
    
    * player stats: display real data
    
    * add player detail pane
    
    * change player detail component name
    
    * remove debug
    
    * Player Stats & Player detail panes (#298)
    
    * player stats: display real data
    
    * add player detail pane
    
    * change player detail component name
    
    * remove debug
    
    * Update home page sections

--- website/javascript/templates/Home.vue
+++ website/javascript/templates/Home.vue
@@ -1,5 +1,7 @@
  <template>
      <div class="home-container">
+         <div class="notification">THERE'S AN AMA ON REDDIT TODAY, <a>CLICK HERE</a> TO JOIN THE CONVERSATION.</div>
+
          <div class="row">
              <div class="col-md-12">
                  <p class="d1 c-org">INTRODUCING</p>
@@ -9,8 +11,131 @@
                      <div class="ha-button"><span>PLAY NOW</span></div>
                  </div>
                  <div v-else class="me-in ha-button-container">
-                     <div class="ha-button"><span>CHECK YOUR STATS</span></div>
+                     <div class="ha-button no-bg-button"><span>CHECK YOUR STATS</span></div>
                      <div class="ha-button"><span>SUBMIT A BOT</span></div>
+                 </div>
+             </div>
+             <div class="col-md-12">
+                 <img class="array" :src="`${baseUrl}/assets/images/temp/array.png`"/>
+             </div>
+             <div class="col-md-12 des">
+                 <div class="col-md-offset-1 col-md-5">
+                     <img :src="`${baseUrl}/assets/images/temp/bot_1.png`"/>
+                 </div>
+                 <div class="words col-md-offset-1 col-md-5">
+                    <p class="t2 c-wht">WHAT IS HALITE?</p>
+                     <div class="line-container"><i class="xline xline-top"></i></div>
+                     <p class="t4 c-gry">Halite is an artificial intelligence programming challenge. Players control a bot using the programming language of their choice. Bots fight for control of a 2D grid. The bot with the most territory at the end wins. Victory will require micromanaging of the movement of your pieces, optimizing your bot’s combat ability, and braving a branching factor billions of times higher than that of Go.</p>
+                     <div class="line-container"><i class="xline xline-top"></i></div>
+                     <div class="ha-button-container">
+                         <div class="ha-button no-bg-button"><span>READ DOCUMENT</span></div>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-md-12">
+                 <img class="array" :src="`${baseUrl}/assets/images/temp/array.png`"/>
+             </div>
+             <div class="col-md-12 des">
+                 <div class="words col-md-offset-1 col-md-5">
+                     <p class="t2 c-wht">MINING HALITE IN SPACE</p>
+                     <div class="line-container"><i class="xline xline-top"></i></div>
+                     <p class="t4 c-gry">This year's game is a space story.</p>
+                     <p class="t4 c-gry">Every product that is labeled as a Pocket PC must be accompanied with specific software to operate the unit and must feature a touchscreen and touchpad. </p>
+                     <div class="line-container"><i class="xline xline-top"></i></div>
+                     <div class="ha-button-container">
+                         <div class="ha-button no-bg-button"><span>READ STORY</span></div>
+                     </div>
+                 </div>
+                 <div class="col-md-offset-1 col-md-5">
+                     <img :src="`${baseUrl}/assets/images/temp/bot_2.png`"/>
+                 </div>
+             </div>
+             <div class="col-md-12 ha-line">
+             </div>
+             <div class="col-md-12 big-menu">
+                 <p class="t2 c-wht">WATCH HALITE TV!</p>
+                 <div class="line-container"><i class="xline xline-top"></i></div>
+                 <p class="t4 c-gry">See what's happening right now or watch some of the base</p>
+                 <div class="line-container"><i class="xline xline-top"></i></div>
+                 <div class="col-md-12">
+                     <div class="col-md-3">
+                         <img :src="`${baseUrl}/assets/images/temp/watch.png`"/>
+                         <div class="img-des">
+                             <i class="icon-medal-1"></i>
+                             <span class="t3 c-wht">Juliak vs Erdman</span>
+                             <div class="mgt-10">
+                                 <span class="t4 c-gry">July 5, 08:08:34</span>
+                                 <i class="right-btn fa fa-share-square-o c-yel"></i>
+                             </div>
+                         </div>
+                     </div>
+                     <div class="col-md-3">
+                         <img :src="`${baseUrl}/assets/images/temp/watch.png`"/>
+                         <div class="img-des">
+                             <i class="icon-medal-1"></i>
+                             <span class="t3 c-wht">CariosW... vs Erdman, +2</span>
+                             <div class="mgt-10">
+                                 <span class="t4 c-gry">July 5, 08:08:34</span>
+                                 <i class="right-btn fa fa-share-square-o c-yel"></i>
+                             </div>
+                         </div>
+                     </div>
+                     <div class="col-md-3">
+                         <img :src="`${baseUrl}/assets/images/temp/watch.png`"/>
+                         <div class="img-des">
+                             <i class="icon-medal-1"></i>
+                             <span class="t3 c-wht">Harikmeon vs Erdman</span>
+                             <div class="mgt-10">
+                                 <span class="t4 c-gry">July 5, 08:08:34</span>
+                                 <i class="right-btn fa fa-share-square-o c-yel"></i>
+                             </div>
+                         </div>
+                     </div>
+                     <div class="col-md-3">
+                         <img :src="`${baseUrl}/assets/images/temp/watch.png`"/>
+                         <div class="img-des">
+                             <i class="icon-medal-1"></i>
+                             <span class="t3 c-wht">Harikmeon vs Erdman</span>
+                             <div class="mgt-10">
+                                 <span class="t4 c-gry">July 5, 08:08:34</span>
+                                 <i class="right-btn fa fa-share-square-o c-yel"></i>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="clear"></div>
+                 <div class="ha-button-container">
+                     <div class="ha-button no-bg-button"><span>WATCH HALITE TV</span></div>
+                 </div>
+             </div>
+             <div class="col-md-12 ha-line">
+             </div>
+             <div class="col-md-12 big-menu">
+                 <p class="t2 c-wht">JOIN THE CONVERSATION</p>
+                 <div class="line-container"><i class="xline xline-top"></i></div>
+                 <p class="t4 c-gry">There are many ways to make Halite a social experience</p>
+                 <div class="line-container"><i class="xline xline-top"></i></div>
+                 <div class="col-md-12">
+                     <div class="col-md-4">
+                         <img :src="`${baseUrl}/assets/images/temp/join_1.png`"/>
+                     </div>
+                     <div class="col-md-4">
+                         <img :src="`${baseUrl}/assets/images/temp/join_2.png`"/>
+                     </div>
+                     <div class="col-md-4">
+                         <img :src="`${baseUrl}/assets/images/temp/join_3.png`"/>
+                     </div>
+                 </div>
+                 <div class="clear"></div>
+             </div>
+             <div class="col-md-12 ha-line">
+             </div>
+             <div class="col-md-12 big-menu">
+                 <p class="t2 c-wht">JOIN US NOW</p>
+                 <div class="line-container"><i class="xline xline-top"></i></div>
+                 <div class="ha-button-container">
+                     <div v-if="!me_in" class="ha-button"><span>SIGN UP</span></div>
+                     <div v-else class="ha-button"><span>PLAY NOW</span></div>
                  </div>
              </div>
          </div>
