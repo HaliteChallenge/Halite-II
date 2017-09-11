5eaccf46d363bbb9d28ae6a494cfdbadc3fa3348
Harikrishna Menon <harikrishna.menon@outlook.com>
2017-09-11 10:31:26 -0400

    Base APIs (#354)
    * Base APIs
    
    * Fixed util sucess call
    
    * Fixed feedback from code review

--- promo-website/deploy_site_remote.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Set up builds folder with Windows.exe, Linux-x64, MacOS halite environment executables
-# you need make sure that website/assets/downloads and website/_data needs to be created for this script to work
-
-bundle exec jekyll build
-gcloud compute scp --recurse _site/* ubuntu@halite-web-server-prod:/var/www/html
-
