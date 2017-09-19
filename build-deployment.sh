04d6a0827416ec691f16c1586f204fd922521eab
Harikrishna Menon <harikrishna.menon@outlook.com>
2017-09-19 11:53:06 -0400

    Changed build script to always copy launch site untill release

--- build-deployment.sh
+++ build-deployment.sh
@@ -3,7 +3,7 @@ mkdir Halite/website/
 mkdir Halite/website/_site
 cp -rf apiserver Halite/
 cp -rf environment Halite/
-cp -rf website/_site/* Halite/website/_site
+cp -rf website-drip/_site/* Halite/website/_site
 cp -f ../admin-scripts/config.py Halite/apiserver/apiserver/
 tar -czf ../Halite.tgz Halite
 rm -r Halite
