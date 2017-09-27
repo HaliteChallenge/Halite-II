5eb5f6185a96ecc5b1c871245eecd70a65dd6a7a
Harikrishna Menon <harikrishna.menon@outlook.com>
2017-09-27 15:53:28 -0400

    Added CSS Minifier

--- website/build_site.sh
+++ website/build_site.sh
@@ -6,9 +6,19 @@ bundle install --path=vendor/bundle
 # you need make sure that website/assets/downloads and website/_data needs to be created for this script to work
 python3 make_starter_kits.py 0.9.0-Beta ../../builds/
 
+# building the website
 npm install
 cd ../libhaliteviz
 npm install
 cd ../website
 npm run build
 bundle exec jekyll build
+
+
+# post processing steps
+cd _site/assets/css
+
+# keeping it separate for now pending decision on code editor workflows
+postcss main_new.css > main_new_min.css
+cp main_new_min.css main_new.css
+rm -f main_new_min.css
