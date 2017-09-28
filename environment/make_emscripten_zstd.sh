678d3cb5df62b6605fb91df2537e5e32b5d266b7
David Li <li.davidm96@gmail.com>
2017-09-28 10:57:57 -0400

    Fix problem playing large replay files (#556)
    * Allow JS libzstd to resize memory dynamically (fixes #311)
    
    * Add script to build new copies of libzstd.js

---
+++ environment/make_emscripten_zstd.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Script to build JavaScript/Emscripten version of libzstd
+
+cd ./zstd-1.3.0/lib
+emmake make SHARED_EXT='bc' lib-release
+emcc libzstd.bc -s 'EXPORT_NAME="libzstd"' -s 'EXPORTED_FUNCTIONS=["_ZSTD_versionNumber", "_ZSTD_decompress", "_ZSTD_getFrameContentSize", "_ZSTD_isError"]' -s 'MODULARIZE=1' -s 'ALLOW_MEMORY_GROWTH=1' -o ../../libzstd.js
