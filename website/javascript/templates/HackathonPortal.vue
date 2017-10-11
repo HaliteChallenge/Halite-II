5de79387894941ebecd37b52cd4be117ae415964
Harikrishna Menon <harikrishna.menon@outlook.com>
2017-10-11 17:03:17 -0400

    Updated hackathon page

--- website/javascript/templates/HackathonPortal.vue
+++ website/javascript/templates/HackathonPortal.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hackathon-container">
-    <div class="hackathon-in-progress-container" v-show="showHackathonInProgress">
+    <div class="hackathon-in-progress-container">
       <div class="page-header">
         <h1>EVENTS &amp; HACKATHONS</h1>
         <i class="xline xline-bottom"></i>
@@ -42,33 +42,31 @@
         <i class="xline xline-bottom"></i>
         <p class="t2 c-wht font-headline">ALL HACKATHONS</p>
       </div>
-      <div class="event-cards">
-        <div class="event-card" v-for="event in events" @click="jumpToHackathon(event.id)">
-          <div class="event-img" :style="{'background-image':`url(${event.img})`}"></div>
-          <div class="event-desc">
-            <p class="t3 c-wht">{{event.title}}</p>
-          </div>
-          <div class="event-attr">
-            <ul class="hackathon-info">
-                <li>
-                  <img :src="`${baseUrl}/assets/images/halite-date.svg`"/>
-                  <p class="hackathon-card-item-text">{{event.date}}</p>
-                </li>
-                <li>
-                  <img :src="`${baseUrl}/assets/images/halite-time.svg`"/>
-                  <p class="hackathon-card-item-text">{{event.time}}</p>
-                </li>
-                <li>
-                  <img :src="`${baseUrl}/assets/images/halite-pin.svg`"/>
-                  <p class="hackathon-card-item-text">{{event.pin}}</p>
-                </li>
-              </ul>
-          </div>
-          <div class="event-btn">
-            <a href="javascript:;"><span class="icon-join"></span> JOIN</a>
-          </div>
-        </div>
-      </div>
+      <table class="table table-leader hackathon-table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Location</th>
+            <th>Status</th>
+            <th>Start Date</th>
+            <th>End Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="event in events">
+            <td>
+              <a class="leaderboard-name" :href="'/hackathon-individual?hackathon_id=' + event.id">
+                <img width="30" height="30" :src="`${event.img}`" alt="">
+                {{event.title}}
+              </a>
+            </td>
+            <td>{{event.pin}}</td>
+            <td>{{event.status.charAt(0).toUpperCase() + event.status.slice(1)}}</td>
+            <td>{{event.start_date}}</td>
+            <td>{{event.end_date}}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -96,11 +94,8 @@ export default {
   props: ['baseUrl'],
   data: function() {
     return {
-      showHackathonInProgress: true,
       showEvents: false,
-      hackathons: [],
       events: [],
-      loginServerUrl:`${api.LOGIN_SERVER_URL}/github`,
     };
   },
   mounted: function() {
@@ -128,43 +123,22 @@ export default {
       if(hackathons && hackathons instanceof Array) {
             const wrapEvent = [];
             hackathons.map(hackathon => {
-              const date = moment(hackathon.start_date).format('dddd, MMMM Do');
-              const time = moment(hackathon.start_date).format('LT');
-
               const newEvent = Object.assign({}, eventTemplate, {
                 img: hackathon.thumbnail,
                 pin: hackathon.location,
                 title: hackathon.title,
                 text: hackathon.description,
                 id: hackathon.hackathon_id,
-                date: date,
-                time: time
+                status: hackathon.status,
+                start_date: moment(hackathon.start_date).format('MMMM Do'),
+                end_date: moment(hackathon.end_date).format('MMMM Do')
               })
               wrapEvent.push(newEvent);
             });
 
             this.events = wrapEvent;
-            if(hackathons.length >= 4) {
-              hackathons = hackathons.slice(0, 4);
-            }
-            const wrapHackathon = [];
-            hackathons.map(hackathon => {
-              const newHackathon = Object.assign({}, hackathonTemplate, {
-                img: hackathon.thumbnail,
-                pin: hackathon.location,
-                id: hackathon.hackathon_id,
-                title: hackathon.title
-              });
-              wrapHackathon.push(newHackathon);
-            })
-            this.hackathons = wrapHackathon;
-            this.showHackathonInProgress = true;
           }
           this.showEvents = true;
-    },
-    jumpToHackathon: function(id) {
-      this.gaData('hackathon', 'click-to-join', 'hackathon-flow');
-      window.location.href = `hackathon-individual/?hackathon_id=${id}`
     },
     gaData: function(category, action, label) {
       utils.gaEvent(category, action, label);
