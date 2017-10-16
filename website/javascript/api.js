a61fffdef601c1eaf1bbc02f9621f13bd00489cf
Harikrishna Menon <harikrishna.menon@outlook.com>
2017-10-16 05:35:59 -0400

    Auto ESLint fixes

--- website/javascript/api.js
+++ website/javascript/api.js
@@ -1,322 +1,317 @@
-/*jshint esversion: 6 */
+/* jshint esversion: 6 */
 
-export const API_SERVER_URL = api_server_url;
-export const LOGIN_SERVER_URL = login_server_url;
-export const LOGOUT_SERVER_URL = logout_server_url;
+export const API_SERVER_URL = api_server_url
+export const LOGIN_SERVER_URL = login_server_url
+export const LOGOUT_SERVER_URL = logout_server_url
 
-let cached_me = null;
-let logged_in = null;
+let cached_me = null
+let logged_in = null
 
-export function me_cached() {
-    if (cached_me) {
-        return {
-            user_id: cached_me["user_id"],
-            username: cached_me["username"],
-        };
+export function me_cached () {
+  if (cached_me) {
+    return {
+      user_id: cached_me['user_id'],
+      username: cached_me['username']
     }
-    else if (window.localStorage["cache"]) {
-        return {
-            user_id: window.localStorage["user_id"],
-            username: window.localStorage["username"],
-        };
+  } else if (window.localStorage['cache']) {
+    return {
+      user_id: window.localStorage['user_id'],
+      username: window.localStorage['username']
     }
-    else {
-        return null;
-    }
+  } else {
+    return null
+  }
 }
 
-export function me() {
-    if (cached_me !== null) return Promise.resolve(cached_me);
-    else if (logged_in === false) return Promise.resolve(null);
+export function me () {
+  if (cached_me !== null) return Promise.resolve(cached_me)
+  else if (logged_in === false) return Promise.resolve(null)
 
-    return $.get({
-        url: `${LOGIN_SERVER_URL}/me`,
-        xhrFields: {
-            withCredentials: true,
-        },
-    }).then((me) => {
-        if (me === null) {
-            logged_in = false;
-            return null;
+  return $.get({
+    url: `${LOGIN_SERVER_URL}/me`,
+    xhrFields: {
+      withCredentials: true
+    }
+  }).then((me) => {
+    if (me === null) {
+      logged_in = false
+      return null
+    }
+    logged_in = true
+    return get_user(me.user_id).then((user) => {
+      cached_me = user
+      window.localStorage['cache'] = Date.now()
+      window.localStorage['user_id'] = user.user_id
+      window.localStorage['username'] = user.username
+      return user
+    })
+  })
+}
+
+export function get_user (user_id) {
+  return $.get({
+    url: `${API_SERVER_URL}/user/${user_id}`,
+    xhrFields: {
+      withCredentials: true
+    }
+  })
+}
+
+export function logout () {
+  return $.post({
+    url: `${LOGOUT_SERVER_URL}`,
+    xhrFields: {
+      withCredentials: true
+    }
+  }).then((res) => {
+    logged_in = false
+    window.localStorage.removeItem('cache')
+    window.localStorage.removeItem('user_id')
+    window.localStorage.removeItem('username')
+  })
+}
+
+export function make_profile_image_url (username) {
+  return `https://github.com/${username}.png`
+}
+
+export function list_bots (user_id) {
+  return $.get({
+    url: `${API_SERVER_URL}/user/${user_id}/bot`
+  })
+}
+
+export function makeRequest () {
+  const xhr = new XMLHttpRequest()
+  return xhr
+}
+
+export function update_bot (user_id, bot_id, file, progress_callback) {
+  const method = bot_id === null ? 'POST' : 'PUT'
+  const endpoint = bot_id === null ? 'bot' : `bot/${bot_id}`
+
+  const xhr = makeRequest()
+  xhr.upload.addEventListener('progress', function (e) {
+    if (e.lengthComputable) {
+      progress_callback(e.loaded / e.total)
+    }
+  }, false)
+  xhr.upload.addEventListener('load', function (e) {
+    progress_callback(1)
+  }, false)
+  xhr.withCredentials = true
+  xhr.open(method, `${API_SERVER_URL}/user/${user_id}/${endpoint}`)
+
+  const form_data = new FormData()
+  form_data.append('name', 'botFile')
+  form_data.append('botFile', file)
+
+  xhr.send(form_data)
+
+  return new Promise((resolve, reject) => {
+    xhr.onload = function (e) {
+      if (this.status === 200 || this.status == 201) {
+        resolve()
+      } else {
+        const response = JSON.parse(e.target.responseText)
+        reject(response)
+      }
+    }
+  })
+}
+
+export function list_organizations () {
+  return $.get({
+    url: `${API_SERVER_URL}/organization`
+  })
+}
+
+export function register_me (data) {
+  return $.post({
+    url: `${API_SERVER_URL}/user`,
+    data: JSON.stringify(data),
+    contentType: 'application/json',
+    xhrFields: {
+      withCredentials: true
+    }
+  })
+}
+
+export function update_me (user_id, data) {
+  return $.ajax({
+    url: `${API_SERVER_URL}/user/${user_id}`,
+    method: 'PUT',
+    data: JSON.stringify(data),
+    contentType: 'application/json',
+    xhrFields: {
+      withCredentials: true
+    }
+  })
+}
+
+export function get_replay (game_id, progress_callback) {
+  let game_data_promise = Promise.resolve($.get(`${API_SERVER_URL}/user/0/match/${game_id}`))
+  let replay_promise = new Promise((resolve, reject) => {
+    const xhr = makeRequest()
+    xhr.withCredentials = true
+    xhr.open('GET', `${API_SERVER_URL}/user/0/match/${game_id}/replay`, true)
+    xhr.responseType = 'arraybuffer'
+
+    if (progress_callback) {
+      xhr.onprogress = function (e) {
+        progress_callback(e.loaded, e.total)
+      }
+    }
+
+    xhr.onload = function (e) {
+      if (this.status === 200) {
+        const blob = this.response
+        resolve(blob)
+      } else {
+        reject()
+      }
+    }
+
+    xhr.onerror = function () {
+      reject()
+    }
+
+    xhr.onreadystatechange = function (e) {
+      if (xhr.readyState === 4) {
+        if (xhr.status !== 200) {
+          reject()
         }
-        logged_in = true;
-        return get_user(me.user_id).then((user) => {
-            cached_me = user;
-            window.localStorage["cache"] = Date.now();
-            window.localStorage["user_id"] = user.user_id;
-            window.localStorage["username"] = user.username;
-            return user;
-        });
-    });
+      }
+    }
+
+    xhr.send()
+  })
+  return Promise.all([game_data_promise, replay_promise]).then(([game, replay]) => {
+    return {
+      game: game,
+      replay: replay
+    }
+  })
 }
 
-export function get_user(user_id) {
-    return $.get({
-        url: `${API_SERVER_URL}/user/${user_id}`,
-        xhrFields: {
-            withCredentials: true,
-        },
-    });
-}
+export function get_expired_replay (replay_class, replay_name) {
+  return new Promise((resolve, reject) => {
+    const xhr = makeRequest()
+    xhr.withCredentials = true
+    xhr.open('GET', `${API_SERVER_URL}/replay/class/${replay_class}/name/${replay_name}`, true)
+    xhr.responseType = 'arraybuffer'
 
-export function logout() {
-    return $.post({
-        url: `${LOGOUT_SERVER_URL}`,
-        xhrFields: {
-            withCredentials: true,
-        },
-    }).then((res) => {
-        logged_in = false;
-        window.localStorage.removeItem("cache");
-        window.localStorage.removeItem("user_id");
-        window.localStorage.removeItem("username");
-    });
-}
+    xhr.onload = function (e) {
+      if (this.status === 200) {
+        const blob = this.response
+        resolve(blob)
+      } else {
+        reject()
+      }
+    }
 
-export function make_profile_image_url(username) {
-    return `https://github.com/${username}.png`;
-}
+    xhr.onerror = function () {
+      reject()
+    }
 
-export function list_bots(user_id) {
-    return $.get({
-        url: `${API_SERVER_URL}/user/${user_id}/bot`,
-    });
-}
-
-export function makeRequest() {
-    const xhr = new XMLHttpRequest();
-    return xhr;
-}
-
-export function update_bot(user_id, bot_id, file, progress_callback) {
-    const method = bot_id === null ? "POST" : "PUT";
-    const endpoint = bot_id === null ? "bot" : `bot/${bot_id}`;
-
-    const xhr = makeRequest();
-    xhr.upload.addEventListener("progress", function (e) {
-        if (e.lengthComputable) {
-            progress_callback(e.loaded / e.total);
+    xhr.onreadystatechange = function (e) {
+      if (xhr.readyState === 4) {
+        if (xhr.status !== 200) {
+          reject()
         }
-    }, false);
-    xhr.upload.addEventListener("load", function (e) {
-        progress_callback(1);
-    }, false);
-    xhr.withCredentials = true;
-    xhr.open(method, `${API_SERVER_URL}/user/${user_id}/${endpoint}`);
-
-    const form_data = new FormData();
-    form_data.append("name", "botFile");
-    form_data.append("botFile", file);
-
-    xhr.send(form_data);
-
-    return new Promise((resolve, reject) => {
-        xhr.onload = function (e) {
-            if (this.status === 200 || this.status == 201) {
-                resolve();
-            }
-            else {
-                const response = JSON.parse(e.target.responseText);
-                reject(response);
-            }
-        };
-    });
-}
-
-export function list_organizations() {
-    return $.get({
-        url: `${API_SERVER_URL}/organization`,
-    });
-}
-
-export function register_me(data) {
-    return $.post({
-        url: `${API_SERVER_URL}/user`,
-        data: JSON.stringify(data),
-        contentType: "application/json",
-        xhrFields: {
-            withCredentials: true,
-        },
-    });
-}
-
-export function update_me(user_id, data) {
-    return $.ajax({
-        url: `${API_SERVER_URL}/user/${user_id}`,
-        method: "PUT",
-        data: JSON.stringify(data),
-        contentType: "application/json",
-        xhrFields: {
-            withCredentials: true,
-        },
-    });
-}
-
-export function get_replay(game_id, progress_callback) {
-    let game_data_promise = Promise.resolve($.get(`${API_SERVER_URL}/user/0/match/${game_id}`));
-    let replay_promise = new Promise((resolve, reject) => {
-        const xhr = makeRequest();
-        xhr.withCredentials = true;
-        xhr.open("GET", `${API_SERVER_URL}/user/0/match/${game_id}/replay`, true);
-        xhr.responseType = "arraybuffer";
-
-        if (progress_callback) {
-            xhr.onprogress = function (e) {
-                progress_callback(e.loaded, e.total);
-            };
-        }
-
-        xhr.onload = function (e) {
-            if (this.status === 200) {
-                const blob = this.response;
-                resolve(blob);
-            }
-            else {
-                reject();
-            }
-        };
-
-        xhr.onerror = function () {
-            reject();
-        };
-
-        xhr.onreadystatechange = function (e) {
-            if (xhr.readyState === 4) {
-                if (xhr.status !== 200) {
-                    reject();
-                }
-            }
-        };
-
-        xhr.send();
-    });
-    return Promise.all([game_data_promise, replay_promise]).then(([game, replay]) => {
-        return {
-            game: game,
-            replay: replay,
-        };
-    });
-}
-
-export function get_expired_replay(replay_class, replay_name) {
-    return new Promise((resolve, reject) => {
-        const xhr = makeRequest();
-        xhr.withCredentials = true;
-        xhr.open("GET", `${API_SERVER_URL}/replay/class/${replay_class}/name/${replay_name}`, true);
-        xhr.responseType = "arraybuffer";
-
-        xhr.onload = function (e) {
-            if (this.status === 200) {
-                const blob = this.response;
-                resolve(blob);
-            }
-            else {
-                reject();
-            }
-        };
-
-        xhr.onerror = function () {
-            reject();
-        };
-
-        xhr.onreadystatechange = function (e) {
-            if (xhr.readyState === 4) {
-                if (xhr.status !== 200) {
-                    reject();
-                }
-            }
-        };
-
-        xhr.send();
-    });
-}
-
-export function leaderboard(filters, hackathon = null, offset = null, limit = null) {
-    let url = `${API_SERVER_URL}/leaderboard`;
-    let fields = {};
-    if (hackathon) {
-        url = `${API_SERVER_URL}/hackathon/${hackathon}/leaderboard`;
-        fields.withCredentials = true;
+      }
     }
 
-    const querystring = [];
-    if (offset !== null && limit !== null) {
-        querystring.push(`offset=${offset}&limit=${limit}`);
+    xhr.send()
+  })
+}
+
+export function leaderboard (filters, hackathon = null, offset = null, limit = null) {
+  let url = `${API_SERVER_URL}/leaderboard`
+  let fields = {}
+  if (hackathon) {
+    url = `${API_SERVER_URL}/hackathon/${hackathon}/leaderboard`
+    fields.withCredentials = true
+  }
+
+  const querystring = []
+  if (offset !== null && limit !== null) {
+    querystring.push(`offset=${offset}&limit=${limit}`)
+  }
+  if (filters && filters.length > 0) {
+    querystring.push(`filter=${filters.join('&filter=')}`)
+  }
+  if (querystring.length > 0) {
+    url += `?${querystring.join('&')}`
+  }
+  return $.get({
+    url: url,
+    xhrFields: fields
+  })
+}
+
+export function reset_api_key () {
+  return $.post({
+    url: `${API_SERVER_URL}/api_key`,
+    xhrFields: {
+      withCredentials: true
     }
-    if (filters && filters.length > 0) {
-        querystring.push(`filter=${filters.join('&filter=')}`);
+  })
+}
+
+export function registerHackathon (code) {
+  const me = me_cached()
+  if (!me) {
+    return Promise.reject({
+      message: 'You must be logged in to register for a hackathon.'
+    })
+  }
+  return $.post({
+    url: `${API_SERVER_URL}/user/${me.user_id}/hackathon`,
+    data: {
+      verification_code: code
+    },
+    xhrFields: {
+      withCredentials: true
     }
-    if (querystring.length > 0) {
-        url += `?${querystring.join("&")}`;
+  })
+}
+
+export function getHackathon (id) {
+  return $.get({
+    url: `${API_SERVER_URL}/hackathon/${id}`,
+    xhrFields: {
+      withCredentials: true
     }
-    return $.get({
-        url: url,
-        xhrFields: fields,
-    });
+  })
 }
 
-export function reset_api_key() {
-    return $.post({
-        url: `${API_SERVER_URL}/api_key`,
-        xhrFields: {
-            withCredentials: true,
-        },
-    });
-}
-
-export function registerHackathon(code) {
-    const me = me_cached();
-    if (!me) {
-        return Promise.reject({
-            message: "You must be logged in to register for a hackathon.",
-        });
+export function getUserHackathons (userId) {
+  return $.get({
+    url: `${API_SERVER_URL}/user/${userId}/hackathon`,
+    xhrFields: {
+      withCredentials: true
     }
-    return $.post({
-        url: `${API_SERVER_URL}/user/${me.user_id}/hackathon`,
-        data: {
-            verification_code: code,
-        },
-        xhrFields: {
-            withCredentials: true,
-        },
-    });
+  })
 }
 
-export function getHackathon(id) {
-    return $.get({
-        url: `${API_SERVER_URL}/hackathon/${id}`,
-        xhrFields: {
-            withCredentials: true,
-        },
-    });
+export function getUserHistory (userId) {
+  return $.get({
+    url: `${API_SERVER_URL}/user/${userId}/history`,
+    xhrFields: {
+      withCredentials: true
+    }
+  })
 }
 
-export function getUserHackathons(userId) {
-    return $.get({
-        url: `${API_SERVER_URL}/user/${userId}/hackathon`,
-        xhrFields: {
-            withCredentials: true,
-        }
-    });
+export function invitefriend (email) {
+  return $.post({
+    url: `${API_SERVER_URL}/invitation/user/` + email
+  })
 }
 
-export function getUserHistory(userId) {
-    return $.get({
-        url: `${API_SERVER_URL}/user/${userId}/history`,
-        xhrFields: {
-            withCredentials: true,
-        }
-    });
-}
-
-export function invitefriend(email) {
-    return $.post({
-        url: `${API_SERVER_URL}/invitation/user/` + email
-    });
-}
-
-export function subscribe(email) {
-    return $.post({
-        url: `${API_SERVER_URL}/user/addsubscriber/` + email
-    });
+export function subscribe (email) {
+  return $.post({
+    url: `${API_SERVER_URL}/user/addsubscriber/` + email
+  })
 }
--- website/javascript/main.js
+++ website/javascript/main.js
@@ -1,163 +1,163 @@
-/*jshint esversion: 6 */
+/* jshint esversion: 6 */
 
-import Vue from "vue";
-import 'url-search-params-polyfill';
-import 'element-ui/lib/theme-default/index.css';
-import ApiKey from "./templates/ApiKey.vue";
-import Associate from "./templates/Associate.vue";
-import BotEditor from "./templates/BotEditor.vue";
-import HackathonLeaderboard from "./templates/HackathonLeaderboard.vue";
-import LeaderboardContainer from "./templates/LeaderboardContainer.vue";
-import Upload from "./templates/Upload.vue";
-import UserProfile from "./templates/UserProfile.vue";
-import UserProfileBar from "./templates/UserProfileBar.vue";
-import EditUserProfile from "./templates/EditUserProfile.vue";
-import VerifyEmail from "./templates/VerifyEmail.vue";
-import VisualizerContainer from "./templates/VisualizerContainer.vue";
-import Home from "./templates/Home.vue";
-import HackathonPortal from "./templates/HackathonPortal.vue";
-import HackathonIndividual from './templates/HackathonIndividual.vue';
-import Leagues from './templates/Leagues.vue';
-import LeagueBoard from './templates/LeagueBoard.vue';
-import Play from "./templates/Play.vue";
+import Vue from 'vue'
+import 'url-search-params-polyfill'
+import 'element-ui/lib/theme-default/index.css'
+import ApiKey from './templates/ApiKey.vue'
+import Associate from './templates/Associate.vue'
+import BotEditor from './templates/BotEditor.vue'
+import HackathonLeaderboard from './templates/HackathonLeaderboard.vue'
+import LeaderboardContainer from './templates/LeaderboardContainer.vue'
+import Upload from './templates/Upload.vue'
+import UserProfile from './templates/UserProfile.vue'
+import UserProfileBar from './templates/UserProfileBar.vue'
+import EditUserProfile from './templates/EditUserProfile.vue'
+import VerifyEmail from './templates/VerifyEmail.vue'
+import VisualizerContainer from './templates/VisualizerContainer.vue'
+import Home from './templates/Home.vue'
+import HackathonPortal from './templates/HackathonPortal.vue'
+import HackathonIndividual from './templates/HackathonIndividual.vue'
+import Leagues from './templates/Leagues.vue'
+import LeagueBoard from './templates/LeagueBoard.vue'
+import Play from './templates/Play.vue'
 
 // Include bootstrap.js - do not remove
-import _ from "../vendor_assets/bootstrap-sass-3.3.7/assets/javascripts/bootstrap";
+import _ from '../vendor_assets/bootstrap-sass-3.3.7/assets/javascripts/bootstrap'
 
-import * as api from "./api";
+import * as api from './api'
 
-Vue.use(require('vue-moment'));
-Vue.use(require('vue-cookie'));
-Vue.use(require('element-ui'));
+Vue.use(require('vue-moment'))
+Vue.use(require('vue-cookie'))
+Vue.use(require('element-ui'))
 
 window.views = {
-    ApiKey: function () {
-        new Vue({
-            el: "#api-key-container",
-            render: (h) => h(ApiKey),
-        });
-    },
-    Associate: function () {
-        new Vue({
-            el: "#associate-container",
-            render: (h) => h(Associate),
-        });
-    },
-    BotEditor: function () {
-        new Vue({
-            el: "#bot-editor-container",
-            render: (h) => h(BotEditor),
-        });
-    },
-    HackathonLeaderboard: function () {
-        new Vue({
-            el: "#hackathon-leaderboard-container",
-            render: (h) => h(HackathonLeaderboard),
-        });
-    },
-    LeaderboardContainer: function () {
-        new Vue({
-            el: "#leaderboard-container",
-            render: (h) => h(LeaderboardContainer, { props: { baseUrl: _global.baseUrl } }),
-        });
-    },
-    Upload: function () {
-        new Vue({
-            el: "#upload-container",
-            render: (h) => h(Upload),
-        });
-    },
-    UserProfile: function () {
-        new Vue({
-            el: "#user-profile-container",
-            render: (h) => h(UserProfile, { props: { baseUrl: _global.baseUrl } }),
-        });
-    },
-    EditUserProfile: function () {
-        new Vue({
-            el: "#edit-user-profile-container",
-            render: (h) => h(EditUserProfile, { props: { baseUrl: _global.baseUrl } }),
-        });
-    },
-    VerifyEmail: function () {
-        new Vue({
-            el: "#verify-email-container",
-            render: (h) => h(VerifyEmail),
-        });
-    },
-    Visualizer: function () {
-        new Vue({
-            el: "#visualizer-container",
-            render: (h) => h(VisualizerContainer),
-        });
-    },
-    HaliteTV: function () {
-        new Vue({
-            el: "#halitetv-container",
-            render: (h) => h(VisualizerContainer, { props: { baseUrl: _global.baseUrl } })
-        });
-    },
-    Home: function () {
-        new Vue({
-            el: "#home-container",
-            render: (h) => h(Home, { props: { baseUrl: _global.baseUrl } }),
-        });
-    },
-    HackathonPortal: function () {
-        new Vue({
-            el: "#hackathon-container",
-            render: (h) => h(HackathonPortal, { props: { baseUrl: _global.baseUrl } }),
-        });
-    },
-    HackathonIndividual: function () {
-        new Vue({
-            el: '#hackathon-container',
-            render: (h) => h(HackathonIndividual, { props: { baseUrl: _global.baseUrl } })
-        });
-    },
-    Play: function () {
-        new Vue({
-            el: "#play-container",
-            render: (h) => h(Play, { props: { baseUrl: _global.baseUrl } }),
-        });
-    },
-    Leagues: function () {
-        new Vue({
-            el: "#leagues-container",
-            render: (h) => h(Leagues, { props: { baseUrl: _global.baseUrl } }),
-        });
-    },
-    LeagueBoard: function () {
-        new Vue({
-            el: "#leaderboard-container",
-            render: (h) => h(LeagueBoard, { props: { baseUrl: _global.baseUrl } }),
-        });
-    }
-};
+  ApiKey: function () {
+    new Vue({
+      el: '#api-key-container',
+      render: (h) => h(ApiKey)
+    })
+  },
+  Associate: function () {
+    new Vue({
+      el: '#associate-container',
+      render: (h) => h(Associate)
+    })
+  },
+  BotEditor: function () {
+    new Vue({
+      el: '#bot-editor-container',
+      render: (h) => h(BotEditor)
+    })
+  },
+  HackathonLeaderboard: function () {
+    new Vue({
+      el: '#hackathon-leaderboard-container',
+      render: (h) => h(HackathonLeaderboard)
+    })
+  },
+  LeaderboardContainer: function () {
+    new Vue({
+      el: '#leaderboard-container',
+      render: (h) => h(LeaderboardContainer, { props: { baseUrl: _global.baseUrl } })
+    })
+  },
+  Upload: function () {
+    new Vue({
+      el: '#upload-container',
+      render: (h) => h(Upload)
+    })
+  },
+  UserProfile: function () {
+    new Vue({
+      el: '#user-profile-container',
+      render: (h) => h(UserProfile, { props: { baseUrl: _global.baseUrl } })
+    })
+  },
+  EditUserProfile: function () {
+    new Vue({
+      el: '#edit-user-profile-container',
+      render: (h) => h(EditUserProfile, { props: { baseUrl: _global.baseUrl } })
+    })
+  },
+  VerifyEmail: function () {
+    new Vue({
+      el: '#verify-email-container',
+      render: (h) => h(VerifyEmail)
+    })
+  },
+  Visualizer: function () {
+    new Vue({
+      el: '#visualizer-container',
+      render: (h) => h(VisualizerContainer)
+    })
+  },
+  HaliteTV: function () {
+    new Vue({
+      el: '#halitetv-container',
+      render: (h) => h(VisualizerContainer, { props: { baseUrl: _global.baseUrl } })
+    })
+  },
+  Home: function () {
+    new Vue({
+      el: '#home-container',
+      render: (h) => h(Home, { props: { baseUrl: _global.baseUrl } })
+    })
+  },
+  HackathonPortal: function () {
+    new Vue({
+      el: '#hackathon-container',
+      render: (h) => h(HackathonPortal, { props: { baseUrl: _global.baseUrl } })
+    })
+  },
+  HackathonIndividual: function () {
+    new Vue({
+      el: '#hackathon-container',
+      render: (h) => h(HackathonIndividual, { props: { baseUrl: _global.baseUrl } })
+    })
+  },
+  Play: function () {
+    new Vue({
+      el: '#play-container',
+      render: (h) => h(Play, { props: { baseUrl: _global.baseUrl } })
+    })
+  },
+  Leagues: function () {
+    new Vue({
+      el: '#leagues-container',
+      render: (h) => h(Leagues, { props: { baseUrl: _global.baseUrl } })
+    })
+  },
+  LeagueBoard: function () {
+    new Vue({
+      el: '#leaderboard-container',
+      render: (h) => h(LeagueBoard, { props: { baseUrl: _global.baseUrl } })
+    })
+  }
+}
 
 api.me().then((me) => {
-    if (me) {
-        $(".not-logged-in").hide();
-        $(".navbar-signin").hide();
-        new Vue({
-            el: "#user-profile-bar-container",
-            render: (h) => h(UserProfileBar, { props: { baseUrl: _global.baseUrl } }),
-        });
+  if (me) {
+    $('.not-logged-in').hide()
+    $('.navbar-signin').hide()
+    new Vue({
+      el: '#user-profile-bar-container',
+      render: (h) => h(UserProfileBar, { props: { baseUrl: _global.baseUrl } })
+    })
 
-        if (me.is_new_user === true && window.location.pathname !== "/create-account") {
-            window.location.replace("/create-account");
-        }
+    if (me.is_new_user === true && window.location.pathname !== '/create-account') {
+      window.location.replace('/create-account')
     }
+  }
 });
 
 // auto scroll to the anchor position
 (function () {
-    if (document.location.hash) {
-        const hash = document.location.hash.slice(1);
-        const targetElement = document.getElementById(hash);
-        const top = targetElement.getBoundingClientRect().top - document.body.getBoundingClientRect().top;
-        setTimeout(function () {
-            window.scrollTo(0, 250);
-        }, 1000);
-    }
-})();
+  if (document.location.hash) {
+    const hash = document.location.hash.slice(1)
+    const targetElement = document.getElementById(hash)
+    const top = targetElement.getBoundingClientRect().top - document.body.getBoundingClientRect().top
+    setTimeout(function () {
+      window.scrollTo(0, 250)
+    }, 1000)
+  }
+})()
