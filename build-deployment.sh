b459fd023842dad0bc4468753337135c47c548e3
Harikrishna Menon <harikrishna.menon@outlook.com>
2017-10-18 06:49:24 -0400

    Removed some scripts

--- build-deployment.sh
+++
@@ -1,9 +0,0 @@
-mkdir Halite
-mkdir Halite/website/
-mkdir Halite/website/_site
-cp -rf apiserver Halite/
-cp -rf environment Halite/
-cp -rf extensions/website-drip/_site/* Halite/website/_site
-cp -f ../admin-scripts/config.py Halite/apiserver/apiserver/
-tar -czf ../Halite.tgz Halite
-rm -r Halite
