10aabf46dc7b17bb86d9f45b56cef97912b3dcd1
Harikrishna Menon <harikrishna.menon@outlook.com>
2017-10-19 06:26:16 -0400

    cleaned up unsused code

--- website/javascript/templates/404.vue
+++ website/javascript/templates/404.vue
@@ -14,50 +14,20 @@
       </div>
   </div>
 </template>
-Nothing to see here ... move along... move along
-<script>
-    import * as api from '../api'
 
+<script>
 export default {
-      name: 'verify-email',
+      name: 'view404',
       data: function () {
         return {
-          verification_code: '',
-          user_id: '',
-          error_message: '',
-          success_message: ''
-        }
-  },
-      mounted: function () {
-        const params = new URLSearchParams(window.location.search)
 
-        if (params.has('user_id') && params.has('verification_code')) {
-          this.user_id = params.get('user_id')
-          this.verification_code = params.get('verification_code')
-          this.submit()
         }
       },
+      mounted: function () {
+       
+      },
       methods: {
-        submit: function () {
-          if (this.verification_code && this.user_id) {
-            $.post({
-              url: `${api.API_SERVER_URL}/user/${this.user_id}/verify`,
-              data: {
-                verification_code: this.verification_code
-              },
-              error: (xhr) => {
-                this.error_message = 'Your email verification failed, please contact halite@halite.io: Error Details -' + xhr.responseJSON.message
-              },
-              success: (xhr) => {
-                this.success_message = 'Your email has been verified successfully. If you signed up for a hackathon that requires a verified email, you will now appear on the hackathon leaderboard. \n Sit tight, you’ll be automatically redirected in a few seconds.'
-              }
-            }).then(() => {
-              window.setTimeout(function () {
-                window.location.replace('/user/?me')
-              }, 10000)
-            })
-          }
-        }
+      
       }
     }
 </script>
