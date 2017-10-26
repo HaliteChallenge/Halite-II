c2e0d5586b9f2b450437aad688ead060ecb509b3
Harikrishna Menon <harikrishna.menon@outlook.com>
2017-10-26 11:02:05 -0400

    Revert "Merge pull request #87 from toannguyen132/master"
    This reverts commit e13f745f745699d3122d07e78f5d689fd2da17cc, reversing
    changes made to 6d146e5b700741ad4889d4bfc0567e9e7fbcbaa4.

--- website/javascript/templates/Associate.vue
+++ website/javascript/templates/Associate.vue
@@ -13,16 +13,11 @@
                     <!-- profession -->
                     <div class="form-group">
                         <label for="country">Which of the following describes you best?<span class="text-danger">*</span></label>
-                        <!-- <select class="form-control" id="level" v-model="level">
+                        <select class="form-control" id="level" v-model="level">
                             <option>Professional</option>
                             <option value="University">University</option>
                             <option value="High School">High school</option>
-                        </select> -->
-                        <v-select
-                          placeholder="Professional"
-                          v-model="level"
-                          :options="['Professional', 'University', 'High School']">
-                        </v-select>
+                        </select>
                     </div>
 
                     <div v-if="level === 'Professional'">
