2aa54818047a90249987ec7117bfbc401b612722
David Li <li.davidm96@gmail.com>
2017-10-27 17:41:36 -0400

    Enable zSTD optimizations in Emscripten

--- environment/make_emscripten_zstd.sh
+++ environment/make_emscripten_zstd.sh
@@ -4,4 +4,4 @@
 
 cd ./zstd-1.3.0/lib
 emmake make SHARED_EXT='bc' lib-release
-emcc libzstd.bc -s 'EXPORT_NAME="libzstd"' -s 'EXPORTED_FUNCTIONS=["_ZSTD_versionNumber", "_ZSTD_decompress", "_ZSTD_getFrameContentSize", "_ZSTD_isError"]' -s 'MODULARIZE=1' -s 'ALLOW_MEMORY_GROWTH=1' -o ../../libzstd.js
+emcc libzstd.bc -O2 --memory-init-file 0 -s 'EXPORT_NAME="libzstd"' -s 'EXPORTED_FUNCTIONS=["_ZSTD_versionNumber", "_ZSTD_decompress", "_ZSTD_getFrameContentSize", "_ZSTD_isError"]' -s 'MODULARIZE=1' -s 'ALLOW_MEMORY_GROWTH=1' -o ../../libzstd.js
