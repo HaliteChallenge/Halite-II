f088aa0e126e360766d95b297727057f2543b93e
David Li <li.davidm96@gmail.com>
2017-10-27 18:22:57 -0400

    Extract bot directly to target folder (fixes #106)

--- apiserver/worker/archive.py
+++ apiserver/worker/archive.py
@@ -7,25 +7,15 @@ import shutil
 def unpack(filePath):
     """Unpacks and deletes a zip file into the files current path"""
     folderPath = os.path.dirname(filePath)
-    tempPath = os.path.join(folderPath, "bot")
-    os.mkdir(tempPath)
 
-    # Extract the archive into a folder call 'bot'
-    if platform.system() == 'Windows':
-        os.system("7z x -o"+tempPath+" -y "+filePath+". > NUL")
-    else:
-        os.system("unzip -u -d"+tempPath+" "+filePath+" > /dev/null 2> /dev/null")
+    with zipfile.ZipFile(filePath) as f:
+        f.extractall(folderPath)
 
     # Remove __MACOSX folder if present
-    macFolderPath = os.path.join(tempPath, "__MACOSX")
+    macFolderPath = os.path.join(folderPath, "__MACOSX")
     if os.path.exists(macFolderPath) and os.path.isdir(macFolderPath):
         shutil.rmtree(macFolderPath)
 
-    # Copy contents of bot folder to folderPath remove bot folder
-    for filename in os.listdir(tempPath):
-        shutil.move(os.path.join(tempPath, filename), os.path.join(folderPath, filename))
-
-    shutil.rmtree(tempPath)
     os.remove(filePath)
 
 
