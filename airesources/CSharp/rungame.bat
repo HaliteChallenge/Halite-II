a785a9341c881822da06334f145790aacf88799c
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2017-10-29 20:01:12 +0200

    Fixed rungame.bat.

--- airesources/CSharp/rungame.bat
+++ airesources/CSharp/rungame.bat
@@ -1,2 +1,4 @@
+@echo off
+
 dotnet build -c Release
-./halite -d "240 160" "dotnet ./bin/Release/netcoreapp2.0/Halite2.dll" "dotnet ./bin/Debug/Release/Halite2.dll"
+halite -d "240 160" "dotnet ./bin/Release/netcoreapp2.0/Halite2.dll" "dotnet ./bin/Debug/Release/Halite2.dll"
