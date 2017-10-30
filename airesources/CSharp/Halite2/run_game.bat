c8098790a211210515b4de3080b8685f746b9c42
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2017-10-30 16:36:21 +0200

    Fixed the run scripts. Replaced the old csc invocation.

--- airesources/CSharp/Halite2/run_game.bat
+++ airesources/CSharp/Halite2/run_game.bat
@@ -1,4 +1,3 @@
-csc hlt/Collision.cs hlt/Constants.cs hlt/DockMove.cs hlt/Entity.cs hlt/GameMap.cs hlt/Log.cs hlt/Metadata.cs hlt/MetadataParser.cs hlt/Move.cs hlt/Navigation.cs hlt/Networking.cs hlt/Planet.cs hlt/Player.cs hlt/Position.cs hlt/Ship.cs hlt/ThrustMove.cs hlt/UndockMove.cs hlt/Util.cs -out:MyBot.exe MyBot.cs
-csc hlt/Collision.cs hlt/Constants.cs hlt/DockMove.cs hlt/Entity.cs hlt/GameMap.cs hlt/Log.cs hlt/Metadata.cs hlt/MetadataParser.cs hlt/Move.cs hlt/Navigation.cs hlt/Networking.cs hlt/Planet.cs hlt/Player.cs hlt/Position.cs hlt/Ship.cs hlt/ThrustMove.cs hlt/UndockMove.cs hlt/Util.cs -out:OpponentBot.exe MyBot.cs
-.\halite -d "240 160" "MyBot.exe" "OpponentBot.exe Enemy"
+dotnet build -c Release
+.\halite -d "240 160" "dotnet run -c Release --no-build" "OpponentBot.exe Enemy"
 
--- airesources/CSharp/rungame.sh
+++ airesources/CSharp/rungame.sh
@@ -10,7 +10,7 @@ dotnet build -c Release
 if [ $? -eq 0 ]
 then
 # run bots like it would do in our servers
-./halite -d "240 160" "dotnet ./bin/Debug/netcoreapp2.0/Halite2.dll" "dotnet ./bin/Debug/netcoreapp2.0/Halite2.dll"
+./halite -d "240 160" "dotnet ./bin/Release/netcoreapp2.0/Halite2.dll" "dotnet ./bin/Release/netcoreapp2.0/Halite2.dll"
 
 # run bots without timeouts (good for debugging)
 #./halite -d "240 160" "dotnet ./bin/Debug/netcoreapp2.0/Halite2.dll" "dotnet ./bin/Debug/netcoreapp2.0/Halite2.dll" -t
