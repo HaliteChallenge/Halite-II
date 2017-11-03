1a1a4c0f9893997c2b0a93b4b8faa0bc73260b47
David Li <li.davidm96@gmail.com>
2017-11-03 17:19:13 -0400

    Use multithreading in Flask servers (fixes #147)

--- apiserver/setup.sh
+++ apiserver/setup.sh
@@ -18,10 +18,10 @@ screen -S sqlproxy -d -m /bin/bash -c \
     "./cloud_sql_proxy -instances=${DB_INSTANCE}=tcp:3307"
 
 screen -S api -d -m /bin/bash -c \
-    "PYTHONPATH=$(pwd) FLASK_APP=apiserver.server flask run -h 0.0.0.0 -p 5000"
+    "PYTHONPATH=$(pwd) FLASK_APP=apiserver.server flask run --with-threads -h 0.0.0.0 -p 5000"
 
 screen -S coordinator_internal -d -m /bin/bash -c \
-    "PYTHONPATH=$(pwd) FLASK_APP=apiserver.coordinator_server flask run -h 0.0.0.0 -p 5001"
+    "PYTHONPATH=$(pwd) FLASK_APP=apiserver.coordinator_server flask run --with-threads -h 0.0.0.0 -p 5001"
 
 screen -S badge_daemon -d -m /bin/bash -c \
     "PYTHONPATH=$(pwd) python3 -m apiserver.scripts.badge_daemon.py"
