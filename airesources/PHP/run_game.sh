751f0e47035be29b1f8a430a10322ae81f49266a
Mike Meier <mike@gotom.io>
2017-11-08 11:03:56 +0100

    run_game without seed

--- airesources/PHP/run_game.sh
+++ airesources/PHP/run_game.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-./halite -s 1621542274 -d "240 160" "php MyBot.php" "php MyBot.php"
+#./halite -s 1621542274 -d "240 160" "php MyBot.php" "php MyBot.php"
+./halite -d "240 160" "php MyBot.php" "php MyBot.php" "php MyBot.php" "php MyBot.php"
