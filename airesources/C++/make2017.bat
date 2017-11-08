55e9d3972b080c9935b0f814836263fd44b0642a
Snaar <snaar@snaar.net>
2017-11-08 02:42:32 -0500

    cpp-client: clean up build bat files a bit

--- airesources/C++/make2017.bat
+++ airesources/C++/make2017.bat
@@ -5,8 +5,7 @@ SET PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\1
 SET "VSCMD_START_DIR=%CD%"
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 
-cl.exe /std:c++14 /O2 /MT /EHsc /I . /Fo.\obj\ ^
+cl.exe /FeMyBot.exe /std:c++14 /O2 /MT /EHsc /I . /Fo.\obj\ ^
  /D_USE_MATH_DEFINES ^
  .\hlt\hlt_in.cpp .\hlt\location.cpp .\hlt\map.cpp ^
  .\MyBot.cpp ^
- /link /out:MyBot.exe
