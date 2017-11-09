3876adccb1334d5e3eb51e31d14d8991ba58c91b
Christian Kurz <c.kurz@tu-bs.de>
2017-11-09 18:48:38 +0100

    update outdated const description

--- airesources/Julia/hlt/halite.jl
+++ airesources/Julia/hlt/halite.jl
@@ -20,29 +20,30 @@ include("game_map.jl")
 include("game.jl")
 
 # Constants
-#: Max number of units of distance a ship can travel in a turn
+
+# Max number of units of distance a ship can travel in a turn
 const MAX_SPEED = 7
-#: Radius of a ship
+# Radius of a ship
 const SHIP_RADIUS = 0.5
-#: Starting health of ship, also its max
+# Starting health of ship, also its max
 const MAX_SHIP_HEALTH = 255
-#: Starting health of ship, also its max
+# Starting health of ship, also its max
 const BASE_SHIP_HEALTH = 255
-#: Weapon cooldown period
+# Weapon cooldown period
 const WEAPON_COOLDOWN = 1
-#: Weapon damage radius
+# Weapon damage radius
 const WEAPON_RADIUS = 5.0
-#: Weapon damage
+# Weapon damage
 const WEAPON_DAMAGE = 64
-#: Radius in which explosions affect other entities
+# Radius in which explosions affect other entities
 const EXPLOSION_RADIUS = 10.0
-#: Distance from the edge of the planet at which ships can try to dock
+# Distance from the edge of the planet at which ships can try to dock
 const DOCK_RADIUS = 4.0
-#: Number of turns it takes to dock a ship
+# Number of turns it takes to dock a ship
 const DOCK_TURNS = 4
-#: Number of turns it takes to create a ship per docked ship
+# Number of production units per turn contributed by each docked ship
 const BASE_PRODUCTIVITY = 6
-#: Distance from the planets edge at which new ships are created
+# Distance from the planets edge at which new ships are created
 const SPAWN_RADIUS = 2.0
 
 end #module
