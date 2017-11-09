2ea1c57de90958254523046abee161db6a057f1f
Toan Nguyen <n.minhtoan@gmail.com>
2017-11-08 20:42:38 -0800

    open post game dashboard by default #235

--- website/javascript/templates/Visualizer.vue
+++ website/javascript/templates/Visualizer.vue
@@ -349,7 +349,7 @@
         playing: false,
         showObjectPanel: sessionStorage.getItem('halite-showMapObjectPanel') !== 'false',
         showPlayerDetailPanel: sessionStorage.getItem('halite-showPlayerDetailPanel') === 'true',
-        showChartPanel: sessionStorage.getItem('halite-showChartPanel') === 'true',
+        showChartPanel: sessionStorage.getItem('halite-showChartPanel') ? sessionStorage.getItem('halite-showChartPanel') === 'true' : true,
         speedIndex: 3,
         speedLabel: '3x',
         stats: null,
