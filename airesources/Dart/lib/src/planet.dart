7ddee1be0b5c09a87a44e0a56315520c5e41b214
jerold <jerold.albertson@gmail.com>
2017-11-18 13:08:45 -0700

    fix tab space inconsistency

--- airesources/Dart/lib/src/planet.dart
+++ airesources/Dart/lib/src/planet.dart
@@ -4,15 +4,15 @@ part of hlt;
 class Planet extends Entity {
   int _dockingSpots;
   int _currentProduction;
-	int _remainingProduction;
-	List<int> _dockedShipIDs = new List<int>();
+  int _remainingProduction;
+  List<int> _dockedShipIDs = new List<int>();
 
   Planet._() : super._();
 
   int get currentProduction => _currentProduction;
   int get dockingSpots => _dockingSpots;
-	int get remainingProduction => _remainingProduction;
-	Iterable<int> get dockedShipIDs => new List<int>.from(_dockedShipIDs);
+  int get remainingProduction => _remainingProduction;
+  Iterable<int> get dockedShipIDs => new List<int>.from(_dockedShipIDs);
 
   bool isOwned() => _owner != NO_OWNER;
 }
