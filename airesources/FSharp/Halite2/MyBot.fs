7e9d914caff06a15165a34afbd0f0396a8c0757b
Odonno <bottiau.david@laposte.net>
2017-12-25 13:20:31 +0100

    first version of the fsharp bot

---
+++ airesources/FSharp/Halite2/MyBot.fs
@@ -0,0 +1,32 @@
+﻿open System
+open hlt
+
+[<EntryPoint>]
+let main argv =
+    let botName = 
+        match argv.Length with 
+            | x when x > 0 -> argv.[0] 
+            | _ -> "Ephesus"
+
+    let conn = newConnection botName
+
+    let mutable gameMap = updateMap conn
+    let mutable gameTurn = 1
+
+    while true do
+        gameMap <- updateMap conn
+
+        let myPlayer = 
+            gameMap.Players 
+                |> Array.find (fun p -> p.Id = gameMap.MyId)
+        
+        let commandQueue = 
+            myPlayer.Ships
+                |> Array.filter (fun s -> s.DockingStatus = Undocked)
+                |> Array.map (fun s -> strategyBasicBot s gameMap)
+
+        submitCommands commandQueue
+
+        gameTurn <- gameTurn + 1      
+
+    0 // return an integer exit code
